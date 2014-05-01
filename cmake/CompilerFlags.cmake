9b441ba610d7c5a0d355369d9091496a592c2990
Edwin Lee <leeed2001@gmail.com>
2014-05-01 12:24:05 -0600

    Fix build-type related issues with MSVC (build type is selected in VS, not in CMake)

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -2,7 +2,9 @@
 
 # convert the build type string to upper case for comparisons
 #  http://www.cmake.org/pipermail/cmake/2012-June/050651.html
-STRING( TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_B )
+if ( NOT MSVC )
+	STRING( TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_B )
+endif ()
 
 # **** WINDOWS **** #
 if (WIN32 AND NOT UNIX)  # UNIX clause required to filter out cygwin installations
@@ -11,7 +13,7 @@ if (WIN32 AND NOT UNIX)  # UNIX clause required to filter out cygwin installatio
     # Configuration Properties ->Debugging -> Environment, use drop-down list to choose <Edit> and type _NO_DEBUG_HEAP=1 then click OK 
     # C/C++ -> General -> Multi-processor Compilation use drop-down to choose Yes (/MP) 
     # C/C++ -> Code Generation -> Basic Runtime Checks use drop-down to choose Default 
-    # C/C++ -> Language -> Disable Language Extensions use drop-down to choose Yes (/Za)
+    # C/C++ -> Language -> Disable Language Extensions use drop-down to choose Yes (/Za) -- IN USE BELOW
     
     # visual c++ (VS 2013)
     if (MSVC)    
@@ -20,17 +22,11 @@ if (WIN32 AND NOT UNIX)  # UNIX clause required to filter out cygwin installatio
         #  4258  Definition from the loop is ignored
         #  4355  Passing this pointer in class initializer (object is incomplete so bases/members can only use this in limited ways)
         #  4996  Deprecated" STL functions (that MS has safer, non-std alternatives for)
-        if (CMAKE_BUILD_TYPE_B MATCHES RELEASE)
-            SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nologo -Za -EHsc -GR -wd4244 -wd4258 -wd4355 -wd4996 -DVC_EXTRALEAN -DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_CRT_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_DEPRECATE -D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES -TP -O2 -DNDEBUG")
-            SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nologo -F2097152")  # was LDFLAGS
-            SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -link -NODEFAULTLIB:libcd")  # was LINKFLAGS
-        endif ()
-        if (CMAKE_BUILD_TYPE_B MATCHES DEBUG)
-            # -RTCc gave exe blocked by Windows 8.1 Defender with ostringstream
-            SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nologo -Za -EHsc -GR -wd4244 -wd4258 -wd4355 -wd4996 -DVC_EXTRALEAN -DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_CRT_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_DEPRECATE -D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES -TP -Z7 -Od -Ob0 -RTCsu")
-            SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nologo -F2097152")  # was LDFLAGS
-            SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -link -NODEFAULTLIB:libcd")  # was LINKFLAGS
-        endif ()
+        SET(CMAKE_CXX_FLAGS_RELEASE  "${CMAKE_CXX_FLAGS_RELEASE} -nologo -Za -EHsc -GR -wd4244 -wd4258 -wd4355 -wd4996 -DVC_EXTRALEAN -DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_CRT_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_DEPRECATE -D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES -TP -O2 -DNDEBUG")
+        # -RTCc gave exe blocked by Windows 8.1 Defender with ostringstream
+        SET(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -nologo -Za -EHsc -GR -wd4244 -wd4258 -wd4355 -wd4996 -DVC_EXTRALEAN -DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_CRT_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_DEPRECATE -D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES -TP -Z7 -Od -Ob0 -RTCsu")
+        SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nologo -F2097152")  # was LDFLAGS
+        SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -link -NODEFAULTLIB:libcd")  # was LINKFLAGS
     endif ()
     # g++
     if (CMAKE_COMPILER_IS_GNUCXX)
