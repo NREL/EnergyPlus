b4b249db79d26a706d4d032a4778ecff32dc4715
Edwin Lee <leeed2001@gmail.com>
2014-10-21 09:12:54 -0600

    Update name of callback test binary; indent cleanup in API header

--- src/EnergyPlus/EnergyPlusAPI.hh
+++ src/EnergyPlus/EnergyPlusAPI.hh
@@ -2,16 +2,13 @@
 #define ENERGYPLUS_LIB_ENERGYPLUSAPI_HPP
 
 #if _WIN32 || _MSC_VER
-
-#ifdef EnergyPlusLib_EXPORTS
-#define ENERGYPLUSLIB_API __declspec(dllexport)
+ #ifdef EnergyPlusLib_EXPORTS
+  #define ENERGYPLUSLIB_API __declspec(dllexport)
+ #else
+  #define ENERGYPLUSLIB_API __declspec(dllimport)
+ #endif
 #else
-#define ENERGYPLUSLIB_API __declspec(dllimport)
-#endif
-
-#else
-#define ENERGYPLUSLIB_API
-
+ #define ENERGYPLUSLIB_API
 #endif
 
 #endif
