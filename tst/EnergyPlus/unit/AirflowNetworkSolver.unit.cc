096d8993880e75a2d8917509d2504c28be55d320
Lixing Gu <gu@fsec.ucf.edu>
2015-03-04 13:25:49 -0500

    Add a unit test file for #4736

---
+++ tst/EnergyPlus/unit/AirflowNetworkSolver.unit.cc
@@ -0,0 +1,81 @@
+// EnergyPlus::DXCoils unit tests
+// DX heating coil defrost capacity with electric resistance
+
+// Google test headers
+#include <gtest/gtest.h>
+
+// C++ Headers
+#include <cassert>
+#include <cmath>
+#include <string>
+
+// ObjexxFCL Headers
+#include <ObjexxFCL/FArray.functions.hh>
+#include <ObjexxFCL/Fmath.hh>
+#include <ObjexxFCL/gio.hh>
+
+// EnergyPlus Headers
+#include <DataAirflowNetwork.hh>
+#include <AirflowNetworkBalanceManager.hh>
+#include <AirflowNetworkSolver.hh>
+
+using namespace EnergyPlus;
+using namespace AirflowNetworkBalanceManager;
+using namespace DataAirflowNetwork;
+using namespace AirflowNetworkSolver;
+
+TEST( AirflowNetworkSolverTest, Test1 )
+{
+
+//	FArray1D< DXCoilData > DXCoil;
+	int i = 1;
+	int j = 1;
+	int CurveNum;
+	int n;
+	int m;
+	int NF;
+	FArray1D< Real64 > F;
+	FArray1D< Real64 > DF;
+
+	n = 1;
+	m = 2;
+
+
+	AirflowNetworkCompData.allocate( j );
+	AirflowNetworkCompData( j ).TypeNum = 1;
+	MultizoneSurfaceData.allocate( i );
+	MultizoneSurfaceData( i ).Width = 10.0;
+	MultizoneSurfaceData( i ).Height = 5.0;
+	MultizoneSurfaceData( i ).OpenFactor = 1.0;
+
+	RHOZ.allocate( 2 );
+	RHOZ( 1 ) = 1.2;
+	RHOZ( 2 ) = 1.18;
+
+	MultizoneCompHorOpeningData.allocate( 1 );
+	MultizoneCompHorOpeningData( 1 ).FlowCoef = 0.1;
+	MultizoneCompHorOpeningData( 1 ).FlowExpo = 0.5;
+	MultizoneCompHorOpeningData( 1 ).Slope = 90.0;
+	MultizoneCompHorOpeningData( 1 ).DischCoeff = 0.2;
+
+	F.allocate( 2 );
+	DF.allocate( 2 );
+
+	AirflowNetworkLinkageData.allocate( i );
+	AirflowNetworkLinkageData( i ).NodeHeights( 1 ) = 4.0;
+	AirflowNetworkLinkageData( i ).NodeHeights( 2 ) = 2.0;
+
+	AFEHOP( 1, 1, 0.05, 1, 1, 2, F, DF, NF );
+	EXPECT_NEAR( 3.47863, F( 1 ), 0.00001 );
+	EXPECT_NEAR( 34.7863, DF( 1 ), 0.0001 );
+	EXPECT_NEAR( 2.96657, F( 2 ), 0.00001 );
+	EXPECT_EQ( 0.0, DF( 2 ) );
+
+	AFEHOP( 1, 1, -0.05, 1, 1, 2, F, DF, NF );
+	EXPECT_NEAR( -3.42065, F( 1 ), 0.00001 );
+	EXPECT_NEAR( 34.20649, DF( 1 ), 0.0001 );
+	EXPECT_NEAR( 2.96657, F( 2 ), 0.00001 );
+	EXPECT_EQ( 0.0, DF( 2 ) );
+}
+
+
