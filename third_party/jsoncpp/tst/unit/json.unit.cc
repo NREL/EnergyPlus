b25ddc52906a56ac66fc43c80c2bf6e43a30cfe6
Jason W. DeGraw <jason.degraw@gmail.com>
2015-05-29 23:03:10 -0600

    Add a simple JSON test

--- third_party/jsoncpp/tst/unit/json.unit.cc
+++ third_party/jsoncpp/tst/unit/json.unit.cc
@@ -3,9 +3,33 @@
 // Google test headers
 #include <gtest/gtest.h>
 
+#include <json/json.h>
+
 TEST( JsonCppTests, Simple )
 {
-
+  std::string string("{\"name\":\"EnergyPlus\", \"value\":1, \"string array\":[\"yellow\", \"blue\"]}");
+  Json::Reader reader;
+  Json::Value root;
+  EXPECT_TRUE(reader.parse(string, root));
+  ASSERT_FALSE(root.isNull());
+  ASSERT_TRUE(root.isObject());
+  EXPECT_EQ(root["name"], "EnergyPlus");
+  EXPECT_TRUE(root["string array"].isArray());
+  EXPECT_EQ(2, root["string array"].size());
+  int i = 0;
+  for (const Json::Value &value : root["string array"]) {
+    ASSERT_TRUE(value.isString());
+    switch (i) {
+    case 0:
+      EXPECT_EQ(value, "yellow");
+      break;
+    case 1:
+      EXPECT_EQ(value, "blue");
+      break;
+    }
+    ++i;
+  }
+  EXPECT_FALSE(root["not a key"]);
 }
 
 
