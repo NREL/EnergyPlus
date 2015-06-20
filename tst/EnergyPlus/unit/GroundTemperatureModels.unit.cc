d404fe36b4ca241ea7cc458bf33e9afba4d6ebb1
mmatts <matt.s.mitchell@okstate.edu>
2015-06-20 08:04:52 -0500

    Stub-outs for ground temperature model unit tests

---
+++ tst/EnergyPlus/unit/GroundTemperatureModels.unit.cc
@@ -0,0 +1,10 @@
+// EnergyPlus::GroundTemperatureModels Unit Tests
+
+// Google Test Headers
+#include <gtest/gtest.h>
+
+// EnergyPlus Headers
+#include <EnergyPlus/GroundTempsManager.hh>
+
+using namespace EnergyPlus;
+using namespace EnergyPlus::GroundTemps;
