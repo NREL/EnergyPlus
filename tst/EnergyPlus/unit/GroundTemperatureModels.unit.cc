a01558b8d2ea95b6ae458e05ffc23f6c203df14c
Rajesh Bojja <rajesh.bojja@okstate.edu>
2015-06-23 15:38:28 -0500

    Adding first unit test.

--- tst/EnergyPlus/unit/GroundTemperatureModels.unit.cc
+++ tst/EnergyPlus/unit/GroundTemperatureModels.unit.cc
@@ -5,6 +5,28 @@
 
 // EnergyPlus Headers
 #include <EnergyPlus/GroundTempsManager.hh>
+#include <EnergyPlus/UtilityRoutines.hh>
 
 using namespace EnergyPlus;
+
 using namespace EnergyPlus::GroundTemps;
+
+TEST(GroundTempsModelKusudaAchenbachTest, getGroundTemp)
+{
+	ShowMessage( "Begin Test: GroundTempsModelKusudaAchenbachTest, getGroundTemp" );
+
+	// Initialization
+	KusudaGroundTempsModel thisGroundTempsModel;
+	Real64 thisGFunc;
+	Real64 z = 0; // Depth
+	Real64 diffusivityGround = 4.0e-7; // Groud props
+	Real64 simTimeInSeconds = 0; // Simulation time
+
+	thisGroundTempsModel.aveGroundTemp = 15;
+	thisGroundTempsModel.aveGroundTempAmplitude = 5;
+	thisGroundTempsModel.phaseShiftInSecs = 0;
+	
+	thisGFunc = thisGroundTempsModel.getGroundTemp( z, diffusivityGround, simTimeInSeconds );
+	EXPECT_DOUBLE_EQ( 10.0, thisGFunc );
+
+}
