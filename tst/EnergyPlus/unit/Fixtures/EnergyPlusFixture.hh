8dca04a560f62d483cf2005e0dbd5b2534b3d571
Mark Adams <adamsmb@ornl.gov>
2015-06-30 19:05:50 -0400

    Clean up EnergyPlusFixture

--- tst/EnergyPlus/unit/Fixtures/EnergyPlusFixture.hh
+++ tst/EnergyPlus/unit/Fixtures/EnergyPlusFixture.hh
@@ -7,25 +7,7 @@
 // EnergyPlus Headers
 #include <EnergyPlus/DataStringGlobals.hh>
 
-#include <regex>
-
 namespace EnergyPlus {
-
-	// void redirect_cout( std::unique_ptr<std::ostringstream> const & m_buffer, std::unique_ptr<std::streambuf> & m_old_buffer ) {
-	// 	m_buffer = std::unique_ptr<std::ostringstream>( new std::ostringstream );
-	// 	// Save cout's buffer here
-	// 	m_old_buffer = std::unique_ptr<std::streambuf>( std::cout.rdbuf() );
-	// 	// Redirect cout to our ostringstream buffer or any other ostream
-	// 	std::cout.rdbuf( m_buffer->rdbuf() );
-	// }
-
-	// void redirect_cout( std::unique_ptr<std::ostringstream> const & m_buffer, std::unique_ptr<std::streambuf> & m_old_buffer ) {
-	// 	m_buffer = std::unique_ptr<std::ostringstream>( new std::ostringstream );
-	// 	// Save cout's buffer here
-	// 	m_old_buffer = std::unique_ptr<std::streambuf>( std::cout.rdbuf() );
-	// 	// Redirect cout to our ostringstream buffer or any other ostream
-	// 	std::cout.rdbuf( m_buffer->rdbuf() );
-	// }
 
 	class EnergyPlusFixture : public testing::Test
 	{
@@ -63,18 +45,12 @@ namespace EnergyPlus {
 		virtual void SetUp() {
 			m_cout_buffer = std::unique_ptr<std::ostringstream>( new std::ostringstream );
 			m_redirect_cout = std::unique_ptr<RedirectCout>( new RedirectCout( m_cout_buffer ) );
-			
+
 			m_cerr_buffer = std::unique_ptr<std::ostringstream>( new std::ostringstream );
 			m_redirect_cerr = std::unique_ptr<RedirectCerr>( new RedirectCerr( m_cerr_buffer ) );
-			// Save cout's buffer here
-			// m_old_cout_buffer = std::unique_ptr<std::streambuf>( std::cout.rdbuf() );
-			// Redirect cout to our ostringstream buffer or any other ostream
-			// std::cout.rdbuf( m_buffer->rdbuf() );
 		}
 
 		virtual void TearDown() {
-			// When done redirect cout to its old self
-			// std::cout.rdbuf( m_old_buffer.release() );
 			m_redirect_cout.reset();
 			m_redirect_cout = nullptr;
 			m_redirect_cerr.reset();
@@ -94,42 +70,9 @@ namespace EnergyPlus {
 			return compare_text->str();
 		}
 
-
-		// void death_function( std::function<void()> func, std::string const & cerr_regex = ".*", bool assert_death = false, bool assert_message = false ) {
-		// 	if ( assert_death ) {
-		// 		ASSERT_DEATH( func(), cerr_regex );
-		// 	} else {
-		// 		EXPECT_DEATH( func(), cerr_regex );
-		// 	}
-
-		// 	// std::regex test_regex( fatal_regex );
-		// 	// if ( assert_message ) {
-		// 	// 	ASSERT_TRUE( std::regex_search( m_buffer->str(), test_regex ) );
-		// 	// } else {
-		// 	// 	EXPECT_TRUE( std::regex_search( m_buffer->str(), test_regex ) );
-		// 	// }
-		// }
-
-		// template <typename T>
-		// void death_function( std::function<T()> func, std::string const & cerr_regex = ".*", bool assert_death = false, bool assert_message = false ) {
-		// 	if ( assert_death ) {
-		// 		ASSERT_DEATH( func(), cerr_regex );
-		// 	} else {
-		// 		EXPECT_DEATH( func(), cerr_regex );
-		// 	}
-
-		// 	// std::regex test_regex( fatal_regex );
-		// 	// if ( assert_message ) {
-		// 	// 	ASSERT_TRUE( std::regex_search( m_buffer->str(), test_regex ) );
-		// 	// } else {
-		// 	// 	EXPECT_TRUE( std::regex_search( m_buffer->str(), test_regex ) ) << "cout: " << m_buffer->str() << " regex: " << fatal_regex;
-		// 	// }
-		// }
-
 	private:
 		std::unique_ptr<std::ostringstream> m_cout_buffer;
 		std::unique_ptr<std::ostringstream> m_cerr_buffer;
-		// std::unique_ptr<std::streambuf> m_old_cout_buffer;
 		std::unique_ptr<RedirectCout> m_redirect_cout;
 		std::unique_ptr<RedirectCerr> m_redirect_cerr;
 
