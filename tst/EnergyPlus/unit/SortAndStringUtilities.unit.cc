5ca93779d9a0ec32bf18ab8d36eb2ea84900d905
rglazer2 <ryan@glazer350.com>
2015-08-07 18:32:05 -0500

    Fixed bug in SortAndSetup function in SortAndStringUtilities.cc and added unit test

--- tst/EnergyPlus/unit/SortAndStringUtilities.unit.cc
+++ tst/EnergyPlus/unit/SortAndStringUtilities.unit.cc
@@ -9,9 +9,11 @@
 // EnergyPlus Headers
 #include <EnergyPlus/SortAndStringUtilities.hh>
 #include <EnergyPlus/UtilityRoutines.hh>
+#include <EnergyPlus/InputProcessor.hh>
 
 using namespace EnergyPlus;
 using namespace EnergyPlus::SortAndStringUtilities;
+using namespace EnergyPlus::InputProcessor;
 using namespace ObjexxFCL;
 
 TEST( SortAndStringUtilitiesTest, Basic )
@@ -22,4 +24,32 @@ TEST( SortAndStringUtilitiesTest, Basic )
 	Array1D_int iAlphas( 5 );
 	SetupAndSort( Alphas, iAlphas );
 	EXPECT_TRUE( eq( Array1D_int( { 4, 5, 2, 3, 1 } ), iAlphas ) );
+}
+
+TEST( SortAndStringUtilitiesTest, findItemInSortedListUnderscoreTest)
+{
+	static Array1D_string ListOfObjects; // stored variable names
+	static Array1D_int iListOfObjects;
+	ListOfObjects.allocate(7);
+
+
+	ListOfObjects = Array1D_string({			//list which has been incorrectly sorted
+		"SYSTEM NODE STANDARD DENSITY VOLUME FLOW RATE",
+		"SYSTEM NODE TEMPERATURE",
+		"SYSTEM NODE WETBULB TEMPERATURE",
+		"S_CCFRAC",
+		"T_TRIG",
+		"VRF HEAT PUMP CONDENSER INLET TEMPERATURE",
+		"VRF HEAT PUMP COOLING COP",
+	});
+
+	int NumObjectDefs = ListOfObjects.size();
+
+	iListOfObjects.allocate(NumObjectDefs);
+	SetupAndSort(ListOfObjects, iListOfObjects);		//list is resorted
+
+	auto index = FindItemInSortedList("SYSTEM NODE TEMPERATURE", ListOfObjects, NumObjectDefs);
+
+	EXPECT_EQ(3, index);
+
 }
