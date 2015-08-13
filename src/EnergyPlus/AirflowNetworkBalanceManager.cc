ca6334d33f8dacb1257f301393c6fb6daca51298
Jason W. DeGraw <jason.degraw@gmail.com>
2015-08-13 13:36:17 -0600

    Change error message to write out missing zone

--- src/EnergyPlus/AirflowNetworkBalanceManager.cc
+++ src/EnergyPlus/AirflowNetworkBalanceManager.cc
@@ -1297,7 +1297,7 @@ namespace AirflowNetworkBalanceManager {
 						MultizoneSurfaceData( i ).NodeNums( 2 ) = j;
 					} else {
 						ShowSevereError( RoutineName + CurrentModuleObject + " = " + MultizoneSurfaceData( i ).SurfName );
-						ShowContinueError( "..Zone = " + Zone( Surface( MultizoneSurfaceData( i ).SurfNum ).Zone ).Name + " is not described in AIRFLOWNETWORK:MULTIZONE:ZONE" );
+						ShowContinueError( "..Zone = " + Zone( Surface( n ).Zone ).Name + " is not described in AIRFLOWNETWORK:MULTIZONE:ZONE" );
 						ErrorsFound = true;
 						continue;
 					}
