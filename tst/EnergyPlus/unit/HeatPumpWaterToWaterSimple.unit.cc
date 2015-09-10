cf001150cac7430a10dda61eb13543dde281aab8
Brent Griffith <Brent.Griffith@EnergyArchmage.com>
2015-09-10 12:12:12 -0600

    add unit test file

---
+++ tst/EnergyPlus/unit/HeatPumpWaterToWaterSimple.unit.cc
@@ -0,0 +1,17 @@
+// Google Test Headers
+#include <gtest/gtest.h>
+
+#include "Fixtures/HVACFixture.hh"
+
+namespace EnergyPlus {
+
+
+	TEST_F( HVACFixture, PlantLoopSourceSideTest ) {
+
+		std::string const idf_objects = delimited_string( { 
+
+		}
+
+	}
+
+}
