621b8629c56175639755f366ce858e44b175086c
Edwin Lee <leeed2001@gmail.com>
2015-10-27 15:37:00 -0600

    Add better stdout flushing

--- src/EnergyPlus/main.cc
+++ src/EnergyPlus/main.cc
@@ -1,10 +1,12 @@
 #include <EnergyPlusPgm.hh>
 #include <CommandLineInterface.hh>
+#include <iostream>
 using EnergyPlus::CommandLineInterface::ProcessArgs;
 
 int
 main( int argc, const char * argv[] )
 {
+	std::cout.setf( std::ios_base::unitbuf );
 	ProcessArgs( argc, argv );
 	EnergyPlusPgm();
 }
