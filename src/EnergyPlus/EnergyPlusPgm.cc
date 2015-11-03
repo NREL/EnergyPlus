e78bf8a443f5f4833cea7bebb5a290259bc7aae4
Jason W. DeGraw <jason.degraw@gmail.com>
2015-11-03 13:35:58 -0700

    Update main program file

--- src/EnergyPlus/EnergyPlusPgm.cc
+++ src/EnergyPlus/EnergyPlusPgm.cc
@@ -1,3 +1,130 @@
+// Portions of the EnergyPlus(tm) software package have been developed and copyrighted by other
+// individuals, companies and institutions.  These portions have been incorporated into the EnergyPlus
+// software package under license.
+
+// In addition to the primary authorship of the LBNL Simulation Research Group (
+// http://simulationresearch.lbl.gov/) and the UIUC Building Systems Laboratory (
+// http://www.bso.uiuc.edu/), the following have contributed to EnergyPlus V1.0:
+
+// Portions of the EnergyPlus weather processor were developed by US Department of Energy, Building
+// Technologies Program, www.energyplus.gov
+
+// Portions of the input processing, output processing, weather processor, BLAST Translator were
+// developed by US Army Corps of Engineers, Construction Engineering Research Laboratories, 2902 Newmark
+// Drive, Champaign IL  61821. www.cecer.army.mil
+
+// Portions of this software package were developed by Linda Lawrie of DHL Consulting LLC.
+
+// Portions of this software package were developed by C.O. Pedersen Associates.
+
+// Portions of the EnergyPlus utility software (EP-Launch, IDFEditor, DOE2Translator, HVAC-Diagram,
+// ExpandObjects, CSVProc, System Templates, and convertESOMTR) were developed by GARD Analytics, Inc.
+// 1028 Busse Highway, Park Ridge, Illinois 60068-1802, USA (847) 698-5690, www.gard.com.  GARD Analytics
+// performed independent verification and validation testing of the software after developing the testing
+// strategy and plan.  GARD Analytics was also responsible for gas absorption chiller, desiccant
+// dehumidifier, ice storage (simple), table reports and economics.
+
+// Portions of flow resolver, chiller models (absorption, electric, const cop, engine-driven, gas-
+// turbine), generator models (diesel electric, gas turbine), furnace models, heat recovery loop, plant
+// loop, plant condenser loop, air-change dependent inside film coefficients were developed by Oklahoma
+// State University, 110 Engineering North, Stillwater, OK 74078.
+
+// Portions of EnergyPlus related to the models for EMPD moisture calculations, DX coils, furnace/unitary
+// systems, cooling towers, air-to-air heat pumps, air distribution systems, refrigerated cases, electric
+// EIR chillers, packaged terminal heat pumps, desuperheater air and water heating coils, and heat pump
+// water heaters were developed by University of Central Florida, Florida Solar Energy Center (FSEC),
+// 1679 Clearlake Road, Cocoa, FL  32922, www.fsec.ucf.edu/.
+
+// Portions of EnergyPlus were developed by the National Renewable Energy Laboratory (NREL), 1617 Cole
+// Blvd, Golden, CO 80401.
+
+// EnergyPlus v1.0.1, v1.0.2, v1.0.3, v1.1, v1.1.1 (Wintel platform) includes a link to TRNSYS (The Transient
+// Energy System Simulation Tool) for photovoltaic calculations developed by Thermal Energy System Specialists,
+// 2916 Marketplace Drive, Suite 104, Madison, WI 53719; Tel: (608) 274-2577. EnergyPlus v1.2 and later
+// includes Photovoltaic calculations implemented in EnergyPlus by Thermal Energy System Specialists.
+// This model was originally developed by Oystein Ulleberg, Institute for Energy Technology, Norway -- based on
+// the Duffie and Beckman equivalent one-diode model.
+
+// Portions of this software package that convert certain stand-alone heat transfer models for slab-on-
+// grade and basement foundations were developed by William Bahnfleth, Cynthia Cogil, and Edward
+// Clements, Department of Architectural Engineering, Pennsylvania State University, 224 Engineering Unit
+// A, University Park, Pennsylvania  16802-1416, (814) 863-2076.
+
+// The concept and initial implementation for the EnergyPlus COM/DLL version (Wintel platform) was made
+// possible through cooperation with DesignBuilder Software, Ltd, Andy Tindale - an EnergyPlus
+// collaborative developer.
+
+// The thickness, conductivity, density and specific heat values of the material layers for the
+// constructions in the Composite Wall Construction reference data set have been taken from the ASHRAE
+// report "Modeling Two- and Three-Dimensional Heat Transfer through Composite Wall and Roof Assemblies
+// in Hourly  Energy Simulation Programs (1145-TRP)," by Enermodal Engineering Limited, Oak Ridge
+// National Laboratory, and the Polish Academy of Sciences, January 2001.
+
+// EnergyPlus v1.2 contains DELight2 (wintel platform), a simulation engine for daylighting and electric
+// lighting system analysis developed at Ernest Orlando Lawrence Berkeley National Laboratory.
+
+// Portions of the EnergyPlus v1.2 air distribution system calculations were written by George Walton of
+// the National Institute for Standards and Technology (NIST), 100 Bureau Drive, Gaithersburg, MD 20899,
+// (301) 975-6478.  The EnergyPlus AirflowNetwork model also includes portions of an early version of COMIS
+// (Conjunction Of Multizone Infiltration Specialists) developed by a multinational, multi-institutional
+// effort under the auspices of the International Energy Agency's Buildings and Community Systems Agreement
+// working group focusing on multizone air flow modeling (Annex 23) and now administered by the Swiss Federal
+// Laboratories for Materials Testing and Research (EMPA), Division 175, Uberlandstrasse 129, CH-8600 Dubendorf,
+// Switzerland.
+
+// The EnergyPlus v1.2 model for displacement ventilation and cross-ventilation was developed
+// by Guilherme Carrilho da Graca and Paul Linden of the Department of Mechanical and Aerospace
+// Engineering, University of California, San Diego.
+
+// The EnergyPlus models for UFAD served zones were developed by Anna Liu and Paul Linden at the Department
+// of Mechanical and Aerospace Engineering, University of California, San Diego.
+
+// ASHRAE research project 1254-RP supported the development of the following features first added in
+// EnergyPlus v1.2.2:
+//    DXSystem:AirLoop enhancements (valid as OA system equipment, new humidity control options);
+//    New set point managers: SET POINT MANAGER:SINGLE ZONE HEATING, SET POINT MANAGER:SINGLE ZONE COOLING,
+//            and SET POINT MANAGER:OUTSIDE AIR PRETREAT;
+//    New 2-stage DX coil with enhanced dehumidification option (COIL:DX:MultiMode:CoolingEmpirical);
+//    Additional DESICCANT DEHUMIDIFIER:SOLID setpoint control option;
+// American Society of Heating Refrigerating and Air-Conditioning Engineers, Inc,,
+// 1791 Tullie Circle, N.E., Atlanta, GA 30329. www.ashrae.org
+// Work performed by GARD Analytics, Inc., 1028 Busse Highway, Park Ridge, Illinois 60068-1802, USA.
+// www.gard.com, November 2004.
+
+// EnergyPlus v1.2.2 and later versions (wintel platform) contains links to SPARK, a simulation engine for
+// detailed system modeling developed at Ernest Orlando Lawrence Berkeley National Laboratory in
+// conjunction with Ayres Sowell Associates, Inc.  SPARK was removed in V3.1 - April 2009 release.
+
+// The Ecoroof (Green Roof) model, first introduced in EnergyPlus v2.0, was developed at Portland State University,
+// by David Sailor and his students. It is based on the FASST vegetation models developed by Frankenstein and
+// Koenig for the US Army Corps of Engineers.
+
+// The HAMT (Heat And Moisture Transfer) model, first introduced in EnergyPlus v3.0.0 was developed by Phillip Biddulph,
+// Complex Built Environment Systems, The Bartlett School of Graduate Studies, University College London, Gower Street,
+// London WC1E 6BT, United Kingdom. http://www.cbes.ucl.ac.uk/.
+
+// The SQLite output module, first introduced in EnergyPlus v3.0.0, was developed by Gregory B. Stark, P.E.,
+// Building Synergies, LLC, 1860 Washington Street, Suite 208, Denver, Colorado 80203, United States.
+// http://www.buildingsynergies.com/
+
+// Refrigeration compressor performance data and refrigeration practices were provided by CDH Energy, Cazenovia, NY 12035.
+
+// Other Acknowledgments
+
+// This work was supported by the Assistant Secretary for Energy Efficiency and Renewable Energy, Office
+// of Building Technologies Program of the US Department of Energy.
+
+// Additional support was provided by the Gas Technology Institute and the California Energy Commission.
+
+// The ice thermal storage module development was supported by the U.S. Department of Energy Office of
+// Electricity Delivery and Energy Reliability.
+
+// The HAMT (Heat And Moisture Transfer) model was supported by the Engineering and Physical Sciences Research Council (EPSRC),
+// the UK government agency for funding research and training in engineering and the physical sciences.
+
+// The SQLite output module was funded by Building Synergies, LLC and was made possible by inclusion of software code
+// from the SQLite project (http://www.sqlite.org/).
+
 #ifdef _WIN32
 #include <Windows.h>
 #endif
@@ -48,149 +175,6 @@ EnergyPlusPgm( std::string const & filepath )
 {
 	// Using/Aliasing
 	using namespace EnergyPlus;
-
-	//      NOTICE
-
-	//     Copyright (c) 1996-2015 The Board of Trustees of the University of Illinois
-	//      University of California through Ernest Orlando Lawrence Berkeley National Laboratory.  All rights
-	//      reserved.
-
-	//      Portions of the EnergyPlus(tm) software package have been developed and copyrighted by other
-	//      individuals, companies and institutions.  These portions have been incorporated into the EnergyPlus
-	//      software package under license.
-
-	//      In addition to the primary authorship of the LBNL Simulation Research Group (
-	//      http://simulationresearch.lbl.gov/) and the UIUC Building Systems Laboratory (
-	//      http://www.bso.uiuc.edu/), the following have contributed to EnergyPlus V1.0:
-
-	//      Portions of the EnergyPlus weather processor were developed by US Department of Energy, Building
-	//      Technologies Program, www.energyplus.gov
-
-	//      Portions of the input processing, output processing, weather processor, BLAST Translator were
-	//      developed by US Army Corps of Engineers, Construction Engineering Research Laboratories, 2902 Newmark
-	//      Drive, Champaign IL  61821. www.cecer.army.mil
-
-	//      Portions of this software package were developed by Linda Lawrie of DHL Consulting LLC.
-
-	//      Portions of this software package were developed by C.O. Pedersen Associates.
-
-	//      Portions of the EnergyPlus utility software (EP-Launch, IDFEditor, DOE2Translator, HVAC-Diagram,
-	//      ExpandObjects, CSVProc, System Templates, and convertESOMTR) were developed by GARD Analytics, Inc.
-	//      1028 Busse Highway, Park Ridge, Illinois 60068-1802, USA (847) 698-5690, www.gard.com.  GARD Analytics
-	//      performed independent verification and validation testing of the software after developing the testing
-	//      strategy and plan.  GARD Analytics was also responsible for gas absorption chiller, desiccant
-	//      dehumidifier, ice storage (simple), table reports and economics.
-
-	//      Portions of flow resolver, chiller models (absorption, electric, const cop, engine-driven, gas-
-	//      turbine), generator models (diesel electric, gas turbine), furnace models, heat recovery loop, plant
-	//      loop, plant condenser loop, air-change dependent inside film coefficients were developed by Oklahoma
-	//      State University, 110 Engineering North, Stillwater, OK 74078.
-
-	//      Portions of EnergyPlus related to the models for EMPD moisture calculations, DX coils, furnace/unitary
-	//      systems, cooling towers, air-to-air heat pumps, air distribution systems, refrigerated cases, electric
-	//      EIR chillers, packaged terminal heat pumps, desuperheater air and water heating coils, and heat pump
-	//      water heaters were developed by University of Central Florida, Florida Solar Energy Center (FSEC),
-	//      1679 Clearlake Road, Cocoa, FL  32922, www.fsec.ucf.edu/.
-
-	//      Portions of EnergyPlus were developed by the National Renewable Energy Laboratory (NREL), 1617 Cole
-	//      Blvd, Golden, CO 80401.
-
-	//      EnergyPlus v1.0.1, v1.0.2, v1.0.3, v1.1, v1.1.1 (Wintel platform) includes a link to TRNSYS (The Transient
-	//      Energy System Simulation Tool) for photovoltaic calculations developed by Thermal Energy System Specialists,
-	//      2916 Marketplace Drive, Suite 104, Madison, WI 53719; Tel: (608) 274-2577. EnergyPlus v1.2 and later
-	//      includes Photovoltaic calculations implemented in EnergyPlus by Thermal Energy System Specialists.
-	//      This model was originally developed by Oystein Ulleberg, Institute for Energy Technology, Norway -- based on
-	//      the Duffie and Beckman equivalent one-diode model.
-
-	//      Portions of this software package that convert certain stand-alone heat transfer models for slab-on-
-	//      grade and basement foundations were developed by William Bahnfleth, Cynthia Cogil, and Edward
-	//      Clements, Department of Architectural Engineering, Pennsylvania State University, 224 Engineering Unit
-	//      A, University Park, Pennsylvania  16802-1416, (814) 863-2076.
-
-	//      The concept and initial implementation for the EnergyPlus COM/DLL version (Wintel platform) was made
-	//      possible through cooperation with DesignBuilder Software, Ltd, Andy Tindale - an EnergyPlus
-	//      collaborative developer.
-
-	//      The thickness, conductivity, density and specific heat values of the material layers for the
-	//      constructions in the Composite Wall Construction reference data set have been taken from the ASHRAE
-	//      report "Modeling Two- and Three-Dimensional Heat Transfer through Composite Wall and Roof Assemblies
-	//      in Hourly  Energy Simulation Programs (1145-TRP)," by Enermodal Engineering Limited, Oak Ridge
-	//      National Laboratory, and the Polish Academy of Sciences, January 2001.
-
-	//      EnergyPlus v1.2 contains DELight2 (wintel platform), a simulation engine for daylighting and electric
-	//      lighting system analysis developed at Ernest Orlando Lawrence Berkeley National Laboratory.
-
-	//      Portions of the EnergyPlus v1.2 air distribution system calculations were written by George Walton of
-	//      the National Institute for Standards and Technology (NIST), 100 Bureau Drive, Gaithersburg, MD 20899,
-	//      (301) 975-6478.  The EnergyPlus AirflowNetwork model also includes portions of an early version of COMIS
-	//      (Conjunction Of Multizone Infiltration Specialists) developed by a multinational, multi-institutional
-	//      effort under the auspices of the International Energy Agency's Buildings and Community Systems Agreement
-	//      working group focusing on multizone air flow modeling (Annex 23) and now administered by the Swiss Federal
-	//      Laboratories for Materials Testing and Research (EMPA), Division 175, Uberlandstrasse 129, CH-8600 Dubendorf,
-	//      Switzerland.
-
-	//      The EnergyPlus v1.2 model for displacement ventilation and cross-ventilation was developed
-	//      by Guilherme Carrilho da Graca and Paul Linden of the Department of Mechanical and Aerospace
-	//      Engineering, University of California, San Diego.
-
-	//      The EnergyPlus models for UFAD served zones were developed by Anna Liu and Paul Linden at the Department
-	//      of Mechanical and Aerospace Engineering, University of California, San Diego.
-
-	//      ASHRAE research project 1254-RP supported the development of the following features first added in
-	//      EnergyPlus v1.2.2:
-	//         DXSystem:AirLoop enhancements (valid as OA system equipment, new humidity control options);
-	//         New set point managers: SET POINT MANAGER:SINGLE ZONE HEATING, SET POINT MANAGER:SINGLE ZONE COOLING,
-	//                 and SET POINT MANAGER:OUTSIDE AIR PRETREAT;
-	//         New 2-stage DX coil with enhanced dehumidification option (COIL:DX:MultiMode:CoolingEmpirical);
-	//         Additional DESICCANT DEHUMIDIFIER:SOLID setpoint control option;
-	//      American Society of Heating Refrigerating and Air-Conditioning Engineers, Inc,,
-	//      1791 Tullie Circle, N.E., Atlanta, GA 30329. www.ashrae.org
-	//      Work performed by GARD Analytics, Inc., 1028 Busse Highway, Park Ridge, Illinois 60068-1802, USA.
-	//      www.gard.com, November 2004.
-
-	//      EnergyPlus v1.2.2 and later versions (wintel platform) contains links to SPARK, a simulation engine for
-	//      detailed system modeling developed at Ernest Orlando Lawrence Berkeley National Laboratory in
-	//      conjunction with Ayres Sowell Associates, Inc.  SPARK was removed in V3.1 - April 2009 release.
-
-	//      The Ecoroof (Green Roof) model, first introduced in EnergyPlus v2.0, was developed at Portland State University,
-	//      by David Sailor and his students. It is based on the FASST vegetation models developed by Frankenstein and
-	//      Koenig for the US Army Corps of Engineers.
-
-	//      The HAMT (Heat And Moisture Transfer) model, first introduced in EnergyPlus v3.0.0 was developed by Phillip Biddulph,
-	//      Complex Built Environment Systems, The Bartlett School of Graduate Studies, University College London, Gower Street,
-	//      London WC1E 6BT, United Kingdom. http://www.cbes.ucl.ac.uk/.
-
-	//      The SQLite output module, first introduced in EnergyPlus v3.0.0, was developed by Gregory B. Stark, P.E.,
-	//      Building Synergies, LLC, 1860 Washington Street, Suite 208, Denver, Colorado 80203, United States.
-	//      http://www.buildingsynergies.com/
-
-	//      Refrigeration compressor performance data and refrigeration practices were provided by CDH Energy, Cazenovia, NY 12035.
-
-	//      NOTICE: The U.S. Government is granted for itself and others acting on its behalf a paid-up,
-	//      nonexclusive, irrevocable, worldwide license in this data to reproduce, prepare derivative works, and
-	//      perform publicly and display publicly.  Beginning five (5) years after permission to assert copyright
-	//      is granted, subject to two possible five year renewals, the U.S. Government is granted for itself and
-	//      others acting on its behalf a paid-up, non-exclusive, irrevocable worldwide license in this data to
-	//      reproduce, prepare derivative works, distribute copies to the public, perform publicly and display
-	//      publicly, and to permit others to do so.
-
-	//      TRADEMARKS: EnergyPlus is a trademark of the US Department of Energy.
-
-	//      Other Acknowledgments
-
-	//      This work was supported by the Assistant Secretary for Energy Efficiency and Renewable Energy, Office
-	//      of Building Technologies Program of the US Department of Energy.
-
-	//      Additional support was provided by the Gas Technology Institute and the California Energy Commission.
-
-	//      The ice thermal storage module development was supported by the U.S. Department of Energy Office of
-	//      Electricity Delivery and Energy Reliability.
-
-	//      The HAMT (Heat And Moisture Transfer) model was supported by the Engineering and Physical Sciences Research Council (EPSRC),
-	//      the UK government agency for funding research and training in engineering and the physical sciences.
-
-	//      The SQLite output module was funded by Building Synergies, LLC and was made possible by inclusion of software code
-	//      from the SQLite project (http://www.sqlite.org/).
 
 	// PROGRAM INFORMATION:
 	//       AUTHOR         Linda K. Lawrie, et al
