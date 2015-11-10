4d53cc709e5a034869b0144b2ea4ea5afcf02c0b
Michael J. Witte <mjwitte@gard.com>
2015-11-09 23:04:43 -0600

    Fix outputchanges file name

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -143,7 +143,7 @@ install_remote(FILES "https://raw.github.com/NREL/EnergyPlusBuildSupport/v8.5.0/
 install_remote(FILES "https://raw.github.com/NREL/EnergyPlusBuildSupport/v8.5.0/release/ObjectStatus.xls" "./")
 install_remote(FILES "https://raw.github.com/NREL/EnergyPlusBuildSupport/v8.5.0/release/readme.html" "./" "readme.html" TRUE)
 install(FILES "${CMAKE_SOURCE_DIR}/src/Transition/InputRulesFiles/${RULES_XLS}" DESTINATION "./")
-install(FILES "${CMAKE_SOURCE_DIR}/src/Transition/OutputRulesFiles/OutputChanges8-4-0-to-5-5-0.txt" DESTINATION "./")
+install(FILES "${CMAKE_SOURCE_DIR}/src/Transition/OutputRulesFiles/OutputChanges8-4-0-to-8-5-0.txt" DESTINATION "./")
 install_remote(FILES "https://raw.github.com/NREL/EnergyPlusBuildSupport/v8.5.0/bin/CurveFitTools/IceStorageCurveFitTool.xlsm" "PreProcess/HVACCurveFitTool/")
 install_remote(FILES "https://raw.github.com/NREL/EnergyPlusBuildSupport/v8.5.0/release/Report%20Variables%208-4-0%20to%208-5-0.csv" "PreProcess/IDFVersionUpdater/" "Report Variables 8-4-0 to 8-5-0.csv")
 install(FILES "${CMAKE_SOURCE_DIR}/idd/V8-4-0-Energy+.idd" DESTINATION "PreProcess/IDFVersionUpdater/")
