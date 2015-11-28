8e176571ef06770466b6f3d8165e34f22a3af490
Richard Raustad <rraustad@fsec.ucf.edu>
2015-11-28 14:14:30 -0500

    Added new node test in BranchNodeConnections

--- src/EnergyPlus/BranchNodeConnections.cc
+++ src/EnergyPlus/BranchNodeConnections.cc
@@ -269,6 +269,10 @@ namespace BranchNodeConnections {
 		//        same node name appears as an INLET to an AirLoopHVAC, CondenserLoop, or PlantLoop.
 		// 6.  Any given node can only be an inlet once in the list of Non-Parent Node Connections
 		// 7.  Any given node can only be an outlet once in the list of Non-Parent Node Connections
+		// 8.  non-parent outlet nodes -- must never be an outlet more than once
+		// 9.  nodes of type OutsideAirReference must be registered as NodeConnectionType_OutsideAir
+		// 10. fluid streams cannot have multiple inlet/outlet nodes on same component
+		// 11. AirLoopHVAC:ReturnPlenum zone node name must be unique
 
 		// METHODOLOGY EMPLOYED:
 		// Needs description, as appropriate.
@@ -616,6 +620,27 @@ namespace BranchNodeConnections {
 			FluidStreamOutletCount.deallocate();
 			FluidStreamCounts.deallocate();
 			NodeObjects.deallocate();
+		}
+
+		// Check 11 -- AirLoopHVAC:ReturnPlenum zone node name must be unique
+		for( Loop1 = 1; Loop1 <= NumOfNodeConnections; ++Loop1 ) {
+			if( NodeConnections( Loop1 ).ConnectionType != ValidConnectionTypes( NodeConnectionType_ZoneNode ) ) continue;
+			if( NodeConnections( Loop1 ).ObjectType != "AIRLOOPHVAC:RETURNPLENUM" ) continue;
+			MatchedAtLeastOne = false;
+			for( Loop2 = 1; Loop2 <= NumOfNodeConnections; ++Loop2 ) {
+				if( Loop1 == Loop2 ) continue;
+				if( NodeConnections( Loop1 ).NodeNumber != NodeConnections( Loop2 ).NodeNumber ) continue;
+				if( NodeConnections( Loop2 ).ConnectionType == ValidConnectionTypes( NodeConnectionType_ZoneNode ) ) {
+					MatchedAtLeastOne = true;
+					continue;
+				}
+			}
+			if( MatchedAtLeastOne ) {
+				ShowSevereError( "Node Connection Error, Node=\"" + NodeConnections( Loop1 ).NodeName + "\", Zone node name used in AirloopHVAC:ReturnPlenum is not unique." );
+				ShowContinueError( "Reference Object=" + NodeConnections( Loop1 ).ObjectType + ", Name=" + NodeConnections( Loop1 ).ObjectName );
+				++ErrorCounter;
+				ErrorsFound = true;
+			}
 		}
 
 		NumNodeConnectionErrors += ErrorCounter;
