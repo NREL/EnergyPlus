19c79dfd84f43a2c0b9651a255630974ef2aab1f
Thierry Nouidui <tsnouidui@lbl.gov>
2015-11-30 09:46:49 -0800

    Remove header definition which happens in .h file.

--- third_party/BCVTB/utilSocket.h
+++ third_party/BCVTB/utilSocket.h
@@ -134,7 +134,9 @@ derivative works thereof, in binary and source code form.
 #include "defines.h"
 
 extern FILE *f1;
-#define HEADER_LENGTH 54 // =10 + 4*(10+1);
+// FIX: Increase length of  
+// buffer for EnergyPlusToFMU 
+#define HEADER_LENGTH 21600 
 extern int REQUIRED_READ_LENGTH;
 extern int REQUIRED_WRITE_LENGTH;
 
