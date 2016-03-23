8911b6ed954a3a0e9073bf54f4b03d41551b19e9
Edwin Lee <leeed2001@gmail.com>
2016-03-23 07:06:16 -0600

    Rework CMake rules for documentation

--- cmake/BuildDocumentation.cmake
+++ cmake/BuildDocumentation.cmake
@@ -2,7 +2,11 @@
   # XELATEX, the path to the xelatex compiler
   # INNAME, the name of the input tex file
   # OUTNAME, the pretty output name for the pdf
-# this will get more complicated as we add things like bibtex and makeindex, for now just execute xelatex twice and rename the output
+  # ORIGINAL_CMAKE_SOURCE_DIR, the root of the source repo
+  # ORIGINAL_CMAKE_BINARY_DIR, the root fo the build tree
+# this will get more complicated as we add things like bibtex and makeindex, for now just copy the files into the build tree, execute xelatex twice and rename the output
+#message("Hoping to copy from ${ORIGINAL_CMAKE_SOURCE_DIR}/doc/${INNAME} to ${ORIGINAL_CMAKE_BINARY_DIR}/doc-build/${INNAME}")
+execute_process( COMMAND cmake -E copy_directory ${ORIGINAL_CMAKE_SOURCE_DIR}/doc/${INNAME} ${ORIGINAL_CMAKE_BINARY_DIR}/doc-build/${INNAME})
 execute_process( COMMAND ${XELATEX} -interaction=nonstopmode ${INNAME}.tex )
 execute_process( COMMAND ${XELATEX} -interaction=nonstopmode ${INNAME}.tex )
 file( RENAME "${INNAME}.pdf" "../${OUTNAME}.pdf" )
