ed0aaa22919706b6ee4ae6ef3acd6d8d5b544f6c
Thierry Nouidui <tsnouidui@lbl.gov>
2016-04-26 13:27:16 -0700

    Minor typo.

--- src/EnergyPlus/RuntimeLanguageProcessor.cc
+++ src/EnergyPlus/RuntimeLanguageProcessor.cc
@@ -2789,7 +2789,7 @@ namespace RuntimeLanguageProcessor {
 					for ( ErlVarLoop = 1; ErlVarLoop <= NumAlphas; ++ErlVarLoop ) {
 						if ((cCurrentModuleObject.compare("ExternalInterface:FunctionalMockupUnitImport:To:Variable") == 0)){
 							if (ErlVarLoop == 1){
-								// Only validate first field of object ExternalInterface:FunctionalMockupUnitExport:To:Variable.
+								// Only validate first field of object ExternalInterface:FunctionalMockupUnitImport:To:Variable.
 								// This object is allowed to contain fields that do not need to be valid EMS fields (e.g. path to the FMU).
 								ValidateEMSVariableName(cCurrentModuleObject, cAlphaArgs(ErlVarLoop), cAlphaFieldNames(ErlVarLoop), errFlag, ErrorsFound);
 							}
