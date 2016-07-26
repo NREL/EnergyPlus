b2c618bf5392c2fb0373970f4fb965ddba4638fb
rglazer2 <ryan@glazer350.com>
2016-07-25 22:58:47 -0500

    Addresses issue 4126_MismatchedPlantLoopVolume. Separated out from earlier branch

--- src/EnergyPlus/HVACInterfaceManager.cc
+++ src/EnergyPlus/HVACInterfaceManager.cc
@@ -701,6 +701,20 @@ namespace HVACInterfaceManager {
 		//update last tank outlet temperature
 		PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).TempInterfaceTankOutlet = TankFinalTemp;
 
+		//update heat trasport and heat storage rates
+		PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_MdotCpDeltaT = ( TankInletTemp - TankFinalTemp ) * Cp * MassFlowRate;
+		PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_McpDTdt = ( ThisTankMass * Cp * ( TankFinalTemp - LastTankOutletTemp ))/ TimeStepSeconds;
+		
+		//Determine excessive storage
+		if (PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_MdotCpDeltaT < PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_McpDTdt) {
+			PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_CapExcessStorageTimeReport = TimeStepSys;
+			PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_CapExcessStorageTime += TimeStepSys;
+		}
+		else {
+			PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_CapExcessStorageTimeReport = 0;
+		}
+		PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_TotalTime += TimeStepSys;
+
 		// update report variable
 		PlantLoop( LoopNum ).LoopSide( TankOutletLoopSide ).LoopSideInlet_TankTemp = TankAverageTemp;
 
