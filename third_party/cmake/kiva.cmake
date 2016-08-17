6f421d709f41323176c2c025b1f07d1e59af8264
Neal Kruis <neal.kruis@bigladdersoftware.com>
2016-08-17 12:41:59 -0600

    Link EnergyPlus to Kiva.

---
+++ third_party/cmake/kiva.cmake
@@ -0,0 +1,11 @@
+include_directories("${CMAKE_SOURCE_DIR}/third_party/kiva/src")
+include_directories("${CMAKE_BINARY_DIR}/third_party/kiva/src/libkiva")
+include_directories("${CMAKE_SOURCE_DIR}/third_party/kiva/vendor/boost-1.61.0/")
+include_directories("${CMAKE_SOURCE_DIR}/third_party/kiva/vendor/lis-1.5.66/include/")
+
+configure_file("${CMAKE_SOURCE_DIR}/third_party/cmake/CMakeLists-kiva.txt" "${CMAKE_BINARY_DIR}/third_party/cmake/CMakeLists.txt")
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/third_party/cmake)
+execute_process(COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/third_party/cmake)
+
+add_subdirectory("${CMAKE_SOURCE_DIR}/third_party/kiva/src/libkiva")
+add_subdirectory("${CMAKE_SOURCE_DIR}/third_party/kiva/vendor/lis-1.5.66/")
