36be4f81212e065634b10e7c835b5c16c019cc6b
Simon Vidanovic <dvvidanovic@lbl.gov>
2016-09-16 11:15:28 -0700

    Fixed gcc and clang compilation errors

--- src/EnergyPlus/WindowModel.hh
+++ src/EnergyPlus/WindowModel.hh
@@ -14,7 +14,7 @@ namespace EnergyPlus {
     EnumParser() {};
 
     T StringToEnum( const std::string &value ) {
-      map< std::string, T >::const_iterator iValue = m_Map.find( value );
+      std::map< std::string, T >::const_iterator iValue = m_Map.find( value );
       if( iValue == m_Map.end() )
         throw std::runtime_error( "Incorrect enumerator assigned." );
       return iValue->second;
