a6cfcf4e811cfaaee8159d66f246cb51f61c48c2
XuanLuoLBL <xuanluo@lbl.gov>
2017-01-20 17:33:35 -0800

    Revert "Thermostat based on Adaptive Thermal Comfort Model "

--- src/EnergyPlus/ZoneTempPredictorCorrector.hh
+++ src/EnergyPlus/ZoneTempPredictorCorrector.hh
@@ -186,25 +186,6 @@ namespace ZoneTempPredictorCorrector {
 
 	};
 
-	struct AdaptiveComfortDailySetPointSchedule
-	{
-		// Members
-		bool initialized;
-		Array1D< Real64 > ThermalComfortAdaptiveASH55_Upper_90;
-		Array1D< Real64 > ThermalComfortAdaptiveASH55_Upper_80;
-		Array1D< Real64 > ThermalComfortAdaptiveASH55_Central;
-		Array1D< Real64 > ThermalComfortAdaptiveCEN15251_Upper_I;
-		Array1D< Real64 > ThermalComfortAdaptiveCEN15251_Upper_II;
-		Array1D< Real64 > ThermalComfortAdaptiveCEN15251_Upper_III;
-		Array1D< Real64 > ThermalComfortAdaptiveCEN15251_Central;
-
-		// Default Constructor
-		AdaptiveComfortDailySetPointSchedule() :
-			initialized( false )
-		{}
-	};
-
-
 	// Object Data
 	extern Array1D< ZoneTempControlType > SetPointSingleHeating;
 	extern Array1D< ZoneTempControlType > SetPointSingleCooling;
@@ -214,8 +195,6 @@ namespace ZoneTempPredictorCorrector {
 	extern Array1D< ZoneComfortFangerControlType > SetPointSingleCoolingFanger;
 	extern Array1D< ZoneComfortFangerControlType > SetPointSingleHeatCoolFanger;
 	extern Array1D< ZoneComfortFangerControlType > SetPointDualHeatCoolFanger;
-	extern AdaptiveComfortDailySetPointSchedule AdapComfortDailySetPointSchedule;
-	extern Array1D< Real64 > AdapComfortSetPointSummerDesDay;
 
 	// Functions
 	void
@@ -245,9 +224,6 @@ namespace ZoneTempPredictorCorrector {
 
 	void
 	CalcZoneAirTempSetPoints();
-
-	void
-	CalculateAdaptiveComfortSetPointSchl();
 
 	void
 	CalcPredictedSystemLoad( int const ZoneNum, Real64 RAFNFrac );
@@ -335,13 +311,6 @@ namespace ZoneTempPredictorCorrector {
 
 	void
 	AdjustAirSetPointsforOpTempCntrl(
-		int const TempControlledZoneID,
-		int const ActualZoneNum,
-		Real64 & ZoneAirSetPoint
-	);
-
-	void
-	AdjustOperativeSetPointsforAdapComfort(
 		int const TempControlledZoneID,
 		int const ActualZoneNum,
 		Real64 & ZoneAirSetPoint
