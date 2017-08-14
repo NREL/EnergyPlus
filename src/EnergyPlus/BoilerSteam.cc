58f94ee34b09bb3d8741e0af053b844de014268b
Matt Mitchell <matt.s.mitchell@okstate.edu>
2017-08-14 13:52:23 -0500

    Boiler:Steam add defaults in idd. Add warning message to getInput for units on Maximum Operation Pressure field.

--- src/EnergyPlus/BoilerSteam.cc
+++ src/EnergyPlus/BoilerSteam.cc
@@ -383,6 +383,10 @@ namespace BoilerSteam {
 
 			// INPUTS from the IDF file
 			Boiler( BoilerNum ).BoilerMaxOperPress = rNumericArgs( 1 );
+			if ( Boiler( BoilerNum ).BoilerInletNodeNum < 10e5 ) {
+				ShowWarningMessage( cCurrentModuleObject + "=\"" + cAlphaArgs( 1 ) + "\"" );
+				ShowContinueError( "Field: Maximum Operation Pressure units are Pa. Verify units." );
+			}
 			Boiler( BoilerNum ).Effic = rNumericArgs( 2 );
 			Boiler( BoilerNum ).TempUpLimitBoilerOut = rNumericArgs( 3 );
 			Boiler( BoilerNum ).NomCap = rNumericArgs( 4 );
