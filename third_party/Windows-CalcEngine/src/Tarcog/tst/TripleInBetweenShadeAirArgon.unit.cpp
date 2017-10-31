a1634f7190c7423bc4d24087f276552de8274a33
Simon Vidanovic <dvvidanovic@lbl.gov>
2017-10-31 12:54:58 -0700

    Another cpp_check update

--- third_party/Windows-CalcEngine/src/Tarcog/tst/TripleInBetweenShadeAirArgon.unit.cpp
+++ third_party/Windows-CalcEngine/src/Tarcog/tst/TripleInBetweenShadeAirArgon.unit.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <stdexcept>
+#include <string>
 #include <gtest/gtest.h>
 
 #include "WCEGases.hpp"
@@ -78,14 +79,16 @@ protected:
 		CIntCoeff AirCp{ 1.002737e+03, 1.2324e-02, 0.0 };
 		CIntCoeff AirVisc{ 3.7233e-06, 4.94e-08, 0.0 };
 
-		CGasData AirData = { "Air", 28.97, 1.4, AirCp, AirCon, AirVisc };
+		const std::string AirName = "Air";
+		CGasData AirData = { AirName , 28.97, 1.4, AirCp, AirCon, AirVisc };
 
 		// Create coefficients for Argon
 		CIntCoeff ArgonCon{ 2.2848e-03, 5.1486e-05, 0.0 };
 		CIntCoeff ArgonCp{ 5.21929e+02, 0.0, 0.0 };
 		CIntCoeff ArgonVisc{ 3.3786e-06, 6.4514e-08, 0.0 };
 
-		CGasData ArgonData{ "Argon", 39.948, 1.67, ArgonCp, ArgonCon, ArgonVisc };
+		const std::string ArgonName = "Argon";
+		CGasData ArgonData{ ArgonName, 39.948, 1.67, ArgonCp, ArgonCon, ArgonVisc };
 
 		CGasItem Air{ 0.1, AirData };
 		CGasItem Argon{ 0.9, ArgonData };
