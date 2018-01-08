296584902ac506015b45ad0552d4d99160261182
pshires <pshires10@gmail.com>
2018-01-08 15:38:56 -0500

    include threading libraries in CMakeLists file and add direct mkl unit test example

--- tst/EnergyPlus/unit/Eigen.unit.cc
+++ tst/EnergyPlus/unit/Eigen.unit.cc
@@ -45,13 +45,54 @@
 // OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#define min(x,y) (((x) < (y)) ? (x) : (y))
+
 // Google Test Headers
 #include <gtest/gtest.h>
 #define EIGEN_USE_MKL_ALL
 #include <Eigen/Dense>
+#include <mkl.h>
+#include <fstream>
 
 TEST(Eigen, EigenTest) {
-    Eigen::MatrixXd u = Eigen::MatrixXd::Random(500, 500);
-    Eigen::MatrixXd v = Eigen::MatrixXd::Random(500, 500);
+//    Eigen::MatrixXd u = Eigen::MatrixXd::Random(1000, 1000);
+//    Eigen::MatrixXd v = Eigen::MatrixXd::Random(1000, 1000);
+    Eigen::MatrixXd u = Eigen::MatrixXd::Random(2000, 200);
+    Eigen::MatrixXd v = Eigen::MatrixXd::Random(200, 1000);
+//    std::ofstream ofs("performanceTesting.txt");
+    auto const s_initial = dsecnd();
     Eigen::MatrixXd w = u * v;
+    auto const s_elapsed = (dsecnd() - s_initial) * 1000;
+//    ofs << "Eigen test: " << s_elapsed << " ms" << std::endl;
+    auto blah = 0;
+}
+
+TEST(EigenIntelMkl, MklTest) {
+    const int m = 2000, k = 200, n = 1000;
+    double alpha = 1.0, beta = 0.0;
+    double *A = (double *)mkl_malloc( m*k*sizeof( double ), 64 );
+    double *B = (double *)mkl_malloc( k*n*sizeof( double ), 64 );
+    double *C = (double *)mkl_malloc( m*n*sizeof( double ), 64 );
+
+    for (int i = 0; i < (m*k); i++) {
+        A[i] = (double)(i+1);
+    }
+    for (int i = 0; i < (k*n); i++) {
+        B[i] = (double)(-i-1);
+    }
+    for (int i = 0; i < (m*n); i++) {
+        C[i] = 0.0;
+    }
+
+//    printf (" Computing matrix product using Intel(R) MKL dgemm function via CBLAS interface \n\n");
+//    std::ofstream ofs("performanceTesting.txt");
+    auto const s_initial = dsecnd();
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                m, n, k, alpha, A, k, B, n, beta, C, n);
+
+    auto const s_elapsed = (dsecnd() - s_initial) * 1000;
+//    ofs << "Eigen test: " << s_elapsed << " ms" << std::endl;
+    mkl_free(A);
+    mkl_free(B);
+    mkl_free(C);
 }
