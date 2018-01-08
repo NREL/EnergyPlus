64387630f5214c2169bf3b712780efb6bb173f28
pshires <pshires10@gmail.com>
2018-01-08 17:44:02 -0500

    update eigen unit tests to contain sequential versions of each test case

--- tst/EnergyPlus/unit/Eigen.unit.cc
+++ tst/EnergyPlus/unit/Eigen.unit.cc
@@ -47,6 +47,7 @@
 
 #define min(x,y) (((x) < (y)) ? (x) : (y))
 
+#include <iostream>
 // Google Test Headers
 #include <gtest/gtest.h>
 #define EIGEN_USE_MKL_ALL
@@ -54,45 +55,76 @@
 #include <mkl.h>
 #include <fstream>
 
-TEST(Eigen, EigenTest) {
-//    Eigen::MatrixXd u = Eigen::MatrixXd::Random(1000, 1000);
-//    Eigen::MatrixXd v = Eigen::MatrixXd::Random(1000, 1000);
-    Eigen::MatrixXd u = Eigen::MatrixXd::Random(2000, 200);
-    Eigen::MatrixXd v = Eigen::MatrixXd::Random(200, 1000);
-//    std::ofstream ofs("performanceTesting.txt");
-    auto const s_initial = dsecnd();
-    Eigen::MatrixXd w = u * v;
-    auto const s_elapsed = (dsecnd() - s_initial) * 1000;
-//    ofs << "Eigen test: " << s_elapsed << " ms" << std::endl;
-    auto blah = 0;
-}
+//TEST(Eigen, EigenTest) {
+//    Eigen::MatrixXd u = Eigen::MatrixXd::Random(3000, 2300);
+//    Eigen::MatrixXd v = Eigen::MatrixXd::Random(2300, 3200);
+//    auto const initial = dsecnd();
+//    Eigen::MatrixXd w = u * v;
+//    auto const elapsed = dsecnd() - initial;
+//    std::cerr << "Eigen w/ IntelMKL: " << elapsed << " seconds" << std::endl;
+//}
 
-TEST(EigenIntelMkl, MklTest) {
-    const int m = 2000, k = 200, n = 1000;
-    double alpha = 1.0, beta = 0.0;
-    double *A = (double *)mkl_malloc( m*k*sizeof( double ), 64 );
-    double *B = (double *)mkl_malloc( k*n*sizeof( double ), 64 );
-    double *C = (double *)mkl_malloc( m*n*sizeof( double ), 64 );
+//TEST(EigenIntelMkl, MklTest) {
+//    const int m = 3000, k = 2300, n = 3200;
+//    double alpha = 1.0, beta = 0.0;
+//    double *A = (double *)mkl_malloc( m*k*sizeof( double ), 64 );
+//    double *B = (double *)mkl_malloc( k*n*sizeof( double ), 64 );
+//    double *C = (double *)mkl_malloc( m*n*sizeof( double ), 64 );
+//
+//    for (int i = 0; i < (m*k); i++) {
+//        A[i] = (double)rand();
+//    }
+//    for (int i = 0; i < (k*n); i++) {
+//        B[i] = (double)rand();
+//    }
+//    for (int i = 0; i < (m*n); i++) {
+//        C[i] = 0.0;
+//    }
+//    auto const sInitial = dsecnd();
+//    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+//                m, n, k, alpha, A, k, B, n, beta, C, n);
+//    auto const sElapsed = dsecnd() - sInitial;
+//    std::cerr << "IntelMKL: " << sElapsed << " seconds" << std::endl;
+//    mkl_free(A);
+//    mkl_free(B);
+//    mkl_free(C);
+//}
 
-    for (int i = 0; i < (m*k); i++) {
-        A[i] = (double)(i+1);
-    }
-    for (int i = 0; i < (k*n); i++) {
-        B[i] = (double)(-i-1);
-    }
-    for (int i = 0; i < (m*n); i++) {
-        C[i] = 0.0;
-    }
+//TEST(EigenIntelMkl, MklTestWithoutThreading) {
+//    mkl_set_num_threads(1); // for testing sequential
+//    const int m = 3000, k = 2300, n = 3200;
+//    double alpha = 1.0, beta = 0.0;
+//    double *A = (double *)mkl_malloc( m*k*sizeof( double ), 64 );
+//    double *B = (double *)mkl_malloc( k*n*sizeof( double ), 64 );
+//    double *C = (double *)mkl_malloc( m*n*sizeof( double ), 64 );
+//
+//    for (int i = 0; i < (m*k); i++) {
+//        A[i] = (double)rand();
+//    }
+//    for (int i = 0; i < (k*n); i++) {
+//        B[i] = (double)rand();
+//    }
+//    for (int i = 0; i < (m*n); i++) {
+//        C[i] = 0.0;
+//    }
+//
+//    auto const sInitial = dsecnd();
+//    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+//                m, n, k, alpha, A, k, B, n, beta, C, n);
+//
+//    auto const sElapsed = dsecnd() - sInitial;
+//    std::cerr << "IntelMKL w/o threading: " << sElapsed << " seconds" << std::endl;
+//    mkl_free(A);
+//    mkl_free(B);
+//    mkl_free(C);
+//}
 
-//    printf (" Computing matrix product using Intel(R) MKL dgemm function via CBLAS interface \n\n");
-//    std::ofstream ofs("performanceTesting.txt");
-    auto const s_initial = dsecnd();
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-                m, n, k, alpha, A, k, B, n, beta, C, n);
-
-    auto const s_elapsed = (dsecnd() - s_initial) * 1000;
-//    ofs << "Eigen test: " << s_elapsed << " ms" << std::endl;
-    mkl_free(A);
-    mkl_free(B);
-    mkl_free(C);
-}
+//TEST(Eigen, EigenTestWithoutThreading) {
+//    mkl_set_num_threads(1);
+//    Eigen::MatrixXd u = Eigen::MatrixXd::Random(3000, 2300);
+//    Eigen::MatrixXd v = Eigen::MatrixXd::Random(2300, 3200);
+//    auto const initial = dsecnd();
+//    Eigen::MatrixXd w = u * v;
+//    auto const elapsed = dsecnd() - initial;
+//    std::cerr << "Eigen w/IntelMKL w/o threading: " << elapsed << " seconds" << std::endl;
+//}
