e4dd3f5760744b50aefe169fd3428c008cf9e664
Xuan Luo <xuanluo@lbl.gov>
2018-02-07 12:49:06 -0500

    file header

--- tst/EnergyPlus/unit/SolarShading.unit.cc
+++ tst/EnergyPlus/unit/SolarShading.unit.cc
@@ -1,4 +1,4 @@
-﻿// EnergyPlus, Copyright (c) 1996-2018, The Board of Trustees of the University of Illinois,
+// EnergyPlus, Copyright (c) 1996-2018, The Board of Trustees of the University of Illinois,
 // The Regents of the University of California, through Lawrence Berkeley National Laboratory
 // (subject to receipt of any required approvals from the U.S. Dept. of Energy), Oak Ridge
 // National Laboratory, managed by UT-Battelle, Alliance for Sustainable Energy, LLC, and other
