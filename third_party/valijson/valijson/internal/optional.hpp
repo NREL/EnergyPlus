f02bade8546b69cdeae3af5805e33fc1ddb2fc6c
Mark Adams <adamsmb@ornl.gov>
2018-03-20 09:51:46 -0400

    Fix build error and a couple err file diffs

--- third_party/valijson/valijson/internal/optional.hpp
+++ third_party/valijson/valijson/internal/optional.hpp
@@ -3,12 +3,12 @@
 #define __VALIJSON_OPTIONAL_HPP
 
 // This should be removed once C++17 is widely available
-#if __has_include(<optional>) && __cplusplus >= 201703
-#  include <optional>
-namespace opt = std;
+#if __cplusplus >= 201703L && __has_include(<optional>)
+	#include <optional>
+	namespace opt = std;
 #else
-#  include <compat/optional.hpp>
-namespace opt = std::experimental;
+	#include <compat/optional.hpp>
+	namespace opt = std::experimental;
 #endif
 
 #endif
