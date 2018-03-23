6b341837cd73eb1acff4fbbc2dbd733323559519
Kyle Benne <kyle.benne@nrel.gov>
2018-03-22 22:55:29 -0500

    Update api

--- src/FMI/EPFMI.cpp
+++ src/FMI/EPFMI.cpp
@@ -5,9 +5,13 @@
 #include "../EnergyPlus/ZoneTempPredictorCorrector.hh"
 #include "../EnergyPlus/DataGlobals.hh"
 #include "../EnergyPlus/DataHeatBalFanSys.hh"
+#include "../EnergyPlus/DataHeatBalance.hh"
 #include <functional>
 #include <map>
 #include <memory>
+#include <string>
+#include <sstream>
+#include <algorithm>
 
 using namespace std::placeholders;
 
@@ -16,6 +20,16 @@ using namespace std::placeholders;
 std::map<unsigned int, std::function<void(double*)> > valueGetters;
 std::map<unsigned int, std::function<void(const double*)> > valueSetters;
 std::thread * epthread;
+
+const char ** g_parameterNames;
+const unsigned int * g_parameterValueReferences;
+size_t g_nPar;
+const char ** g_inputNames;
+const unsigned int * g_inputValueReferences;
+size_t g_nInp;
+const char ** g_outputNames;
+const unsigned int * g_outputValueReferences;
+size_t g_nOut;
 
 void noGetter(double*) {
   // This Getter is not implemented
@@ -39,20 +53,79 @@ void getZoneH(double* h, int zoneNum) {
   *h = EnergyPlus::ZoneTempPredictorCorrector::HDot(zoneNum);
 }
 
+// Pair of strings, first is a zone name, second is a variable name
+typedef std::pair<std::string, std::string> NamePair;
+
+struct VarInfo {
+  std::string zoneName;
+  int zoneNum;
+  int varValueRef;
+  std::string varName;
+};
+
+int zoneIndex(const std::string & zoneName) {
+  std::string name(zoneName);
+  std::transform(name.begin(), name.end(),name.begin(), ::toupper);
+  for ( int i = 0; i < EnergyPlus::DataGlobals::NumOfZones; ++i ) {
+    if ( EnergyPlus::DataHeatBalance::Zone[i].Name == name ) {
+      return i + 1;
+    }
+  }
+
+  return 0;
+}
+
+// Split a char * with zone name and variable name, 
+// separated by a ","
+VarInfo createVarInfo(const char * name, int varValueRef) {
+  NamePair pair;
+
+  std::vector<std::string> strings;
+  std::istringstream f(name);
+  std::string s;    
+  while (std::getline(f, s, ',')) {
+      strings.push_back(s);
+  }
+
+  VarInfo info;
+  info.zoneName = strings[0];
+  info.varName = strings[1];
+  info.zoneNum = zoneIndex(info.zoneName);
+  info.varValueRef = varValueRef;
+
+  return info;
+}
+
 unsigned int instantiate(const char *input,
                          const char *weather,
                          const char *idd,
                          const char *instanceName,
-                         const char** varNames,
-                         double* varPointers[],
-                         size_t nVars,
+                         const char ** parameterNames,
+                         const unsigned int parameterValueReferences[],
+                         size_t nPar,
+                         const char ** inputNames,
+                         const unsigned int inputValueReferences[],
+                         size_t nInp,
+                         const char ** outputNames,
+                         const unsigned int outputValueReferences[],
+                         size_t nOut,
                          const char *log)
 {
   UNUSED(instanceName);
-  UNUSED(varNames);
-  UNUSED(varPointers);
-  UNUSED(nVars);
   UNUSED(log);
+
+  g_parameterNames = parameterNames;
+  g_parameterValueReferences = parameterValueReferences;
+  g_nPar = nPar;
+  g_inputNames = inputNames;
+  g_inputValueReferences = inputValueReferences;
+  g_nInp = nInp;
+  g_outputNames = outputNames;
+  g_outputValueReferences = outputValueReferences;
+  g_nOut = nOut;
+
+  for ( size_t i = 0; i < nPar; ++i ) {
+  }
 
   const int argc = 6;
   const char * argv[argc];
@@ -93,25 +166,28 @@ unsigned int setupExperiment(double tStart,
     time_cv.wait( lk, [](){ return epstatus == EPStatus::IDLE; } );
   }
 
-  // For the purpose of demonstration, assume a one zone model with the following valueReferences
-  // 10 = Temperature, T, of zone
-  // 11 = Enthalpy, H of zone 
+  for ( size_t i = 0; i < g_nInp; ++i ) {
+    const char * inputNamePair = g_inputNames[i];
+    const unsigned int inputValueRef = g_inputValueReferences[i];
 
-  int varsPerZone = 2;
-  for ( int z = 0; z < EnergyPlus::DataGlobals::NumOfZones; ++z ) {
-    unsigned int zz = varsPerZone * z;
-    unsigned int valueReference;
-    unsigned int znumber = z + 1;
+    auto varInfo = createVarInfo(inputNamePair, inputValueRef);
 
-    // 10 = Zone Temperature
-    valueReference = 10;
-    valueGetters[zz + valueReference] = std::bind(getZoneTemperature, _1, znumber);
-    valueSetters[zz + valueReference] = std::bind(setZoneTemperature, _1, znumber);
+    if ( varInfo.varName == "T" ) {
+      valueSetters[varInfo.varValueRef] = std::bind(setZoneTemperature, _1, varInfo.zoneNum);
+    }
 
-    // 11 = Zone Enthalpy, H
-    valueReference = 11;
-    valueGetters[zz + valueReference] = std::bind(getZoneH, _1, znumber);
-    valueSetters[zz + valueReference] = noSetter;
+  }
+
+  for ( size_t i = 0; i < g_nOut; ++i ) {
+    const char * outputNamePair = g_outputNames[i];
+    const unsigned int outputValueRef = g_outputValueReferences[i];
+
+    auto varInfo = createVarInfo(outputNamePair, outputValueRef);
+
+    if ( varInfo.varName == "QConSen_flow" ) {
+      valueGetters[varInfo.varValueRef] = std::bind(getZoneH, _1, varInfo.zoneNum);
+    }
+
   }
 
   return 0;
--- src/FMI/Test/EPFMI_Test.cpp
+++ src/FMI/Test/EPFMI_Test.cpp
@@ -1,16 +1,30 @@
 #include <gtest/gtest.h>
 #include "../EPFMI.hpp"
-#include "config.hpp"
+#include "test-config.hpp"
+
 
 TEST( EPFMI, Alpha ) {
+
+const char * inputNames[] = {"Attic,T", "Core_ZN,T", "Perimeter_ZN_1,T", "Perimeter_ZN_2,T", "Perimeter_ZN_3,T", "Perimeter_ZN_4,T"};
+const unsigned int inputValueReferences[] = {0, 1, 2, 3, 4, 5,};
+
+const char * outputNames[] = {"Attic,QConSen_flow", "Core_ZN,QConSen_flow", "Perimeter_ZN_1,QConSen_flow", "Perimeter_ZN_2,QConSen_flow", "Perimeter_ZN_3,QConSen_flow", "Perimeter_ZN_4,QConSen_flow"};
+const unsigned int outputValueReferences[] = {6, 7, 8, 9, 10, 11};
+
   int result = instantiate(input, // input
                            weather, // weather
                            idd, // idd
                            "Alpha", // instanceName
-                           nullptr, // varNames
-                           nullptr, // varPointers
-                           0, // nVars
-                           nullptr); // log
+                           nullptr, // parameterNames
+                           nullptr, // parameterValueReferences[]
+                           0, // nPar
+                           inputNames, // inputNames
+                           inputValueReferences, // inputValueReferences[]
+                           6, // nInp
+                           outputNames, // outputNames
+                           outputValueReferences, // outputValueReferences[]
+                           6, // nOut
+                           nullptr); //log);
 
   double tStart = 0.0;
   bool stopTimeDefined = true;
@@ -21,22 +35,22 @@ TEST( EPFMI, Alpha ) {
   fmiEventInfo eventInfo;
   double time = tStart;
 
-  double outputs[] = {0.0};
-  const unsigned int outputRefs[] = {10, 11};
+  double outputs[] = {0.0, 0.0};
+  const unsigned int outputRefs[] = {6, 7};
 
-  double inputs[] = {21.0};
-  const unsigned int inputRefs[] = {10};
+  double inputs[] = {21.0, 21.0};
+  const unsigned int inputRefs[] = {0, 1};
 
   while ( time < tEnd ) {
     result = getNextEventTime(&eventInfo, nullptr);
     std::cout << "Current time: " << time << std::endl;
     std::cout << "Next event time: " << eventInfo.nextEventTime << std::endl;
 
-    result = setVariables(inputRefs, inputs, 1, nullptr);
+    result = setVariables(inputRefs, inputs, 2, nullptr);
 
     result = getVariables(outputRefs, outputs, 2, nullptr); 
-    std::cout << "Output 10 - T: " << outputs[0] << std::endl;
-    std::cout << "Output 11 - H: " << outputs[1] << std::endl;
+    std::cout << "Output 6 - Attic QConSen_flow: " << outputs[0] << std::endl;
+    std::cout << "Output 7 - Core_ZN QConSen_flow: " << outputs[1] << std::endl;
 
     time = eventInfo.nextEventTime;
     setTime(time, nullptr);
