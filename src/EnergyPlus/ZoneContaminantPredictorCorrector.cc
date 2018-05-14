b7bda4ecc34120a5b0d478c33c69ba00b6b6fa79
Lixing Gu <gu@fsec.ucf.edu>
2018-05-14 16:12:23 -0400

    Upload a fix

--- src/EnergyPlus/ZoneContaminantPredictorCorrector.cc
+++ src/EnergyPlus/ZoneContaminantPredictorCorrector.cc
@@ -1366,8 +1366,8 @@ namespace ZoneContaminantPredictorCorrector {
         if (allocated(ZoneEquipConfig) && MyConfigOneTimeFlag) {
             for (ContZoneNum = 1; ContZoneNum <= NumContControlledZones; ++ContZoneNum) {
                 ZoneNum = ContaminantControlledZone(ContZoneNum).ActualZoneNum;
-                for (int zoneInNode = 1; zoneInNode <= ZoneEquipConfig(ContZoneNum).NumInletNodes; ++zoneInNode) {
-                    int AirLoopNum = ZoneEquipConfig(ContZoneNum).InletNodeAirLoopNum(zoneInNode);
+                for (int zoneInNode = 1; zoneInNode <= ZoneEquipConfig(ZoneNum).NumInletNodes; ++zoneInNode) {
+                    int AirLoopNum = ZoneEquipConfig(ZoneNum).InletNodeAirLoopNum(zoneInNode);
                     ContaminantControlledZone(ContZoneNum).NumOfZones = 0;
                     for (Loop = 1; Loop <= NumOfZones; ++Loop) {
                         if (!ZoneEquipConfig(Loop).IsControlled) continue;
@@ -1665,27 +1665,41 @@ namespace ZoneContaminantPredictorCorrector {
                 ControlledCO2ZoneFlag = false;
                 // Check all the controlled zones to see if it matches the zone simulated
                 for (ContControlledZoneNum = 1; ContControlledZoneNum <= NumContControlledZones; ++ContControlledZoneNum) {
-                    if (GetCurrentScheduleValue(ContaminantControlledZone(ContControlledZoneNum).AvaiSchedPtr) > 0.0) {
-                        ZoneAirCO2SetPoint = ZoneCO2SetPoint(ContaminantControlledZone(ContControlledZoneNum).ActualZoneNum);
-                        if (ContaminantControlledZone(ContControlledZoneNum).EMSOverrideCO2SetPointOn) {
-                            ZoneAirCO2SetPoint = ContaminantControlledZone(ContControlledZoneNum).EMSOverrideCO2SetPointValue;
+                    if (ContaminantControlledZone(ContControlledZoneNum).ActualZoneNum == ZoneNum) {
+                        if (GetCurrentScheduleValue(ContaminantControlledZone(ContControlledZoneNum).AvaiSchedPtr) > 0.0) {
+                            ZoneAirCO2SetPoint = ZoneCO2SetPoint(ContaminantControlledZone(ContControlledZoneNum).ActualZoneNum);
+                            if (ContaminantControlledZone(ContControlledZoneNum).EMSOverrideCO2SetPointOn) {
+                                ZoneAirCO2SetPoint = ContaminantControlledZone(ContControlledZoneNum).EMSOverrideCO2SetPointValue;
+                            }
+                            ControlledCO2ZoneFlag = true;
+                            break;
                         }
-                        if (ContaminantControlledZone(ContControlledZoneNum).NumOfZones >= 1) {
-                            if (ContaminantControlledZone(ContControlledZoneNum).ActualZoneNum != ZoneNum) {
-                                for (I = 1; I <= ContaminantControlledZone(ContControlledZoneNum).NumOfZones; ++I) {
-                                    if (ContaminantControlledZone(ContControlledZoneNum).ControlZoneNum(I) == ZoneNum) {
-                                        ControlledCO2ZoneFlag = true;
-                                        break;
+                    }
+                }
+                if (!ControlledCO2ZoneFlag) {
+                    for (ContControlledZoneNum = 1; ContControlledZoneNum <= NumContControlledZones; ++ContControlledZoneNum) {
+                        if (GetCurrentScheduleValue(ContaminantControlledZone(ContControlledZoneNum).AvaiSchedPtr) > 0.0) {
+                            ZoneAirCO2SetPoint = ZoneCO2SetPoint(ContaminantControlledZone(ContControlledZoneNum).ActualZoneNum);
+                            if (ContaminantControlledZone(ContControlledZoneNum).EMSOverrideCO2SetPointOn) {
+                                ZoneAirCO2SetPoint = ContaminantControlledZone(ContControlledZoneNum).EMSOverrideCO2SetPointValue;
+                            }
+                            if (ContaminantControlledZone(ContControlledZoneNum).NumOfZones >= 1) {
+                                if (ContaminantControlledZone(ContControlledZoneNum).ActualZoneNum != ZoneNum) {
+                                    for (I = 1; I <= ContaminantControlledZone(ContControlledZoneNum).NumOfZones; ++I) {
+                                        if (ContaminantControlledZone(ContControlledZoneNum).ControlZoneNum(I) == ZoneNum) {
+                                            ControlledCO2ZoneFlag = true;
+                                            break;
+                                        }
                                     }
+                                    if (ControlledCO2ZoneFlag) break;
+                                } else {
+                                    ControlledCO2ZoneFlag = true;
+                                    break;
                                 }
-                                if (ControlledCO2ZoneFlag) break;
-                            } else {
-                                ControlledCO2ZoneFlag = true;
-                                break;
                             }
                         }
                     }
-                } // CO2ControlledZoneNum
+                }// CO2ControlledZoneNum
 
                 if (ControlledCO2ZoneFlag) {
                     // The density of air
--- tst/EnergyPlus/unit/ZoneContaminantPredictorCorrector.unit.cc
+++ tst/EnergyPlus/unit/ZoneContaminantPredictorCorrector.unit.cc
@@ -390,3 +390,220 @@ TEST_F(EnergyPlusFixture, ZoneContaminantPredictorCorrector_CorrectZoneContamina
     DataContaminantBalance::Contaminant.CO2Simulation = false;
     DataContaminantBalance::Contaminant.GenericContamSimulation = false;
 }
+
+TEST_F(EnergyPlusFixture, ZoneContaminantPredictorCorrector_MultiZoneCO2ControlTest)
+{
+
+    ShortenTimeStepSys = false;
+    UseZoneTimeStepHistory = false;
+
+    ZoneAirHumRat.allocate(3);
+    ZT.allocate(3);
+    MixingMassFlowZone.allocate(3);
+
+    DataGlobals::NumOfZones = 3;
+    DataContaminantBalance::Contaminant.CO2Simulation = true;
+ 
+    DataContaminantBalance::AZ.allocate(3);
+    DataContaminantBalance::BZ.allocate(3);
+    DataContaminantBalance::CZ.allocate(3);
+
+    DataContaminantBalance::CO2ZoneTimeMinus1Temp.allocate(3);
+    DataContaminantBalance::CO2ZoneTimeMinus2Temp.allocate(3);
+    DataContaminantBalance::CO2ZoneTimeMinus3Temp.allocate(3);
+    DataContaminantBalance::DSCO2ZoneTimeMinus1.allocate(3);
+    DataContaminantBalance::DSCO2ZoneTimeMinus2.allocate(3);
+    DataContaminantBalance::DSCO2ZoneTimeMinus3.allocate(3);
+    DataContaminantBalance::GCZoneTimeMinus1Temp.allocate(3);
+    DataContaminantBalance::GCZoneTimeMinus2Temp.allocate(3);
+    DataContaminantBalance::GCZoneTimeMinus3Temp.allocate(3);
+    DataContaminantBalance::DSGCZoneTimeMinus1.allocate(3);
+    DataContaminantBalance::DSGCZoneTimeMinus2.allocate(3);
+    DataContaminantBalance::DSGCZoneTimeMinus3.allocate(3);
+
+    DataContaminantBalance::MixingMassFlowCO2.allocate(3);
+    DataContaminantBalance::MixingMassFlowGC.allocate(3);
+    DataContaminantBalance::ZoneAirCO2Temp.allocate(3);
+    DataContaminantBalance::ZoneCO21.allocate(3);
+    DataContaminantBalance::ZoneAirCO2.allocate(3);
+    DataContaminantBalance::ZoneAirGCTemp.allocate(3);
+
+    DataContaminantBalance::ZoneCO2SetPoint.allocate(3);
+    DataContaminantBalance::CO2PredictedRate.allocate(3);
+
+    DataContaminantBalance::ZoneAirDensityCO.allocate(3);
+    DataContaminantBalance::ZoneCO2Gain.allocate(3);
+    DataContaminantBalance::ZoneGCGain.allocate(3);
+    DataContaminantBalance::ZoneCO2Gain(1) = 0.0001;
+    DataContaminantBalance::ZoneCO2Gain(2) = 0.0002;
+    DataContaminantBalance::ZoneCO2Gain(3) = 0.0003;
+    DataContaminantBalance::MixingMassFlowCO2(1) = 0.0;
+    DataContaminantBalance::MixingMassFlowCO2(2) = 0.0;
+    DataContaminantBalance::MixingMassFlowCO2(3) = 0.0;
+
+    DataContaminantBalance::DSCO2ZoneTimeMinus1(1) = 200.0;
+    DataContaminantBalance::DSCO2ZoneTimeMinus2(1) = 200.0;
+    DataContaminantBalance::DSCO2ZoneTimeMinus3(1) = 200.0;
+    DataContaminantBalance::DSCO2ZoneTimeMinus1(2) = 200.0;
+    DataContaminantBalance::DSCO2ZoneTimeMinus2(2) = 200.0;
+    DataContaminantBalance::DSCO2ZoneTimeMinus3(2) = 200.0;
+    DataContaminantBalance::DSCO2ZoneTimeMinus1(3) = 200.0;
+    DataContaminantBalance::DSCO2ZoneTimeMinus2(3) = 200.0;
+    DataContaminantBalance::DSCO2ZoneTimeMinus3(3) = 200.0;
+    DataContaminantBalance::OutdoorCO2 = 400.0;
+    DataContaminantBalance::ZoneCO21(1) = DataContaminantBalance::OutdoorCO2;
+    DataContaminantBalance::ZoneCO21(2) = DataContaminantBalance::OutdoorCO2;
+    DataContaminantBalance::ZoneCO21(3) = DataContaminantBalance::OutdoorCO2;
+    DataContaminantBalance::ZoneCO2SetPoint(1) = 450.0;
+    DataContaminantBalance::ZoneCO2SetPoint(2) = 500.0;
+    DataContaminantBalance::ZoneCO2SetPoint(3) = 550.0;
+    DataContaminantBalance::ZoneAirCO2(1) = DataContaminantBalance::ZoneCO21(1);
+    DataContaminantBalance::ZoneAirCO2(2) = DataContaminantBalance::ZoneCO21(2);
+    DataContaminantBalance::ZoneAirCO2(3) = DataContaminantBalance::ZoneCO21(3);
+
+    Real64 PriorTimeStep;
+
+    TimeStepSys = 15.0 / 60.0; // System timestep in hours
+    PriorTimeStep = TimeStepSys;
+
+    ZoneEquipConfig.allocate(3);
+    ZoneEquipConfig(1).ZoneName = "Zone 1";
+    ZoneEquipConfig(1).ActualZoneNum = 1;
+
+    ZoneEquipConfig(1).NumInletNodes = 2;
+    ZoneEquipConfig(1).InletNode.allocate(2);
+    ZoneEquipConfig(1).InletNode(1) = 1;
+    ZoneEquipConfig(1).InletNode(2) = 2;
+    ZoneEquipConfig(1).NumExhaustNodes = 1;
+    ZoneEquipConfig(1).ExhaustNode.allocate(1);
+    ZoneEquipConfig(1).ExhaustNode(1) = 3;
+    ZoneEquipConfig(1).NumReturnNodes = 1;
+    ZoneEquipConfig(1).ReturnNode.allocate(1);
+    ZoneEquipConfig(1).ReturnNode(1) = 4;
+
+    ZoneEquipConfig(2).ZoneName = "Zone 2";
+    ZoneEquipConfig(2).ActualZoneNum = 2;
+
+    ZoneEquipConfig(2).NumInletNodes = 1;
+    ZoneEquipConfig(2).InletNode.allocate(1);
+    ZoneEquipConfig(2).InletNode(1) = 6;
+    ZoneEquipConfig(2).NumExhaustNodes = 0;
+    ZoneEquipConfig(2).NumReturnNodes = 1;
+    ZoneEquipConfig(2).ReturnNode.allocate(1);
+    ZoneEquipConfig(2).ReturnNode(1) = 7;
+
+    ZoneEquipConfig(3).ZoneName = "Zone 3";
+    ZoneEquipConfig(3).ActualZoneNum = 3;
+
+    ZoneEquipConfig(3).NumInletNodes = 1;
+    ZoneEquipConfig(3).InletNode.allocate(1);
+    ZoneEquipConfig(3).InletNode(1) = 8;
+    ZoneEquipConfig(3).NumExhaustNodes = 0;
+    ZoneEquipConfig(3).NumReturnNodes = 1;
+    ZoneEquipConfig(3).ReturnNode.allocate(1);
+    ZoneEquipConfig(3).ReturnNode(1) = 9;
+
+    Node.allocate(10);
+
+    Zone.allocate(3);
+    Zone(1).Name = ZoneEquipConfig(1).ZoneName;
+    Zone(1).ZoneEqNum = 1;
+    ZoneEqSizing.allocate(3);
+    CurZoneEqNum = 1;
+    Zone(1).Multiplier = 1.0;
+    Zone(1).Volume = 1000.0;
+    Zone(1).SystemZoneNodeNumber = 5;
+    Zone(1).ZoneVolCapMultpMoist = 1.0;
+    Zone(2).Name = ZoneEquipConfig(2).ZoneName;
+    Zone(2).ZoneEqNum = 1;
+    Zone(2).Multiplier = 1.0;
+    Zone(2).Volume = 1000.0;
+    Zone(2).SystemZoneNodeNumber = 5;
+    Zone(2).ZoneVolCapMultpMoist = 1.0;
+    Zone(3).Name = ZoneEquipConfig(3).ZoneName;
+    Zone(3).ZoneEqNum = 1;
+    Zone(3).Multiplier = 1.0;
+    Zone(3).Volume = 1000.0;
+    Zone(3).SystemZoneNodeNumber = 5;
+    Zone(3).ZoneVolCapMultpMoist = 1.0;
+
+    OutBaroPress = 101325.0;
+
+    NumZoneReturnPlenums = 0;
+    NumZoneSupplyPlenums = 0;
+
+    OAMFL.allocate(3);
+    VAMFL.allocate(3);
+    EAMFL.allocate(3);
+    CTMFL.allocate(3);
+    MDotOA.allocate(3);
+    MDotOA = 0.001;
+    ScheduleManager::Schedule.allocate(1);
+
+    ScheduleManager::Schedule(1).CurrentValue = 1.0;
+
+    SimulateAirflowNetwork = 0;
+
+    ZoneAirSolutionAlgo = UseEulerMethod;
+
+    Node(1).MassFlowRate = 0.01; // Zone inlet node 1
+    Node(1).HumRat = 0.008;
+    Node(2).MassFlowRate = 0.02; // Zone inlet node 2
+    Node(2).HumRat = 0.008;
+    ZoneEquipConfig(1).ZoneExhBalanced = 0.0;
+    Node(3).MassFlowRate = 0.00; // Zone exhaust node 1
+    ZoneEquipConfig(1).ZoneExh = Node(3).MassFlowRate;
+    Node(3).HumRat = 0.008;
+    Node(4).MassFlowRate = 0.03; // Zone return node
+    Node(4).HumRat = 0.000;
+    Node(5).HumRat = 0.000;
+    OAMFL(1) = 0.0;
+    VAMFL(1) = 0.0;
+    EAMFL(1) = 0.0;
+    CTMFL(1) = 0.0;
+    OAMFL(2) = 0.0;
+    VAMFL(2) = 0.0;
+    EAMFL(2) = 0.0;
+    CTMFL(2) = 0.0;
+    OAMFL(3) = 0.0;
+    VAMFL(3) = 0.0;
+    EAMFL(3) = 0.0;
+    CTMFL(3) = 0.0;
+    ZoneAirHumRat(1) = 0.008;
+    ZT(1) = 24.0;
+    ZoneAirHumRat(2) = 0.008;
+    ZT(2) = 23.5;
+    ZoneAirHumRat(3) = 0.008;
+    ZT(3) = 24.5;
+    MixingMassFlowZone = 0.0;
+
+    Node(6).MassFlowRate = 0.01; 
+    Node(7).MassFlowRate = 0.01;
+    Node(8).MassFlowRate = 0.01;
+    Node(9).MassFlowRate = 0.01;
+
+
+    DataContaminantBalance::CO2PredictedRate.allocate(3);
+    DataContaminantBalance::ZoneSysContDemand.allocate(3);
+    DataContaminantBalance::NumContControlledZones = 3;
+
+    DataContaminantBalance::ContaminantControlledZone.allocate(3);
+
+    DataContaminantBalance::ContaminantControlledZone(1).AvaiSchedPtr = 1;
+    DataContaminantBalance::ContaminantControlledZone(1).ActualZoneNum = 1;
+    DataContaminantBalance::ContaminantControlledZone(1).NumOfZones = 1;
+    DataContaminantBalance::ContaminantControlledZone(2).AvaiSchedPtr = 1;
+    DataContaminantBalance::ContaminantControlledZone(2).ActualZoneNum = 2;
+    DataContaminantBalance::ContaminantControlledZone(2).NumOfZones = 1;
+    DataContaminantBalance::ContaminantControlledZone(3).AvaiSchedPtr = 1;
+    DataContaminantBalance::ContaminantControlledZone(3).ActualZoneNum = 3;
+    DataContaminantBalance::ContaminantControlledZone(3).NumOfZones = 1;
+
+    PredictZoneContaminants(ShortenTimeStepSys, UseZoneTimeStepHistory, PriorTimeStep);
+    EXPECT_NEAR(1.0416921806, DataContaminantBalance::CO2PredictedRate(1), 0.00001);
+    EXPECT_NEAR(1.0434496257, DataContaminantBalance::CO2PredictedRate(2), 0.00001);
+    EXPECT_NEAR(1.0399406399, DataContaminantBalance::CO2PredictedRate(3), 0.00001);
+
+    DataContaminantBalance::Contaminant.CO2Simulation = false;
+    DataContaminantBalance::Contaminant.GenericContamSimulation = false;
+}
