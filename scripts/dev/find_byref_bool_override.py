e4f919176b648ce322907ed823dc707edd747d49
Julien Marrec <julien.marrec@gmail.com>
2019-01-31 16:46:40 +0100

    Update script to do more, and include a CHECKED_AND_OKED dictionary for false positive
    I already started to go through the list of current failures, updating CHECKED_AND_OKED when I found it to be harmless

--- scripts/dev/find_byref_bool_override.py
+++ scripts/dev/find_byref_bool_override.py
@@ -31,10 +31,388 @@ import warnings
 
 SRC_DIR = '../../src/EnergyPlus'
 INCLUDE_WARNINGS = True
-IS_CI =False
+IS_CI = False
 
-
+# Files for which to ignore missing header warning
 EXPECT_MISSING_HEADER = ['main.cc', 'test_ep_as_library.cc']
+
+# Finds a boolean argument passed by reference
+# Optional_bool acts like one, Array_XD_bool is another possibility
+RE_BOOL = re.compile(r'\b(?P<booltype>'
+                     r'(?:(?:Array\dD_)?(?:bool\s*&\s+|bool\s+&\s*))|'
+                     r'Optional_bool\s+)(?P<boolname>\w+)')
+
+CHECKED_AND_OKED = {
+
+    "AirflowNetworkBalanceManager.cc": {
+        "ManageAirflowNetworkBalance": [
+            "ResimulateAirZone"
+        ]
+    },
+    # "BranchInputManager.cc": {
+        # "GetBranchFanTypeName": [
+            # "ErrFound"
+        # ],
+        # "GetLoopMixer": [
+            # "IsMixer",
+            # "IsMixer"
+        # ],
+        # "GetLoopSplitter": [
+            # "IsSplitter",
+            # "IsSplitter"
+        # ],
+        # "TestBranchIntegrity": [
+            # "ErrFound"
+        # ]
+    # },
+    # "BranchNodeConnections.cc": {
+        # "GetComponentData": [
+            # "IsParent"
+        # ]
+    # },
+    "DElightManagerF.cc": {
+        "CheckForGeometricTransform": [
+            "doTransform",
+            "doTransform"
+        ]
+    },
+    "DataHeatBalance.cc": {
+        "AddVariableSlatBlind": [
+            "errFlag"
+        ],
+        "ComputeNominalUwithConvCoeffs": [
+            "isValid"
+        ]
+    },
+    "DataRuntimeLanguage.cc": {
+        "ValidateEMSProgramName": [
+            "errFlag"
+        ],
+        "ValidateEMSVariableName": [
+            "errFlag"
+        ]
+    },
+    "DataSizing.cc": {
+        "resetHVACSizingGlobals": [
+            "firstPassFlag"
+        ]
+    },
+
+    # Could be changed to just return a string
+    "DataSystemVariables.cc": {
+        "CheckForActualFileName": [
+            "FileFound"
+        ]
+    },
+
+    "DaylightingManager.cc": {
+        "CheckForGeometricTransform": [
+            "doTransform",
+            "doTransform"
+        ]
+    },
+    "DemandManager.cc": {
+        "LoadInterface": [
+            "CanReduceDemand"
+        ]
+    },
+    "EMSManager.cc": {
+        "ManageEMS": [
+            "anyProgramRan"
+        ]
+    },
+    "FaultsManager.cc": {
+        "SetFaultyCoilSATSensor": [
+            "FaultyCoilSATFlag"
+        ]
+    },
+    "FuelCellElectricGenerator.cc": {
+        # Never used anywhere
+        "FigureTransientConstraints": [
+            "Constrained"
+        ],
+
+        # This one is never used anywhere
+        "ManageElectStorInteractions": [
+            "Constrained"
+        ]
+    },
+    "Furnaces.cc": {
+        "HeatPumpRunFrac": [
+            "errFlag"
+        ]
+    },
+    "General.cc": {
+        "ScanForReports": [
+            "DoReport"
+        ]
+    },
+    "GeneratorDynamicsManager.cc": {
+        "ManageGeneratorFuelFlow": [
+            "ConstrainedIncreasingMdot",
+            "ConstrainedDecreasingMdot"
+        ]
+    },
+    # "GlobalNames.cc": {
+        # "VerifyUniqueADUName": [
+            # "ErrorFound"
+        # ],
+        # "VerifyUniqueBaseboardName": [
+            # "ErrorFound"
+        # ],
+        # "VerifyUniqueBoilerName": [
+            # "ErrorFound"
+        # ],
+        # "VerifyUniqueChillerName": [
+            # "ErrorFound"
+        # ],
+        # "VerifyUniqueCoilName": [
+            # "ErrorFound"
+        # ]
+    # },
+    # "HVACControllers.cc": {
+        # "CheckCoilWaterInletNode": [
+            # "NodeNotFound"
+        # ],
+        # "CheckSimpleController": [
+            # "IsConvergedFlag"
+        # ],
+        # "ExitCalcController": [
+            # "IsUpToDateFlag"
+        # ],
+        # "FindRootSimpleController": [
+            # "IsConvergedFlag"
+        # ],
+        # "GetControllerActuatorNodeNum": [
+            # "NodeNotFound"
+        # ],
+        # "InitController": [
+            # "IsConvergedFlag"
+        # ],
+        # "ManageControllers": [
+            # "AllowWarmRestartFlag"
+        # ],
+        # "ResetController": [
+            # "IsConvergedFlag"
+        # ]
+    # },
+    "HVACDXSystem.cc": {
+        "ControlDXSystem": [
+            "HXUnitOn"
+        ]
+    },
+    "HeatBalanceIntRadExchange.cc": {
+        "UpdateMovableInsulationFlag": [
+            "MovableInsulationChange"
+        ]
+    },
+    "HeatBalanceManager.cc": {
+        "SearchWindow5DataFile": [
+            "ConstructionFound",
+            "EOFonFile"
+        ]
+    },
+
+    # Used only once, with a flag set to false beforehand anyway, and processed
+    # right after the function call
+    "InternalHeatGains.cc": {
+        "GetInternalGainDeviceIndex": [
+            "ErrorFound"
+        ]
+    },
+
+    # Used with a dedicated bool set to false just before
+    "LowTempRadiantSystem.cc": {
+        "InitLowTempRadiantSystem": [
+            "InitErrorsFound"
+        ]
+    },
+    # "MixedAir.cc": {
+        # "SimOAComponent": [
+            # "OAHeatingCoil",
+            # "OACoolingCoil",
+            # "OAHX"
+        # ]
+    # },
+    # "NodeInputManager.cc": {
+        # "GetNodeList": [
+            # "errFlag"
+        # ],
+        # "GetNodeListsInput": [
+            # "ErrorsFound"
+        # ]
+    # },
+    # "NonZoneEquipmentManager.cc": {
+        # "ManageNonZoneEquipment": [
+            # "SimNonZoneEquipment"
+        # ]
+    # },
+    # "OutAirNodeManager.cc": {
+        # "CheckAndAddAirNodeNumber": [
+            # "Okay",
+            # "Okay",
+            # "Okay"
+        # ]
+    # },
+    # "OutputProcessor.cc": {
+        # "DetermineMeterIPUnits": [
+            # "ErrorsFound"
+        # ],
+        # "GetCustomMeterInput": [
+            # "ErrorsFound"
+        # ],
+        # "GetStandardMeterResourceType": [
+            # "ErrorsFound"
+        # ],
+        # "ReportTSMeters": [
+            # "PrintESOTimeStamp"
+        # ]
+    # },
+    # "OutputReportTabular.cc": {
+        # "ComputeTableBodyUsingMovingAvg": [
+            # "resCellsUsd"
+        # ]
+    # },
+    "PackagedTerminalHeatPump.cc": {
+        "HeatPumpRunFrac": [
+            "errFlag"
+        ]
+    },
+    # "PlantCondLoopOperation.cc": {
+        # "ActivateEMSControls": [
+            # "LoopShutDownFlag",
+            # "LoopShutDownFlag"
+        # ],
+        # "GetPlantOperationInput": [
+            # "GetInputOK"
+        # ]
+    # },
+    # "PlantUtilities.cc": {
+        # "MyPlantSizingIndex": [
+            # "ErrorsFound"
+        # ]
+    # },
+    # "RoomAirModelManager.cc": {
+        # "GetRAFNNodeNum": [
+            # "Errorfound"
+        # ]
+    # },
+    # "RootFinder.cc": {
+        # "IterateRootFinder": [
+            # "IsDoneFlag"
+        # ]
+    # },
+    # "SimAirServingZones.cc": {
+        # "SolveAirLoopControllers": [
+            # "AirLoopConvergedFlag"
+        # ]
+    # },
+    # "SolarShading.cc": {
+        # "CHKGSS": [
+            # "CannotShade"
+        # ]
+    # },
+    # "SurfaceGeometry.cc": {
+        # "GetMovableInsulationData": [
+            # "ErrorsFound"
+        # ]
+    # },
+    # "SwimmingPool.cc": {
+        # "InitSwimmingPoolPlantLoopIndex": [
+            # "MyPlantScanFlagPool",
+            # "MyPlantScanFlagPool"
+        # ]
+    # },
+    # "SystemReports.cc": {
+        # "FindDemandSideMatch": [
+            # "MatchFound"
+        # ],
+        # "FindFirstLastPtr": [
+            # "ConnectionFlag"
+        # ]
+    # },
+    # "UserDefinedComponents.cc": {
+        # "SimCoilUserDefined": [
+            # "HeatingActive",
+            # "CoolingActive"
+        # ]
+    # },
+    # "UtilityRoutines.cc": {
+        # "ProcessNumber": [
+            # "ErrorFlag",
+            # "ErrorFlag"
+        # ],
+        # "VerifyName": [
+            # "ErrorFound",
+            # "IsBlank",
+            # "ErrorFound",
+            # "IsBlank"
+        # ]
+    # },
+    # "Vectors.cc": {
+        # "CalcCoPlanarNess": [
+            # "IsCoPlanar"
+        # ],
+        # "CompareTwoVectors": [
+            # "areSame",
+            # "areSame",
+            # "areSame"
+        # ],
+        # "PlaneEquation": [
+            # "error"
+        # ]
+    # },
+    # "WaterCoils.cc": {
+        # "CheckActuatorNode": [
+            # "NodeNotFound"
+        # ],
+        # "CheckForSensorAndSetPointNode": [
+            # "NodeNotFound"
+        # ]
+    # },
+    # "WaterThermalTanks.cc": {
+        # "ValidatePLFCurve": [
+            # "IsValid",
+            # "IsValid"
+        # ]
+    # },
+    # "WeatherManager.cc": {
+        # "GetNextEnvironment": [
+            # "Available",
+            # "Available",
+            # "Available",
+            # "Available",
+            # "Available",
+            # "Available"
+        # ],
+        # "InterpretWeatherDataLine": [
+            # "ErrorFound"
+        # ],
+        # "ProcessEPWHeader": [
+            # "ErrorsFound"
+        # ],
+        # "ReportWeatherAndTimeInformation": [
+            # "PrintEnvrnStamp"
+        # ]
+    # },
+    # "WindowAC.cc": {
+        # "ControlCycWindACOutput": [
+            # "HXUnitOn",
+            # "HXUnitOn"
+        # ]
+    # },
+    # "WindowComplexManager.cc": {
+        # "CheckGasCoefs": [
+            # "feedData"
+        # ]
+    # },
+    # "ZoneEquipmentManager.cc": {
+        # "ManageZoneEquipment": [
+            # "SimZone"
+        # ]
+    # }
+}
+
 
 ###############################################################################
 #                              F U N C T I O N S                              #
@@ -93,8 +471,6 @@ def parse_function_signatures_in_header(header_file):
                          r'(?P<post_qualifiers>.*?)(?:override)?\s*;')
     signature_re = re.compile(signature_pattern, re.MULTILINE | re.DOTALL)
 
-    re_bool = re.compile(r'bool(?:& | &)(\w+)')
-
     # Relative path, for cleaner reporting
     rel_file = os.path.relpath(header_file, start=SRC_DIR)
 
@@ -117,10 +493,24 @@ def parse_function_signatures_in_header(header_file):
         with open(header_file, 'r', encoding='latin-1') as f:
             content = f.read()
 
-    found_functions = [m.groupdict() for m in signature_re.finditer(content)
-                       if re_bool.search(m.groupdict()['args'])]
-
+    found_functions = []
+    for m in signature_re.finditer(content):
+        d = m.groupdict()
+        bools = [m2.groupdict() for m2 in RE_BOOL.finditer(d['args'])]
+        if bools:
+            d['bools'] = bools
+            found_functions.append(d)
     return found_functions
+
+
+def check_if_oked(rel_file, function_name, boolname):
+    if rel_file not in CHECKED_AND_OKED:
+        return False
+    if function_name not in CHECKED_AND_OKED[rel_file]:
+        return False
+    if boolname not in CHECKED_AND_OKED[rel_file][function_name]:
+        return False
+    return True
 
 
 def lookup_errors_in_source_file(source_file, found_functions):
@@ -171,10 +561,12 @@ def lookup_errors_in_source_file(source_file, found_functions):
     # We look for the opening of the function in question
     cc_pat = r'{r}\s+{n}\s*\((?P<args>.*?)\)\s*{{'
 
-    for found_function in found_functions:
+    for i, found_function in enumerate(found_functions):
+
+        fname = found_function['function_name']
 
         signature_pattern = cc_pat.format(r=found_function['return_type'],
-                                          n=found_function['function_name'])
+                                          n=fname)
         re_signature = re.compile(signature_pattern, re.MULTILINE | re.DOTALL)
         m = re_signature.search(content)
         if not m:
@@ -182,7 +574,7 @@ def lookup_errors_in_source_file(source_file, found_functions):
             if INCLUDE_WARNINGS:
                 msg = ("In file {f}, cannot find function {n}:\n"
                        "{d}".format(f=rel_file,
-                                    n=found_function['function_name'],
+                                    n=fname,
                                     d=d))
                 if IS_CI:
                     ci_msg = {'tool': 'find_byref_bool_overide',
@@ -197,34 +589,75 @@ def lookup_errors_in_source_file(source_file, found_functions):
                 # Skip iteration
                 continue
 
-        re_bool = re.compile(r'bool(?:& | &)(\w+)')
-        bools = re_bool.findall(m.groupdict()['args'])
+        args = m.groupdict()['args']
+        bools = [m.groupdict() for m in RE_BOOL.finditer(args)]
 
         fbody_start_line_num = content[:m.end()].count('\n')
         line_num = fbody_start_line_num
         n_braces = lines[line_num].count('{') - lines[line_num].count('}')
+        # Shouldn't happen
+        if n_braces == 0:
+            print("Might want to check this...")
+            while n_braces == 0:
+                n_braces = (lines[line_num].count('{') -
+                            lines[line_num].count('}'))
+                line_num += 1
         while n_braces > 0:
             line_num += 1
             line = lines[line_num].strip()
             n_braces += line.count('{') - line.count('}')
 
-            for b in bools:
+            for b_dict in bools:
+                b = b_dict['boolname']
+
+                # If checked and Okay'ed, we skip it
+                if check_if_oked(rel_file=rel_file, function_name=fname,
+                                 boolname=b):
+                    # print("Skipped")
+                    continue
+
                 pat = r'{b}\s*=\s*false;'.format(b=b)
                 re_this_bool = re.compile(pat)
                 if re_this_bool.search(line):
-                    errors.append({'file': rel_file,
-                                   'function': found_function['function_name'],
-                                   'line_num': line_num,
-                                   'line': line})
-                    fname = found_function['function_name']
-                    if INCLUDE_WARNINGS and not IS_CI:
-                        print("Error in {f}:{function}, on line {n}: "
-                              "`{line}`".format(f=rel_file,
-                                                function=fname,
-                                                n=line_num,
-                                                line=line))
+                    b_info = "{}{}".format(b_dict['booltype'],
+                                           b_dict['boolname'])
 
-        return errors
+                    msg = ("Boolean flag `{b}` reset to false in {f}:"
+                           "{function}(), on line {n}")  # :\n"{line}")
+                    msg = msg.format(b=b_info, f=rel_file, function=fname,
+                                     n=line_num, line=line)
+
+                    errors.append({'file': rel_file,
+                                   'function_name': fname,
+                                   'function': found_function,
+                                   'bool': b_dict,
+                                   'line_num': line_num,
+                                   'line': line,
+                                   'msg': msg})
+
+                    if not IS_CI:
+                        print(msg)
+
+    return errors
+
+
+def generate_potential_false_positive_dict(all_errors):
+    """
+    A helper to generate a list of all errors, so you can more easily update it
+    above
+    """
+    errs_by_file = {}
+    for error in all_errors:
+        f = error['file']
+        if f not in errs_by_file:
+            errs_by_file[f] = {}
+        function_name = error['function_name']
+        b = error['bool']['boolname']
+        if function_name not in errs_by_file[f]:
+            errs_by_file[f][function_name] = []
+        errs_by_file[f][function_name].append(b)
+    print(json.dumps(errs_by_file, indent=4, sort_keys=True))
+    return errs_by_file
 
 
 ###############################################################################
@@ -275,23 +708,19 @@ def get_all_errors(source_files):
             if errors:
                 all_errors += errors
 
+    # Sort errors by file
+    all_errors.sort(key=lambda x: x['file'], reverse=False)
+
     return all_errors
 
 
 def output_errors_for_decent_ci(all_errors):
     for error in all_errors:
-
-        msg = ("Boolean flag reset to false in {file}:{function}(), "
-               "on line {line_num}:\n"
-               "{line}".format(file=error['file'],
-                               function=error['function'],
-                               line_num=error['line_num'],
-                               line=error['line'].strip()))
         ci_msg = {'tool': 'find_byref_bool_overide',
                   'filename': error['file'],
                   'line': error['line_num'],
                   'messagetype': 'error',
-                  'message': msg
+                  'message': error['msg']
                   }
         print(json.dumps(ci_msg))
 
