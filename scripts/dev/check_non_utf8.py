383450538e9182149b95550675c1571d764674ec
Edwin Lee <leeed2001@gmail.com>
2019-03-23 10:12:59 -0500

    Post 9.1 CI cleanups: Disable Objexx unit tests - we dont need to run these on every one of our commits Dont run unit tests on the integration coverage configuration, only integration tests Clean up decent ci yaml files Add idf verification as a custom_check Add non-utf8 as a custom_check

---
+++ scripts/dev/check_non_utf8.py
@@ -0,0 +1,40 @@
+import codecs
+import json
+import os
+
+DIRS_TO_SKIP = ('.git', 'build', 'builds', 'cmake-build-debug', 'cmake-build-release', 'design', 'release', 'third_party')
+FILE_PATTERNS = ('.cc', '.hh', '.tex', '.cpp', '.hpp')
+
+current_script_dir = os.path.dirname(os.path.realpath(__file__))
+repo_root = os.path.abspath(os.path.join(current_script_dir, '..', '..'))
+full_path_dirs_to_skip = tuple([os.path.join(repo_root, d) for d in DIRS_TO_SKIP])
+
+# str.decode and str.encode function can be very useful
+files_with_non_utf8 = []
+for root, dirs, filenames in os.walk(repo_root):
+    if root.startswith(full_path_dirs_to_skip):
+        continue
+    for filename in filenames:
+        if not filename.endswith(FILE_PATTERNS):
+            continue
+        file_path = os.path.join(root, filename)
+        relative_file_path = os.path.relpath(file_path, repo_root)
+        print("Trying to open file: " + relative_file_path)
+        line_num = 0
+        try:
+            with codecs.open(file_path, encoding='utf-8', errors='ignore') as f_idf:
+                idf_text = f_idf.readlines()  # EDWIN: Make sure this reads the IDF properly
+                for line in idf_text:
+                    line_num += 1
+                    line.decode('UTF-8', 'strict')
+        except (UnicodeDecodeError, UnicodeEncodeError):
+            # line contains non-utf8 character
+            files_with_non_utf8.append({
+                'tool': 'check_non_utf8',
+                'filename': relative_file_path,
+                'line': line_num,
+                'messagetype': 'warning',
+                'message': 'File had invalid characters/encoding issues'
+            })
+if len(files_with_non_utf8) > 0:
+    print(json.dumps(files_with_non_utf8))
---
+++ scripts/dev/verify_idfs_in_cmake.py
@@ -0,0 +1,60 @@
+import json
+import os
+
+
+def usage():
+    print("""This script verifies that the idf list in CMakeLists.txt matches the idf files in the testfiles directory.
+Call with one argument: the directory which includes idfs and the CMakeLists.txt file""")
+
+
+current_script_dir = os.path.dirname(os.path.realpath(__file__))
+test_files_dir = os.path.join(current_script_dir, '..', '..', 'testfiles')
+
+cmake_lists_file = os.path.join(test_files_dir, "CMakeLists.txt")
+cmake_list_idf_files = set()
+with open(cmake_lists_file) as f:
+    for line in f.readlines():
+        line_stripped = line.strip()
+        if "ADD_SIMULATION_TEST" in line_stripped:
+            idf_file_index = line_stripped.index("IDF_FILE")
+            epw_file_index = line_stripped.index("EPW_FILE")
+            filename = line_stripped[idf_file_index + 9:epw_file_index - 1]
+            cmake_list_idf_files.add(filename)
+
+found_idf_files = set()
+for root, dirs, files in os.walk(test_files_dir):
+    for sfile in files:
+        if sfile.endswith('.idf') or sfile.endswith('.imf'):
+            if root == test_files_dir:
+                found_idf_files.add(sfile)
+            else:
+                folder = os.path.basename(os.path.normpath(root))
+                found_idf_files.add(os.path.join(folder, sfile))
+
+# there are a few files we purposely skip
+files_to_skip = {"_1a-Long0.0.idf", "_ExternalInterface-actuator.idf", "_ExternalInterface-schedule.idf",
+                 "_ExternalInterface-variable.idf", "HVAC3Zone-IntGains-Def.imf", "HVAC3ZoneChillerSpec.imf",
+                 "HVAC3ZoneGeometry.imf", "HVAC3ZoneMat-Const.imf"}
+found_idf_files_trimmed = found_idf_files - files_to_skip
+
+# the CMakeLists file will always have forward slashes
+# on Linux and Mac, the found_idfs will also have forward slashes
+# but on Windows, the path delimiter will be a backslash
+# so replace all backslashes here before comparing anything.
+found_idf_files_refined = set()
+for fil in found_idf_files_trimmed:
+    found_idf_files_refined.add(fil.replace("\\", "/"))
+
+# check if any are missing in cmake
+need_to_add_to_cmake = found_idf_files_refined.difference(cmake_list_idf_files)
+if len(need_to_add_to_cmake) > 0:
+    missing_files = []
+    for this_file in sorted(need_to_add_to_cmake):
+        missing_files.append({
+            'tool': 'verify_idfs_in_cmake',
+            'filename': this_file,
+            'line': 0,
+            'messagetype': 'error',
+            'message': 'File missing from testfiles/CMakeLists.txt'
+        })
+    print(json.dumps(missing_files))
