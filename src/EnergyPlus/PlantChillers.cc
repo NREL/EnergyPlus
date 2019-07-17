3fc95f7c4a93c3c933cb5df3bd4ffbe13d46ba0e
Brent Griffith <Brent.Griffith@EnergyArchmage.com>
2019-07-17 10:02:39 -0700

    Revise CW flow reqeust for VS pumping
    Three types of chillers in PlantChillers.cc

--- src/EnergyPlus/PlantChillers.cc
+++ src/EnergyPlus/PlantChillers.cc
@@ -5978,8 +5978,15 @@ namespace PlantChillers {
             // Either set the flow to the Constant value or caluclate the flow for the variable volume
             if ((ElectricChiller(ChillNum).Base.FlowMode == ConstantFlow) || (ElectricChiller(ChillNum).Base.FlowMode == NotModulated)) {
 
-                // Start by assuming max (design) flow
-                EvapMassFlowRate = EvapMassFlowRateMax;
+                // TRANE, no this messes up flow resolution wrt to load dispatch // Start by assuming max (design) flow
+                // TRANE  EvapMassFlowRate = EvapMassFlowRateMax;
+                // TRANE scale evaporator mass flow with PLR, if VS pumps then flow will modulate per load dispatch, if constant it won't matter
+                if (MyLoad < 0.0 && RunFlag) {                                // TRANE
+                    EvapMassFlowRate = EvapMassFlowRateMax * OperPartLoadRat; // TRANE
+                } else {                                                      // TRANE
+                    EvapMassFlowRate = 0.0;                                   // TRANE
+                }                                                             // TRANE
+
                 // Use SetComponentFlowRate to decide actual flow
                 SetComponentFlowRate(EvapMassFlowRate,
                                      EvapInletNode,
@@ -6690,8 +6697,14 @@ namespace PlantChillers {
 
             // Either set the flow to the Constant value or caluclate the flow for the variable volume
             if ((EngineDrivenChiller(ChillerNum).Base.FlowMode == ConstantFlow) || (EngineDrivenChiller(ChillerNum).Base.FlowMode == NotModulated)) {
-                // Start by assuming max (design) flow
-                EvapMassFlowRate = EvapMassFlowRateMax;
+                // TRANE, no this messes up flow resolution wrt to load dispatch // Start by assuming max (design) flow
+                // TRANE  EvapMassFlowRate = EvapMassFlowRateMax;
+                // TRANE scale evaporator mass flow with PLR, if VS pumps then flow will modulate per load dispatch, if constant it won't matter
+                if (MyLoad < 0.0 && RunFlag) {                                // TRANE
+                    EvapMassFlowRate = EvapMassFlowRateMax * OperPartLoadRat; // TRANE
+                } else {                                                      // TRANE
+                    EvapMassFlowRate = 0.0;                                   // TRANE
+                }                                                             // TRANE
                 // Use SetComponentFlowRate to decide actual flow
                 SetComponentFlowRate(EvapMassFlowRate,
                                      EvapInletNode,
@@ -7454,8 +7467,14 @@ namespace PlantChillers {
 
             // Either set the flow to the Constant value or caluclate the flow for the variable volume
             if ((GTChiller(ChillerNum).Base.FlowMode == ConstantFlow) || (GTChiller(ChillerNum).Base.FlowMode == NotModulated)) {
-                // Start by assuming max (design) flow
-                EvapMassFlowRate = EvapMassFlowRateMax;
+                // TRANE, no this messes up flow resolution wrt to load dispatch // Start by assuming max (design) flow
+                // TRANE  EvapMassFlowRate = EvapMassFlowRateMax;
+                // TRANE scale evaporator mass flow with PLR, if VS pumps then flow will modulate per load dispatch, if constant it won't matter
+                if (MyLoad < 0.0 && RunFlag) {                                // TRANE
+                    EvapMassFlowRate = EvapMassFlowRateMax * OperPartLoadRat; // TRANE
+                } else {                                                      // TRANE
+                    EvapMassFlowRate = 0.0;                                   // TRANE
+                }                                                             // TRANE
                 // Use SetComponentFlowRate to decide actual flow
                 SetComponentFlowRate(EvapMassFlowRate,
                                      EvapInletNode,
