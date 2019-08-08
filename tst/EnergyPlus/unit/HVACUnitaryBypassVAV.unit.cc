685c38ad0f136e1c0a1acc5d79096cb91c9eaa60
rraustad <rraustad@fsec.ucf.edu>
2019-08-08 13:17:32 -0400

    Fix broken unit test.

--- tst/EnergyPlus/unit/HVACUnitaryBypassVAV.unit.cc
+++ tst/EnergyPlus/unit/HVACUnitaryBypassVAV.unit.cc
@@ -242,6 +242,7 @@ protected:
         cbvav.HeatCoilName = "MyHeatingCoil";
         cbvav.DXCoolCoilType_Num = DataHVACGlobals::CoilDX_CoolingSingleSpeed;
         cbvav.HeatCoilType_Num = DataHVACGlobals::Coil_HeatingElectric;
+        cbvav.minModeChangeTime = 0.0;
         cbvav.AirInNode = 1;
         cbvav.AirOutNode = 2;
         cbvav.MixerOutsideAirNode = 3;
