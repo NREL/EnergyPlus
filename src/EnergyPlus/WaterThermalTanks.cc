8ec2c2deb664433d904baa3f391341655025bc14
Julien Marrec <julien.marrec@gmail.com>
2019-08-27 12:52:39 +0200

    Only report if not during warmup.

--- src/EnergyPlus/WaterThermalTanks.cc
+++ src/EnergyPlus/WaterThermalTanks.cc
@@ -7548,23 +7548,24 @@ namespace WaterThermalTanks {
         Tank.TankTemp = TankTemp;            // Final tank temperature for carry-over to next timestep
         Tank.TankTempAvg = TankTempAvg;      // Average tank temperature over the timestep for reporting
 
-        // Warn for potential freezing when avg of final temp over all nodes is below 2°C (nearing 0°C)
-        if (Tank.TankTemp < 2) {
-            if (Tank.FreezingErrorIndex == 0) {
-                ShowWarningError(RoutineName + ": " + Tank.Type +" = '"
-                        + Tank.Name + "':  Temperature of tank < 2C indicates of possibility of freeze. Tank Temperature = "
-                        + General::RoundSigDigits(Tank.TankTemp, 2) + " C.");
-                ShowContinueErrorTimeStamp("");
+        if (!WarmupFlag) {
+            // Warn for potential freezing when avg of final temp over all nodes is below 2°C (nearing 0°C)
+            if (Tank.TankTemp < 2) {
+                if (Tank.FreezingErrorIndex == 0) {
+                    ShowWarningError(RoutineName + ": " + Tank.Type +" = '"
+                            + Tank.Name + "':  Temperature of tank < 2C indicates of possibility of freeze. Tank Temperature = "
+                            + General::RoundSigDigits(Tank.TankTemp, 2) + " C.");
+                    ShowContinueErrorTimeStamp("");
+                }
+                ShowRecurringWarningErrorAtEnd(Tank.Type +" = '" + Tank.Name + "':  Temperature of tank < 2C indicates of possibility of freeze",
+                                               Tank.FreezingErrorIndex,
+                                               Tank.TankTemp, // Report Max
+                                               Tank.TankTemp, // Report Min
+                                               _,             // Don't report Sum
+                                               "{C}",         // Max Unit
+                                               "{C}");        // Min Unit
             }
-            ShowRecurringWarningErrorAtEnd(Tank.Type +" = '" + Tank.Name + "':  Temperature of tank < 2C indicates of possibility of freeze",
-                                           Tank.FreezingErrorIndex,
-                                           Tank.TankTemp, // Report Max
-                                           Tank.TankTemp, // Report Min
-                                           _,             // Don't report Sum
-                                           "{C}",         // Max Unit
-                                           "{C}");        // Min Unit
         }
-
         Tank.UseOutletTemp = TankTempAvg;    // Because entire tank is at same temperature
         Tank.SourceOutletTemp = TankTempAvg; // Because entire tank is at same temperature
         if (Tank.HeatPumpNum > 0) {
@@ -7911,6 +7912,7 @@ namespace WaterThermalTanks {
         using DataGlobals::HourOfDay;
         using DataGlobals::TimeStep;
         using DataGlobals::TimeStepZone;
+        using DataGlobals::WarmupFlag;
         using DataHVACGlobals::SysTimeElapsed;
         using DataHVACGlobals::TimeStepSys;
         using FluidProperties::GetDensityGlycol;
@@ -8338,21 +8340,23 @@ namespace WaterThermalTanks {
         Tank.TankTemp = sum(Tank.Node, &StratifiedNodeData::Temp) / Tank.Nodes;
         Tank.TankTempAvg = sum(Tank.Node, &StratifiedNodeData::TempAvg) / Tank.Nodes;
 
-        // Warn for potential freezing when avg of final temp over all nodes is below 2°C (nearing 0°C)
-        if (Tank.TankTemp < 2) {
-            if (Tank.FreezingErrorIndex == 0) {
-                ShowWarningError(RoutineName + ": " + Tank.Type +" = '"
-                        + Tank.Name + "':  Temperature of tank < 2C indicates of possibility of freeze. Tank Temperature = "
-                        + General::RoundSigDigits(Tank.TankTemp, 2) + " C.");
-                ShowContinueErrorTimeStamp("");
+        if (!WarmupFlag) {
+            // Warn for potential freezing when avg of final temp over all nodes is below 2°C (nearing 0°C)
+            if (Tank.TankTemp < 2) {
+                if (Tank.FreezingErrorIndex == 0) {
+                    ShowWarningError(RoutineName + ": " + Tank.Type +" = '"
+                            + Tank.Name + "':  Temperature of tank < 2C indicates of possibility of freeze. Tank Temperature = "
+                            + General::RoundSigDigits(Tank.TankTemp, 2) + " C.");
+                    ShowContinueErrorTimeStamp("");
+                }
+                ShowRecurringWarningErrorAtEnd(Tank.Type +" = '" + Tank.Name + "':  Temperature of tank < 2C indicates of possibility of freeze",
+                                               Tank.FreezingErrorIndex,
+                                               Tank.TankTemp, // Report Max
+                                               Tank.TankTemp, // Report Min
+                                               _,             // Don't report Sum
+                                               "{C}",         // Max Unit
+                                               "{C}");        // Min Unit
             }
-            ShowRecurringWarningErrorAtEnd(Tank.Type +" = '" + Tank.Name + "':  Temperature of tank < 2C indicates of possibility of freeze",
-                                           Tank.FreezingErrorIndex,
-                                           Tank.TankTemp, // Report Max
-                                           Tank.TankTemp, // Report Min
-                                           _,             // Don't report Sum
-                                           "{C}",         // Max Unit
-                                           "{C}");        // Min Unit
         }
 
         if (Tank.UseOutletStratNode > 0) {
