682b22c86d1a9a0711df5d93924b8dda2525be0a
Neal Kruis <neal.kruis@bigladdersoftware.com>
2020-01-24 10:49:24 -0700

    Make documentation a separate sub-project.

--- doc/cmake/BuildDocumentation.cmake
+++ doc/cmake/BuildDocumentation.cmake
@@ -3,7 +3,7 @@
   # INNAME, the name of the input tex file
   # OUTNAME, the pretty output name for the pdf
   # ORIGINAL_CMAKE_SOURCE_DIR, the root of the source repo
-  # ORIGINAL_CMAKE_BINARY_DIR, the root fo the build tree
+  # ORIGINAL_CMAKE_BINARY_DIR, the root of the build tree
 # this will get more complicated as we add things like bibtex and makeindex, for now just execute xelatex twice and rename the output
 if ("${TEX_INTERACTION}" STREQUAL "")
   set(THIS_TEX_INTERACTION "batchmode")
