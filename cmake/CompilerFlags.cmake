fb85df36a7c7f6ebb5304d38e1aa7e96aa4f9af2
Edwin Lee <leeed2001@gmail.com>
2020-02-04 16:34:43 -0500

    Continuing to clean up hardcoded paths

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,4 +1,6 @@
 
+include(cmake/ProjectMacros.cmake)
+
 # Compiler-agnostic compiler flags first
 ADD_CXX_DEFINITIONS("-DOBJEXXFCL_ALIGN=64") # Align ObjexxFCL arrays to 64B
 ADD_CXX_DEBUG_DEFINITIONS("-DOBJEXXFCL_ARRAY_INIT_DEBUG") # Initialize ObjexxFCL arrays to aid debugging
