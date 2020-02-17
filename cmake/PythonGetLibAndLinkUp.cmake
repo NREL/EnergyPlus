84a34234bb96eb11237a3c7e70dbc618072c424e
Edwin Lee <leeed2001@gmail.com>
2020-02-17 11:27:30 -0600

    Intermediate commit to fixup linking process

--- cmake/PythonGetLibAndLinkUp.cmake
+++ cmake/PythonGetLibAndLinkUp.cmake
@@ -1,5 +1,8 @@
-# Steps we need to follow for now:
-#  Use finder to get Python executable
+# Linking up to Python is not necessarily complex, but it is tricky to get right.
+# Our goal here is to provide a fully packaged embedded Python interpreter and Python 3.5+ standard library
+# This file's job is to copy over the required Python DLL and fixup the EnergyPlus binary on Mac to find on it locally
+# To accomplish this, we need to know which EnergyPlus binary to fixup, and some info about the Python library
+# The steps we follow are:
 #  Use finder to get Python library and include paths
 #  Add Python include path to include_directories
 #  Link E+ against Python library
@@ -18,15 +21,30 @@
 # We are going to create a local virtual environment that is portable so we can package it and install it
 # Users will not need to have Python installed, and it will come with a Python.exe and a Pip.exe for installing libraries
 
-# set RESOLVED_PYTHON_LIB, PYTHON_LIB_FILENAME, and EXECUTABLE_PATH when calling
+# set RESOLVED_PYTHON_LIB, and EXECUTABLE_PATH when calling
 
-message("Setting up Python Linking:\n RESOLVED_PYTHON_LIB: ${RESOLVED_PYTHON_LIB}\n PYTHON_LIB_FILENAME: ${PYTHON_LIB_FILENAME}\n EXECUTABLE_PATH: ${EXECUTABLE_PATH}")
+message("Setting up Python Linking:\n RESOLVED_PYTHON_LIB: ${RESOLVED_PYTHON_LIB}\n EXECUTABLE_PATH: ${EXECUTABLE_PATH}")
 
-include(GetPrerequisites)
-get_filename_component(PYTHON_LIB_FILENAME "${RESOLVED_PYTHON_LIB}" NAME)
+# get some path info things
 get_filename_component(BASE_PATH "${EXECUTABLE_PATH}" DIRECTORY)
+get_filename_component(PYTHON_LIB_FILENAME "${RESOLVED_PYTHON_LIB}" NAME)
+
+# on Windows, RESOLVED_PYTHON_LIB is pointing to the likes of `C:\Python38\libs\python38.lib`,
+# when we actually want the DLL at `C:\Python38\python38.dll`
+# need to fix that up here
+if(WIN32)
+  get_filename_component(LIB_SUB_DIR "${RESOLVED_PYTHON_LIB}" DIRECTORY)
+  get_filename_component(PYTHON_ROOT_DIR "${LIB_SUB_DIR}" DIRECTORY)
+  string(REPLACE ".lib" ".dll" DLL_FILE_NAME "${PYTHON_LIB_FILENAME}")
+  set(RESOLVED_PYTHON_LIB "${PYTHON_ROOT_DIR}/${DLL_FILE_NAME}")
+endif()
+
+# then copy the DLL in
 execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${RESOLVED_PYTHON_LIB}" "${BASE_PATH}")
+
+# on Mac, we then need to fixup the binary
 if(APPLE)
+  include(GetPrerequisites)
   get_prerequisites("${EXECUTABLE_PATH}" PREREQUISITES 1 1 "" "")
   foreach(PREREQ IN LISTS PREREQUISITES)
     string(TOLOWER "${PREREQ}" PREREQ_LOWERCASE )
