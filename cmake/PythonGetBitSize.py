0edc4e01a32157ac7bb9668a914dc130bc4f9ee3
Edwin Lee <leeed2001@gmail.com>
2020-02-20 14:37:45 -0600

    Fix bitsize reporting, now I can produce the error nicely

--- cmake/PythonGetBitSize.py
+++ cmake/PythonGetBitSize.py
@@ -1,4 +1,4 @@
 # this file helps identify the Python bit size (32 or 64)
 # this is important when linking E+ against the Python library
 import struct
-print(8 * struct.calcsize("P"))
+print(8 * struct.calcsize("P"), end="")
