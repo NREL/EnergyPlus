73839441cdaa1b8248ff71861f0e17988509a345
Edwin Lee <leeed2001@gmail.com>
2020-02-27 16:41:20 -0600

    Fleshing out unit test

--- tst/EnergyPlus/unit/Coils/CoilCoolingDX.unit.cc
+++ tst/EnergyPlus/unit/Coils/CoilCoolingDX.unit.cc
@@ -82,7 +82,7 @@ TEST_F( CoilCoolingDXTest, CoilCoolingDXAlternateModePerformance )
 
         "  Coil:Cooling:DX:CurveFit:OperatingMode,",
         "    Coil Mode 1,",
-        "    3000,   !- Rated Gross Total Cooling Capacity {W}",
+        "    5000,   !- Rated Gross Total Cooling Capacity {W}",
         "    0.25,   !- Rated Evaporator Air Flow Rate {m3/s}",
         "    ,,,,,,,,",
         "    2,Coil Mode 1 Speed 1,Coil Mode 1 Speed 2;",
@@ -109,7 +109,7 @@ TEST_F( CoilCoolingDXTest, CoilCoolingDXAlternateModePerformance )
 
         "  Coil:Cooling:DX:CurveFit:OperatingMode,",
         "    Coil Mode 2,",
-        "    3000,   !- Rated Gross Total Cooling Capacity {W}",
+        "    5000,   !- Rated Gross Total Cooling Capacity {W}",
         "    0.25,   !- Rated Evaporator Air Flow Rate {m3/s}",
         "    ,,,,,,,,",
         "    2,Coil Mode 2 Speed 1,Coil Mode 2 Speed 2;",
@@ -141,9 +141,9 @@ TEST_F( CoilCoolingDXTest, CoilCoolingDXAlternateModePerformance )
     // fix the inlet conditions
     auto &evapInletNode = DataLoopNode::Node(thisCoil.evapInletNodeIndex);
     auto &condInletNode = DataLoopNode::Node(thisCoil.condInletNodeIndex);
-    evapInletNode.Temp = 25.5;
+    evapInletNode.Temp = 28.5;
     evapInletNode.Press = 101325;
-    evapInletNode.HumRat = 0.01;
+    evapInletNode.HumRat = 0.014;
     evapInletNode.Enthalpy = Psychrometrics::PsyHFnTdbW(evapInletNode.Temp, evapInletNode.HumRat);
     condInletNode.Temp = 35.0;
     condInletNode.Press = 101325;
@@ -167,31 +167,183 @@ TEST_F( CoilCoolingDXTest, CoilCoolingDXAlternateModePerformance )
     Real64 speedRatio = 1.0;
     int fanOpMode = 1;
     thisCoil.simulate(useAlternateMode, PLR, speedNum, speedRatio, fanOpMode);
-    EXPECT_NEAR(1500, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
-    EXPECT_NEAR(18.098, evapOutletNode.Temp, 0.01);
-    EXPECT_NEAR(0.0088, evapOutletNode.HumRat, 0.001);
+//    std::cout << thisCoil.totalCoolingEnergyRate << ',' << evapOutletNode.Temp << ',' << evapOutletNode.HumRat << std::endl;
+    EXPECT_NEAR(2500, thisCoil.totalCoolingEnergyRate, 0.1); // expect the coil to run full out, at speed 1
+    EXPECT_NEAR(17.896, evapOutletNode.Temp, 0.01);
+    EXPECT_NEAR(0.0114, evapOutletNode.HumRat, 0.001);
 
     // alter values and run at rated conditions normal mode speed 2
     evapInletNode.MassFlowRate = thisCoil.performance.normalMode.speeds.back().RatedAirMassFlowRate;
     speedNum = 2;
     thisCoil.simulate(useAlternateMode, PLR, speedNum, speedRatio, fanOpMode);
-    EXPECT_NEAR(3000, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
-    EXPECT_NEAR(18.098, evapOutletNode.Temp, 0.01);
-    EXPECT_NEAR(0.0088, evapOutletNode.HumRat, 0.001);
+//    std::cout << thisCoil.totalCoolingEnergyRate << ',' << evapOutletNode.Temp << ',' << evapOutletNode.HumRat << std::endl;
+    EXPECT_NEAR(5000, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
+    EXPECT_NEAR(17.896, evapOutletNode.Temp, 0.01);
+    EXPECT_NEAR(0.0114, evapOutletNode.HumRat, 0.001);
 
     // ok so now run at alternate mode, speed 1
     useAlternateMode = true;
     speedNum = 1;
     thisCoil.simulate(useAlternateMode, PLR, speedNum, speedRatio, fanOpMode);
-    EXPECT_NEAR(1350, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
-    EXPECT_NEAR(22.59, evapOutletNode.Temp, 0.01);
-    EXPECT_NEAR(0.0093, evapOutletNode.HumRat, 0.0001);
+//    std::cout << thisCoil.totalCoolingEnergyRate << ',' << evapOutletNode.Temp << ',' << evapOutletNode.HumRat << std::endl;
+    EXPECT_NEAR(2250, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
+    EXPECT_NEAR(24.33, evapOutletNode.Temp, 0.01);
+    EXPECT_NEAR(0.0126, evapOutletNode.HumRat, 0.0001);
 
     // ok so now run at alternate mode, speed 2
     useAlternateMode = true;
     speedNum = 2;
     thisCoil.simulate(useAlternateMode, PLR, speedNum, speedRatio, fanOpMode);
-    EXPECT_NEAR(2700, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
-    EXPECT_NEAR(19.75, evapOutletNode.Temp, 0.01);
-    EXPECT_NEAR(0.0086, evapOutletNode.HumRat, 0.0001);
+//    std::cout << thisCoil.totalCoolingEnergyRate << ',' << evapOutletNode.Temp << ',' << evapOutletNode.HumRat << std::endl;
+    EXPECT_NEAR(4500, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
+    EXPECT_NEAR(20.39, evapOutletNode.Temp, 0.01);
+    EXPECT_NEAR(0.0111, evapOutletNode.HumRat, 0.0001);
+}
+
+
+TEST_F( CoilCoolingDXTest, CoilCoolingDXAlternateModePerformanceHitsSaturation )
+{
+    std::string idf_objects = delimited_string({
+                                                       "  Coil:Cooling:DX,",
+                                                       "    Coil,",
+                                                       "    Evaporator Inlet Node,Evaporator Outlet Node,",
+                                                       "    ,,",
+                                                       "    Condenser Inlet Node,Condenser Outlet Node,",
+                                                       "    Coil Performance,",
+                                                       "    ,;",
+
+                                                       "  Coil:Cooling:DX:CurveFit:Performance,",
+                                                       "    Coil Performance,,,,,,,,,Electricity,Coil Mode 1,Coil Mode 2;",
+
+                                                       "  Coil:Cooling:DX:CurveFit:OperatingMode,",
+                                                       "    Coil Mode 1,",
+                                                       "    10000,   !- Rated Gross Total Cooling Capacity {W}",
+                                                       "    0.25,   !- Rated Evaporator Air Flow Rate {m3/s}",
+                                                       "    ,,,,,,,,",
+                                                       "    2,Coil Mode 1 Speed 1,Coil Mode 1 Speed 2;",
+
+                                                       "  Coil:Cooling:DX:CurveFit:Speed,",
+                                                       "    Coil Mode 1 Speed 1,     !- Name",
+                                                       "    0.50,                    !- Gross Total Cooling Capacity Fraction",
+                                                       "    0.50,                    !- Evaporator Air Flow Rate Fraction",
+                                                       "    ,                        !- Condenser Air Flow Rate Fraction",
+                                                       "    0.7,                     !- Gross Sensible Heat Ratio",
+                                                       "    3,                       !- Gross Cooling COP {W/W}",
+                                                       "    0.5,                     !- Active Fraction of Coil Face Area",
+                                                       "    ,,,,,,,,,,,;",
+
+                                                       "  Coil:Cooling:DX:CurveFit:Speed,",
+                                                       "    Coil Mode 1 Speed 2,     !- Name",
+                                                       "    1.0,                     !- Gross Total Cooling Capacity Fraction",
+                                                       "    1.0,                     !- Evaporator Air Flow Rate Fraction",
+                                                       "    ,                        !- Condenser Air Flow Rate Fraction",
+                                                       "    0.7,                     !- Gross Sensible Heat Ratio",
+                                                       "    3,                       !- Gross Cooling COP {W/W}",
+                                                       "    1.0,                     !- Active Fraction of Coil Face Area",
+                                                       "    ,,,,,,,,,,,;",
+
+                                                       "  Coil:Cooling:DX:CurveFit:OperatingMode,",
+                                                       "    Coil Mode 2,",
+                                                       "    10000,   !- Rated Gross Total Cooling Capacity {W}",
+                                                       "    0.25,   !- Rated Evaporator Air Flow Rate {m3/s}",
+                                                       "    ,,,,,,,,",
+                                                       "    2,Coil Mode 2 Speed 1,Coil Mode 2 Speed 2;",
+
+                                                       "  Coil:Cooling:DX:CurveFit:Speed,",
+                                                       "    Coil Mode 2 Speed 1,     !- Name",
+                                                       "    0.45,                    !- Gross Total Cooling Capacity Fraction",
+                                                       "    0.50,                    !- Evaporator Air Flow Rate Fraction",
+                                                       "    ,                        !- Condenser Air Flow Rate Fraction",
+                                                       "    0.6,                     !- Gross Sensible Heat Ratio",
+                                                       "    2.7,                     !- Gross Cooling COP {W/W}",
+                                                       "    0.5,                     !- Active Fraction of Coil Face Area",
+                                                       "    ,,,,,,,,,,,;",
+
+                                                       "  Coil:Cooling:DX:CurveFit:Speed,",
+                                                       "    Coil Mode 2 Speed 2,     !- Name",
+                                                       "    0.9,                     !- Gross Total Cooling Capacity Fraction",
+                                                       "    1.0,                     !- Evaporator Air Flow Rate Fraction",
+                                                       "    ,                        !- Condenser Air Flow Rate Fraction",
+                                                       "    0.6,                     !- Gross Sensible Heat Ratio",
+                                                       "    2.7,                     !- Gross Cooling COP {W/W}",
+                                                       "    1.0,                     !- Active Fraction of Coil Face Area",
+                                                       "    ,,,,,,,,,,,;"
+                                               });
+    EXPECT_TRUE(process_idf( idf_objects, false ));
+    int coilIndex = CoilCoolingDX::factory("Coil");
+    auto &thisCoil(coilCoolingDXs[coilIndex]);
+
+    // fix the inlet conditions
+    auto &evapInletNode = DataLoopNode::Node(thisCoil.evapInletNodeIndex);
+    auto &condInletNode = DataLoopNode::Node(thisCoil.condInletNodeIndex);
+    evapInletNode.Temp = 28.5;
+    evapInletNode.Press = 101325;
+    evapInletNode.HumRat = 0.014;
+    evapInletNode.Enthalpy = Psychrometrics::PsyHFnTdbW(evapInletNode.Temp, evapInletNode.HumRat);
+    condInletNode.Temp = 35.0;
+    condInletNode.Press = 101325;
+    condInletNode.HumRat = 0.008;
+    condInletNode.Enthalpy = Psychrometrics::PsyHFnTdbW(condInletNode.Temp, condInletNode.HumRat);
+
+    // size it
+    thisCoil.size();
+
+    // for speed > 1 we use the mshp rated high speed flow...
+    DataHVACGlobals::MSHPMassFlowRateHigh = thisCoil.performance.normalMode.speeds.back().RatedAirMassFlowRate;
+
+    // we'll use this later
+    auto &evapOutletNode = DataLoopNode::Node(thisCoil.evapOutletNodeIndex);
+
+    bool setExpectations = true;
+
+    // set some values to run at rated conditions and call to run normal mode speed 1
+    evapInletNode.MassFlowRate = thisCoil.performance.normalMode.speeds.front().RatedAirMassFlowRate;
+    bool useAlternateMode = false;
+    Real64 PLR = 1.0;
+    int speedNum = 1;
+    Real64 speedRatio = 1.0;
+    int fanOpMode = 1;
+    thisCoil.simulate(useAlternateMode, PLR, speedNum, speedRatio, fanOpMode);
+    if (!setExpectations) {
+        std::cout << thisCoil.totalCoolingEnergyRate << ',' << evapOutletNode.Temp << ',' << evapOutletNode.HumRat << std::endl;
+    } else {
+        EXPECT_NEAR(5000, thisCoil.totalCoolingEnergyRate, 0.1); // expect the coil to run full out, at speed 1
+        EXPECT_NEAR(10.247, evapOutletNode.Temp, 0.01);
+        EXPECT_NEAR(0.00774, evapOutletNode.HumRat, 0.0001);
+    }
+    // alter values and run at rated conditions normal mode speed 2
+    evapInletNode.MassFlowRate = thisCoil.performance.normalMode.speeds.back().RatedAirMassFlowRate;
+    speedNum = 2;
+    thisCoil.simulate(useAlternateMode, PLR, speedNum, speedRatio, fanOpMode);
+    if (!setExpectations) {
+        std::cout << thisCoil.totalCoolingEnergyRate << ',' << evapOutletNode.Temp << ',' << evapOutletNode.HumRat << std::endl;
+    } else {
+        EXPECT_NEAR(10000, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
+        EXPECT_NEAR(10.247, evapOutletNode.Temp, 0.01);
+        EXPECT_NEAR(0.00774, evapOutletNode.HumRat, 0.0001);
+    }
+
+    // ok so now run at alternate mode, speed 1
+    useAlternateMode = true;
+    speedNum = 1;
+    thisCoil.simulate(useAlternateMode, PLR, speedNum, speedRatio, fanOpMode);
+    if (!setExpectations) {
+        std::cout << thisCoil.totalCoolingEnergyRate << ',' << evapOutletNode.Temp << ',' << evapOutletNode.HumRat << std::endl;
+    } else {
+        EXPECT_NEAR(4500, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
+        EXPECT_NEAR(19.387, evapOutletNode.Temp, 0.01);
+        EXPECT_NEAR(0.0115, evapOutletNode.HumRat, 0.0001);
+    }
+
+    // ok so now run at alternate mode, speed 2
+    useAlternateMode = true;
+    speedNum = 2;
+    thisCoil.simulate(useAlternateMode, PLR, speedNum, speedRatio, fanOpMode);
+    if (!setExpectations) {
+        std::cout << thisCoil.totalCoolingEnergyRate << ',' << evapOutletNode.Temp << ',' << evapOutletNode.HumRat << std::endl;
+    } else {
+        EXPECT_NEAR(9000, thisCoil.totalCoolingEnergyRate, 0.01); // expect the coil to run full out, at speed 1
+        EXPECT_NEAR(12.163, evapOutletNode.Temp, 0.01);
+        EXPECT_NEAR(0.00833, evapOutletNode.HumRat, 0.0001);
+    }
 }
