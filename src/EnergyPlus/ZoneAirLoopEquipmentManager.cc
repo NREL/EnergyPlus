3de691191230846dd351700c1327026e872a77cd
nigusse <bnigusse@fsec.ucfedu>
2020-03-06 09:24:35 -0500

    Changed sensible output Calc for SingleDuctConstVolNoReheat Air Terminal

--- src/EnergyPlus/ZoneAirLoopEquipmentManager.cc
+++ src/EnergyPlus/ZoneAirLoopEquipmentManager.cc
@@ -824,7 +824,7 @@ namespace ZoneAirLoopEquipmentManager {
                 // Use old direct air method to avoid diffs for now
                 // the following equations are mathematically identical, but regression test results show tiny diffs 
                 // SysOutputProvided == m_dot * [h(t2, w1) - h(t1, w1)] ==  m_dot * cpair(w1) * (t2 - t1)
-                Real64 CpAirAvg = PsyCpAirFnW(SpecHumIn);
+                Real64 CpAirAvg = PsyCpAirFnW(SpecHumOut);
                 SysOutputProvided = Node(OutletNodeNum).MassFlowRate * CpAirAvg * (Node(OutletNodeNum).Temp - Node(ZoneAirNode).Temp);
 
             } else {
