c92288f0e842b51a9319430b65d7ee9cca33d393
Matt Mitchell <matt.mitchell@nrel.gov>
2020-04-09 16:26:30 -0600

    translating UserDefindedWindACAuto

--- testfiles/PythonPluginUserDefinedWindACAuto.py
+++ testfiles/PythonPluginUserDefinedWindACAuto.py
@@ -1,5 +1,5 @@
-import sys
 from pyenergyplus.plugin import EnergyPlusPlugin
+import math
 
 
 class Zone1WinACModel(EnergyPlusPlugin):
@@ -10,114 +10,158 @@ class Zone1WinACModel(EnergyPlusPlugin):
 
         # handles
         self.need_to_get_handles = True
-        self.prim_air_inlet_temp_handle = None
-        self.prim_air_inlet_hum_handle = None
-        self.outdoor_air_drybulb_handle = None
-        self.outdoor_air_hum_handle = None
-        self.sens_q_request_handle = None
-        self.oa_des_flow_handle = None
-        self.oa_rho_handle = None
-        self.des_mdot_handle = None
-        self.prim_air_cp_handle = None
-        self.des_cool_cap_handle = None
-        self.cool_setpoint_temp_handle = None
-        self.cool_coil_avail_sched_handle = None
+        self.Zone1WinAC_PrimAir_Tinlet_handle = None
+        self.Zone1WinAC_PrimAir_Winlet_handle = None
+        self.Zone1WinAC_OA_Tdb_handle = None
+        self.Zone1WinAC_OA_W_handle = None
+        self.Zone1WinAC_Qdot_Request_handle = None
+        self.Zone1_OADesign_Vdot_handle = None
+        self.Zone1WinAC_OA_rho_handle = None
+        self.Zone1_CoolDesign_Mdot_handle = None
+        self.Zone1WinAC_PrimAir_Cp_handle = None
+        self.Zone1_CoolDesign_Cap_handle = None
+        self.Zone1Cooling_Tstat_handle = None
+        self.COOLINGCOILAVAILSCHED_handle = None
+        self.psych = self.api.functional.psychrometrics()
 
     def on_user_defined_component_model(self) -> int:
 
         # setup
         if self.need_to_get_handles and self.api.exchange.api_data_fully_ready():
-            self.prim_air_inlet_temp_handle = self.api.exchange.get_internal_variable_handle(
+            self.Zone1WinAC_PrimAir_Tinlet_handle = self.api.exchange.get_internal_variable_handle(
                 "Inlet Temperature for Primary Air Connection",
                 "Zone1WindAC")
 
-            self.prim_air_inlet_hum_handle = self.api.exchange.get_internal_variable_handle(
+            self.Zone1WinAC_PrimAir_Winlet_handle = self.api.exchange.get_internal_variable_handle(
                 "Inlet Humidity Ratio for Primary Air Connection",
                 "Zone1WindAC")
 
-            self.outdoor_air_drybulb_handle = self.api.exchange.get_internal_variable_value(
+            self.Zone1WinAC_OA_Tdb_handle = self.api.exchange.get_internal_variable_value(
                 "Inlet Temperature for Secondary Air Connection",
                 "Zone1WindAC")
 
-            self.outdoor_air_hum_handle = self.api.exchange.get_internal_variable_value(
+            self.Zone1WinAC_OA_W_handle = self.api.exchange.get_internal_variable_value(
                 "Inlet Humidity Ratio for Secondary Air Connection",
                 "Zone1WindAC")
 
-            self.sens_q_request_handle = self.api.exchange.get_internal_variable_value(
+            self.Zone1WinAC_Qdot_Request_handle = self.api.exchange.get_internal_variable_value(
                 "Remaining Sensible Load to Cooling Setpoint",
                 "Zone1WindAC")
 
-            self.oa_des_flow_handle = self.api.exchange.get_internal_variable_value(
+            self.Zone1_OADesign_Vdot_handle = self.api.exchange.get_internal_variable_value(
                 "Zone Outdoor Air Design Volume Flow Rate",
                 "West Zone")
 
-            self.oa_rho_handle = self.api.exchange.get_internal_variable_value(
+            self.Zone1WinAC_OA_rho_handle = self.api.exchange.get_internal_variable_value(
                 "Inlet Density for Secondary Air Connection",
                 "Zone1WindAC")
 
-            self.des_mdot_handle = self.api.exchange.get_internal_variable_value(
+            self.Zone1_CoolDesign_Mdot_handle = self.api.exchange.get_internal_variable_value(
                 "Final Zone Design Cooling Air Mass Flow Rate",
                 "West Zone")
 
-            self.prim_air_cp_handle = self.api.exchange.get_internal_variable_value(
+            self.Zone1WinAC_PrimAir_Cp_handle = self.api.exchange.get_internal_variable_value(
                 "Inlet Specific Heat for Primary Air Connection",
                 "Zone1WindAC")
 
-            self.des_mdot_handle = self.api.exchange.get_variable_handle(
+            self.des_mdot_handle_2 = self.api.exchange.get_variable_handle(
                 "Final Zone Design Cooling Load",
                 "West Zone")
 
-            self.cool_setpoint_temp_handle = self.api.exchange.get_variable_handle(
+            self.Zone1Cooling_Tstat_handle = self.api.exchange.get_variable_handle(
                 "Zone Thermostat Cooling Setpoint Temperature",
                 "West Zone")
 
-            self.cool_coil_avail_sched_handle = self.api.exchange.get_variable_handle(
+            self.COOLINGCOILAVAILSCHED_handle = self.api.exchange.get_variable_handle(
                 "Schedule Value",
                 "COOLINGCOILAVAILSCHED")
+
+            self.OA_Press_handle = self.api.exchange.get_variable_handle(
+                "Site Outdoor Air Barometric Pressure",
+                "Environment"
+            )
 
         else:
             return 0
 
         # init block
-        prim_air_inlet_temp = self.api.exchange.get_internal_variable_value(self.prim_air_inlet_temp_handle)
-        prim_air_inlet_hum = self.api.exchange.get_internal_variable_value(self.prim_air_inlet_hum_handle)
-        outdoor_air_drybulb = self.api.exchange.get_internal_variable_value(self.outdoor_air_drybulb_handle)
-        outdoor_air_hum = self.api.exchange.get_internal_variable_value(self.outdoor_air_hum_handle)
-        sens_q_request = self.api.exchange.get_internal_variable_value(self.sens_q_request_handle)
-        oa_des_flow = self.api.exchange.get_internal_variable_value(self.oa_des_flow_handle)
-        oa_rho = self.api.exchange.get_internal_variable_value(self.oa_rho_handle)
-        des_mdot = self.api.exchange.get_internal_variable_value(self.des_mdot_handle)
-        prim_air_cp = self.api.exchange.get_internal_variable_value(self.prim_air_cp_handle)
-        fan_eff = 0.5
-        fan_delta_p = 75.0
-        des_cool_cap = self.api.exchange.get_internal_variable_value(self.des_cool_cap_handle)
-        rated_cap = des_cool_cap / 0.75
-        rated_eir = 1.0 / 3.0
-        cool_setpoint_temp = self.api.exchange.get_variable_value(self.cool_setpoint_temp_handle)
+        Zone1WinAC_PrimAir_Tinlet = self.api.exchange.get_internal_variable_value(self.Zone1WinAC_PrimAir_Tinlet_handle)
+        PrimAir_Tinlet = Zone1WinAC_PrimAir_Tinlet
+
+        Zone1WinAC_PrimAir_Winlet = self.api.exchange.get_internal_variable_value(self.Zone1WinAC_PrimAir_Winlet_handle)
+        PrimAir_Winlet = Zone1WinAC_PrimAir_Winlet
+
+        Zone1WinAC_OA_Tdb = self.api.exchange.get_internal_variable_value(self.Zone1WinAC_OA_Tdb_handle)
+        OA_Tdb = Zone1WinAC_OA_Tdb
+
+        Zone1WinAC_OA_W = self.api.exchange.get_internal_variable_value(self.Zone1WinAC_OA_W_handle)
+        OA_W = Zone1WinAC_OA_W
+
+        Zone1WinAC_Qdot_Request = self.api.exchange.get_internal_variable_value(self.Zone1WinAC_Qdot_Request_handle)
+        Qdot_Request = Zone1WinAC_Qdot_Request
+
+        Zone1_OADesign_Vdot = self.api.exchange.get_internal_variable_value(self.Zone1_OADesign_Vdot_handle)
+        Zone1WinAC_OA_rho = self.api.exchange.get_internal_variable_value(self.Zone1WinAC_OA_rho_handle)
+        AirOAMdotDesign = Zone1_OADesign_Vdot * Zone1WinAC_OA_rho
+
+        Zone1_CoolDesign_Mdot = self.api.exchange.get_internal_variable_value(self.Zone1_CoolDesign_Mdot_handle)
+        AirSupMdotDesign = Zone1_CoolDesign_Mdot
+
+        Zone1WinAC_PrimAir_Cp = self.api.exchange.get_internal_variable_value(self.Zone1WinAC_PrimAir_Cp_handle)
+        AirCp = Zone1WinAC_PrimAir_Cp
+        FanEff = 0.5
+        FanDeltaP = 75.0
+        Zone1_CoolDesign_Cap = self.api.exchange.get_internal_variable_value(self.Zone1_CoolDesign_Cap_handle)
+        RatedCap = Zone1_CoolDesign_Cap / 0.75
+        RatedEIR = 1.0 / 3.0
+        Zone1Cooling_Tstat = self.api.exchange.get_variable_value(self.Zone1Cooling_Tstat_handle)
+        ZoneCoolTstat = Zone1Cooling_Tstat
 
         # sim block
-        q_dot_req = 0.0
-        if sens_q_request < 0.0:
-            q_dot_req = sens_q_request
+        QdotReq = 0.0
+        if Qdot_Request < 0.0:
+            QdotReq = Qdot_Request
 
-        if self.api.exchange.get_variable_value(self.cool_coil_avail_sched_handle) == 0.0:
-            q_dot_req = 0.0
+        if self.api.exchange.get_variable_value(self.COOLINGCOILAVAILSCHED_handle) == 0.0:
+            QdotReq = 0.0
 
-        prim_air_mdot_in = 0.0
-        prim_air_mdot_out = 0.0
-        prim_air_outlet_temp = 0.0
-        prim_air_outlet_hum = 0.0
-        tot_cool_power = 0.0
-        elect_power = 0.0
-        elect_energy = 0.0
-        oa_mdot_in = 0.0
-        if q_dot_req == 0.0:
-            prim_air_outlet_temp = prim_air_inlet_temp
-            prim_air_outlet_hum = prim_air_inlet_hum
-            tot_cool_power = 0.0
-            elect_power = 0.0
-            elect_energy = 0.0
-            oa_mdot_in = 0.0
+        PrimAir_MdotIn = 0.0
+        PrimAir_MdotOut = 0.0
+        PrimAir_Tout = 0.0
+        PrimAir_Wout = 0.0
+        tot_cool_Power = 0.0
+        ElectPower = 0.0
+        ElectEnergy = 0.0
+        OA_MdotIn = 0.0
+        if QdotReq == 0.0:
+            PrimAir_Tout = PrimAir_Tinlet
+            PrimAir_Wout = PrimAir_Winlet
+            tot_cool_Power = 0.0
+            ElectPower = 0.0
+            ElectEnergy = 0.0
+            OA_MdotIn = 0.0
         else:
+            Recirc_Mdot = AirSupMdotDesign - AirOAMdotDesign
+            Recirc_H = self.psych.enthalpy(PrimAir_Tinlet, PrimAir_Winlet)
+            OA_H = self.psych.enthalpy(OA_Tdb, OA_W)
+            Mix_H = ((Recirc_Mdot * Recirc_H) + (AirOAMdotDesign * OA_H)) / AirSupMdotDesign
+            Mix_W = ((Recirc_Mdot * PrimAir_Winlet) + (AirOAMdotDesign * OA_W)) / AirSupMdotDesign
+            Mix_Tdb = self.psych.dry_bulb(Mix_H, Mix_W)
+            Mix_Cp = self.psych.specific_heat(Mix_W)
+            OA_Press = self.api.exchange.get_variable_value(self.OA_Press_handle)
+            Mix_rho = self.psych.density(OA_Press, Mix_Tdb, Mix_W)
+            FanPower = (AirSupMdotDesign * FanDeltaP) / (FanEff * Mix_rho)
+            FanPowerToAir = FanPower
+            FanDeltaH = FanPowerToAir / AirSupMdotDesign
+            FanOut_H = Mix_H + FanDeltaH
+            FanOut_W = Mix_W
+            FanOut_Tdb = self.psych.dry_bulb(FanOut_H, FanOut_W)
+            FanOut_Twb = self.psych.wet_bulb(FanOut_Tdb, FanOut_W, OA_Press)
+            RatedCBF = 0.12666
+            LogRatedCBF = math.log(RatedCBF)
+            A0 = (0.0 - 1.0) * LogRatedCBF * AirSupMdotDesign
+            ADiff = (0.0 - 1.0) * (A0 / AirSupMdotDesign)
+            CBF = math.exp(ADiff)
+            dummy = 0.0
 
         return 0
