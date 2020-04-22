eaef2be6941ccf21a3a207ce77bd4885e5e775b4
Edwin Lee <leeed2001@gmail.com>
2020-04-22 07:28:30 -0500

    Tidying up docs

--- src/EnergyPlus/api/docs/python/conf.py
+++ src/EnergyPlus/api/docs/python/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', '..', '..', '..', 'builds', 'r', 'Products')))
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..', '..', '..', '..', 'builds', 'develop', 'r', 'Products')))
 
 # -- Project information -----------------------------------------------------
@@ -28,6 +29,9 @@ version = u''
 # The full version, including alpha/beta/rc tags
 release = u'0.1'
 
+extra_nav_links = {
+    'Blah': 'https://energyplus.net'
+}
 
 # -- General configuration ---------------------------------------------------
 
@@ -46,7 +50,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['.templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -77,8 +81,10 @@ pygments_style = None
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_theme = 'haiku'
+html_logo = '../../../../../release/favicon.png'
+html_show_sphinx = False
+# html_show_copyright = False
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -89,7 +95,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -99,8 +105,14 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
-
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+    ]
+}
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
