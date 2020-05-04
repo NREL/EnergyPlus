f1a33275b4c1fb97cb44d62f02e29a1c461e286b
Edwin Lee <leeed2001@gmail.com>
2020-05-04 14:58:31 -0500

    Update variable used for trending

--- testfiles/PythonPluginLrgOff_GridStorageSmoothing.py
+++ testfiles/PythonPluginLrgOff_GridStorageSmoothing.py
@@ -7,9 +7,8 @@ class BatteryControlDemandDemo(EnergyPlusPlugin):
         self.data['current_demand'] = self.api.exchange.get_variable_handle(
             "Facility Total Electric Demand Power", "Whole Building"
         )
-        self.data["trend"] = self.api.exchange.get_trend_handle(
-            "CurntFacilityElectDemandTrend"
-        )
+        self.data["trend"] = self.api.exchange.get_trend_handle("CurntFacilityElectDemandTrend")
+        self.data["var"] = self.api.exchange.get_global_handle("CurntFacilityElectDemand")
         self.data["discharge_rate"] = self.api.exchange.get_actuator_handle(
             "Electrical Storage", "Power Draw Rate", "Grid Battery Load Center"
         )
@@ -19,7 +18,9 @@ class BatteryControlDemandDemo(EnergyPlusPlugin):
 
     def handles_are_valid(self):
         handles = [
-            self.data['current_demand'], self.data["trend"], self.data["discharge_rate"], self.data["charge_rate"]
+            self.data['current_demand'], self.data["trend"],
+            self.data["discharge_rate"], self.data["charge_rate"],
+            self.data["var"]
         ]
         return all([x > -1 for x in handles])
 
@@ -35,6 +36,7 @@ class BatteryControlDemandDemo(EnergyPlusPlugin):
         elif current_facility_elect_demand < (avg_facility_demand * 0.95):
             charge_rate = (avg_facility_demand - current_facility_elect_demand) * dampen_factor
             self.api.exchange.set_actuator_value(self.data["charge_rate"], charge_rate)
+        self.api.exchange.set_global_value(self.data['var'], current_facility_elect_demand)
 
     def on_begin_zone_timestep_before_init_heat_balance(self) -> int:
         if 'current_demand' not in self.data or self.data['current_demand'] == -1:
