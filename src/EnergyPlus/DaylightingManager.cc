bc09c629aa500d8ce544b6e4228c3653cb388e45
Matthew Larson <matthew.larson@bigladdersoftware.com>
2020-06-10 09:09:22 -0600

    Change fatal error to warning if TDDs are modeled but no Daylighting:Controls object is found.

--- src/EnergyPlus/DaylightingManager.cc
+++ src/EnergyPlus/DaylightingManager.cc
@@ -5581,13 +5581,7 @@ namespace DaylightingManager {
                     }
                 }
                 if (!daylightControlFound) {
-                    ShowWarningError("DaylightingDevice:Tubular = " + TDDPipe(PipeNum).Name + ":  is not connected to a Zone that has Daylighting.  ");
-//                    ShowContinueError("Add Daylighting:Controls one of the following zones in the same enclosure:  ");
-//                    for (int const zoneNum2 : DataViewFactorInformation::ZoneSolarInfo(Surface(SurfNum).SolarEnclSurfIndex).ZoneNums) {
-//                        ShowContinueError(Zone(zoneNum2).Name);
-//                    }
-//                    ErrorsFound = true;
-//                    CheckTDDZone(Surface(SurfNum).Zone) = false;
+                    ShowWarningError("DaylightingDevice:Tubular = " + TDDPipe(PipeNum).Name + ":  is not connected to a Zone that has Daylighting, no visible transmittance will be modeled through the daylighting device.  ");
                 }
 
             } else { // SurfNum == 0
