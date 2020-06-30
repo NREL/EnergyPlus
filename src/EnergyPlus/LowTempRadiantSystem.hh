f150c066be601dafe64b0ace2d94d3ec417893fe
Matt Mitchell <matt.mitchell@nrel.gov>
2020-06-30 10:19:57 -0600

    add back squashed forward declare to fix build errors

--- src/EnergyPlus/LowTempRadiantSystem.hh
+++ src/EnergyPlus/LowTempRadiantSystem.hh
@@ -58,6 +58,7 @@
 namespace EnergyPlus {
     // Forward declarations
     struct EnergyPlusData;
+    struct ConvectionCoefficientsData;
     struct ZoneTempPredictorCorrectorData;
 
 namespace LowTempRadiantSystem {
