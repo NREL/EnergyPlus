a584c394cbba1fdfb7970cd6b963b33e2da7d2dc
brianlball <brian.ball@nrel.gov>
2020-07-07 21:27:37 -0600

    basedata

--- src/EnergyPlus/WaterUse.hh
+++ src/EnergyPlus/WaterUse.hh
@@ -52,6 +52,7 @@
 #include <ObjexxFCL/Array1D.hh>
 
 // EnergyPlus Headers
+#include <EnergyPlus/Data/BaseData.hh>
 #include <EnergyPlus/DataGlobals.hh>
 #include <EnergyPlus/EnergyPlus.hh>
 #include <EnergyPlus/PlantComponent.hh>
