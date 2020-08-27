a90fa994cc82105ceff1cfe33c8d998fb8e039d5
Edwin Lee <leeed2001@gmail.com>
2020-08-27 10:22:58 -0500

    Modify fixup script to find Python prereq properly

--- cmake/PythonFixUpOnMac.cmake
+++ cmake/PythonFixUpOnMac.cmake
@@ -12,10 +12,10 @@
 #  this library was added to detach energyplus from a hard dependency on the python dylib, and e+ now instead lazy loads this interface
 #  this wrapper depends on the core python dll at /some/path/to/Python.framework/Versions/3.7/Python
 #  we are packing up the python lib with E+, so it just needs to look for it at @executable_path/Python
-#  this library also depends on itself, so we may need to change @rpath/libpythonwrapper to @executable_path/libpythonwrapper
+#  we also change the -id from @rpath/libpythonwrapper to @executable_path/libpythonwrapper
 # Python, the actual python library
 #  this is the main python dynamic library that we distribute with e+
-#  it depends on itself, so we need to convert /some/path/to/Python.framework/Versions/3.7/Python to @executable_path/Python
+#  we just need to change the -id from /some/path/to/Python.framework/Versions/3.7/Python to @executable_path/Python
 
 # the paths are dynamic, so we need to build out all these change commands somewhat dynamically
 # the only thing we have to go on is that we know:
@@ -36,4 +36,13 @@ set(PYTHON_WRAPPER_PATH "${BASE_PATH}/${WRAPPER_FILE_NAME}")
 # now just fix up the pieces we need to fix up
 execute_process(COMMAND "install_name_tool" -id "@executable_path/${PYTHON_LIB_FILENAME}" "${LOCAL_PYTHON_LIBRARY}")
 execute_process(COMMAND "install_name_tool" -id "@executable_path/${WRAPPER_FILE_NAME}" "${PYTHON_WRAPPER_PATH}")
-execute_process(COMMAND "install_name_tool" -change "${RESOLVED_PYTHON_LIB}" "@executable_path/${PYTHON_LIB_FILENAME}" "${PYTHON_WRAPPER_PATH}")
+
+# changing the libpythonwrapper Python prereq is a bit funny - we should search to get the exact string original
+include(GetPrerequisites)
+get_prerequisites("${PYTHON_WRAPPER_PATH}" PREREQUISITES 1 1 "" "")
+foreach(PREREQ IN LISTS PREREQUISITES)
+    string(FIND "${PREREQ}" "Python" PYTHON_IN_PREREQ)
+    if (NOT PYTHON_IN_PREREQ EQUAL -1)
+        execute_process(COMMAND "install_name_tool" -change "${PREREQ}" "@executable_path/${PYTHON_LIB_FILENAME}" "${PYTHON_WRAPPER_PATH}")
+    endif()
+endforeach()
