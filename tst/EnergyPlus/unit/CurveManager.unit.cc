897e1ea7e7875eecdbeb84f09b7514f61da005a1
Tony Scimone <ajscimone@live.com>
2020-09-02 13:10:32 -0600

    Change numeric types in csv carriage return unit test to be of size_t to eliminate compiler warnings.

--- tst/EnergyPlus/unit/CurveManager.unit.cc
+++ tst/EnergyPlus/unit/CurveManager.unit.cc
@@ -648,11 +648,11 @@ TEST_F(EnergyPlusFixture, CSV_CarriageReturns_Handling)
     std::vector<double> TestArray;
     std::size_t col = 2;
     std::size_t row = 1;
-    unsigned long expected_length = 168;
+    std::size_t expected_length = 168;
     TestArray = testTableFile.getArray(std::make_pair(col,row));
     EXPECT_EQ(TestArray.size(), expected_length );
 
-    for (int i=0; i<TestArray.size(); i++ ){
+    for (std::size_t i=0; i<TestArray.size(); i++ ){
         EXPECT_FALSE(std::isnan(TestArray[i]));
     }
 }
