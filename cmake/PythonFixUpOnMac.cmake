985dc1bce66c0c8a64aebcb862394ab6b7ffda53
Edwin Lee <leeed2001@gmail.com>
2020-09-24 21:01:10 -0500

    Fix up executable as well

--- cmake/PythonFixUpOnMac.cmake
+++ cmake/PythonFixUpOnMac.cmake
@@ -6,8 +6,10 @@
 #  it has a dependency on the libenergyplusapi dynamic library, and it will be looking for it at: @rpath/libenergyplusapi.X.Y.Z.dylib
 #  it will need to be changed to look for the api lib at @executable_path/libenergyplusapi.X.Y.Z.dylib
 #  this is handled separately
+#  however it also has a dependency on the core python dll at /some/path/to/Python.framework/Versions/3.7/{SomePythonLibName}
+#  we are packing up the python lib with E+, so ti just needs to look for it at @executable_path/{SomePythonLibName}
 # libenergyplusapi, the dynamic library
-#  this depends on the core python dll at /some/path/to/Python.framework/Versions/3.7/{SomePythonLibName}
+#  like the exe, this depends on the core python dll at /some/path/to/Python.framework/Versions/3.7/{SomePythonLibName}
 #  we are packing up the python lib with E+, so it just needs to look for it at @executable_path/{SomePythonLibName}
 # Python, or libpython3.7m.dylib, or whatever - the actual python library
 #  this is the main python dynamic library that we distribute with e+
@@ -16,12 +18,13 @@
 # the paths are dynamic, so we need to build out all these change commands somewhat dynamically
 # the only thing we have to go on is that we know:
 #   EXECUTABLE_PATH points to the current energyplus executable, and
+#   EPLUS_DYNAMIC_LIB_NAME is the file _name_ of the E+ API dylib (libenergyplusapi.X.Y.Z.dylib)
 #   RESOLVED_PYTHON_LIB points to the Python dynamic library
 
 message("PYTHON: Fixing up Python Dependencies on Mac")
 
-message("ENERGYPLUS API DYNAMIC LIB NAME: ${EPLUS_DYNAMIC_LIB_NAME}")
-message("RESOLVED PYTHON LIB: ${RESOLVED_PYTHON_LIB}")
+# message("ENERGYPLUS API DYNAMIC LIB NAME: ${EPLUS_DYNAMIC_LIB_NAME}")
+# message("RESOLVED PYTHON LIB: ${RESOLVED_PYTHON_LIB}")
 
 # get the python lib filename
 get_filename_component(PYTHON_LIB_FILENAME ${RESOLVED_PYTHON_LIB} NAME)
@@ -42,5 +45,14 @@ foreach(PREREQ IN LISTS PREREQUISITES)
     string(FIND "${PREREQ}" "${PYTHON_LIB_FILENAME}" PYTHON_IN_PREREQ)
     if (NOT PYTHON_IN_PREREQ EQUAL -1)
         execute_process(COMMAND "install_name_tool" -change "${PREREQ}" "@executable_path/${PYTHON_LIB_FILENAME}" "${ENERGYPLUS_API_PATH}")
+    endif()
+endforeach()
+
+# and the exact same thing with the energyplus binary itself
+get_prerequisites("${EXECUTABLE_PATH}" PREREQUISITES 1 1 "" "")
+foreach(PREREQ IN LISTS PREREQUISITES)
+    string(FIND "${PREREQ}" "${PYTHON_LIB_FILENAME}" PYTHON_IN_PREREQ)
+    if (NOT PYTHON_IN_PREREQ EQUAL -1)
+        execute_process(COMMAND "install_name_tool" -change "${PREREQ}" "@executable_path/${PYTHON_LIB_FILENAME}" "${EXECUTABLE_PATH}")
     endif()
 endforeach()
