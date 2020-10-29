da60d38d1f14e9ed39aeb6e01ffd2189d2aafc4b
Matt Mitchell <matt.mitchell@nrel.gov>
2020-10-29 16:00:24 -0600

    minor workflow cleanups to the python plugin test file

--- testfiles/PythonPlugin1ZoneUncontrolledCondFD.py
+++ testfiles/PythonPlugin1ZoneUncontrolledCondFD.py
@@ -5,20 +5,24 @@ class CondFDSurfaceManager(EnergyPlusPlugin):
 
     def __init__(self):
         super().__init__()
-        self.cond_fd_ready = False
         self.surf_cond_handle = None
+        self.need_to_get_handles = True
+
+    def get_handles(self, state):
+        self.surf_cond_handle = self.api.exchange.get_actuator_handle(state,
+                                                                      "CondFD Surface Material Layer",
+                                                                      "Thermal Conductivity",
+                                                                      "ZN001:WALL001:WallStudAndCavityk1")
+
+        self.need_to_get_handles = False
 
     def on_begin_timestep_before_predictor(self, state) -> int:
-        # check if API ready
-        if self.api.exchange.cond_fd_ready(state):
-            self.surf_cond_handle = self.api.exchange.get_actuator_handle(state,
-                                                                          "CondFD Surface Material Layer",
-                                                                          "Thermal Conductivity",
-                                                                          "ZN001:WALL001:WallStudAndCavityk1")
 
-            self.cond_fd_ready = True
-        else:
-            return 0
+        if self.need_to_get_handles:
+            if self.api.exchange.cond_fd_ready(state) and self.api.exchange.api_data_fully_ready(state):
+                self.get_handles(state)
+            else:
+                return 0
 
         print(self.surf_cond_handle)
 
