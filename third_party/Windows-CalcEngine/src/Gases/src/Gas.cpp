2db34ed73ad445f8ce4ff8be825ba0c13b0d8ae9
Julien Marrec <julien.marrec@gmail.com>
2020-11-26 09:22:55 +0100

    Fix warnings from Xcode 12.0

--- third_party/Windows-CalcEngine/src/Gases/src/Gas.cpp
+++ third_party/Windows-CalcEngine/src/Gases/src/Gas.cpp
@@ -256,7 +256,7 @@ namespace Gases {
 
 	CGas& CGas::operator=( CGas const& t_Gas ) {
 		m_GasItem.clear();
-		for ( auto const item : t_Gas.m_GasItem ) {
+		for ( auto const &item : t_Gas.m_GasItem ) {
 			m_GasItem.push_back( item );
 		}
 		m_SimpleProperties = t_Gas.m_SimpleProperties;
