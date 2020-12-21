148f7e7bb310d9ed0721031788a1aa2a2f92349c
Noel Merket <noel.merket@nrel.gov>
2020-12-21 13:46:15 -0700

    paring down what is built in ssc

--- third_party/ssc/ssc/ssc_equations.h
+++ third_party/ssc/ssc/ssc_equations.h
@@ -41,63 +41,63 @@ struct ssc_equation_entry{
  */
 
 static ssc_equation_entry ssc_equation_table [] = {
-        // Marine energy
-		{"me_array_cable_length", me_array_cable_length,
-            "Marine energy", me_array_cable_length_doc,
-            false, true},
-		{"mp_ancillary_services", mp_ancillary_services,
-            "Merchant plant", mp_ancillary_services_doc,
-            false, true},
-
-        // PV
-        {"Reopt_size_battery_post", Reopt_size_battery_params,
-            "Pvsamv1", Reopt_size_battery_params_doc,
-            false, true},
-        {"Reopt_size_battery_post", Reopt_size_battery_params,
-            "Pvwattsv7", Reopt_size_battery_params_doc,
-            false, true},
-
-        // Wind
-        {"Turbine_calculate_powercurve", Turbine_calculate_powercurve,
-            "Windpower", Turbine_calculate_powercurve_doc,
-            false, true},
-
-        // CSP
-        {"MSPT_System_Design_Equations", MSPT_System_Design_Equations,
-            "Tcsmolten_salt", MSPT_System_Design_Equations_doc,
-            true, false},
-        {"Tower_SolarPilot_Solar_Field_Equations", Tower_SolarPilot_Solar_Field_Equations,
-            "Tcsmolten_salt", Tower_SolarPilot_Solar_Field_Equations_doc,
-            true, false},
-        {"MSPT_Receiver_Equations", MSPT_Receiver_Equations,
-            "Tcsmolten_salt", MSPT_Receiver_Equations_doc,
-            true, false},
-        {"MSPT_System_Control_Equations", MSPT_System_Control_Equations,
-            "Tcsmolten_salt", MSPT_System_Control_Equations_doc,
-            true, false},
-        {"Tower_SolarPilot_Capital_Costs_MSPT_Equations", Tower_SolarPilot_Capital_Costs_MSPT_Equations,
-            "Tcsmolten_salt", Tower_SolarPilot_Capital_Costs_MSPT_Equations_doc,
-            true, false},
-        //{"Tower_SolarPilot_Capital_Costs_DSPT_Equations", Tower_SolarPilot_Capital_Costs_DSPT_Equations,
-        //    "Tcsdirect_steam", Tower_SolarPilot_Capital_Costs_DSPT_Equations_doc,
-        //    true, false},
-        //{"Tower_SolarPilot_Capital_Costs_ISCC_Equations", Tower_SolarPilot_Capital_Costs_ISCC_Equations,
-        //    "Tcsiscc", Tower_SolarPilot_Capital_Costs_ISCC_Equations_doc,
-        //    true, false},
-
-        // Single owner
-        {"Financial_Construction_Financing_Equations", Financial_Construction_Financing_Equations,
-            "Tcsmolten_salt", Financial_Construction_Financing_Equations_doc,
-            true, false},
-        {"Financial_Capacity_Payments_Equations", Financial_Capacity_Payments_Equations,
-            "Singleowner", Financial_Capacity_Payments_Equations_doc,
-            true, false},
-
-        // Utility Rate
-        {"ElectricityRates_format_as_URDBv7", ElectricityRates_format_as_URDBv7,
-            "UtilityRate5", ElectricityRates_format_as_URDBv7_doc,
-            false, true},
-        {nullptr, nullptr, nullptr, nullptr, false, false}
+//        // Marine energy
+//		{"me_array_cable_length", me_array_cable_length,
+//            "Marine energy", me_array_cable_length_doc,
+//            false, true},
+//		{"mp_ancillary_services", mp_ancillary_services,
+//            "Merchant plant", mp_ancillary_services_doc,
+//            false, true},
+//
+//        // PV
+//        {"Reopt_size_battery_post", Reopt_size_battery_params,
+//            "Pvsamv1", Reopt_size_battery_params_doc,
+//            false, true},
+//        {"Reopt_size_battery_post", Reopt_size_battery_params,
+//            "Pvwattsv7", Reopt_size_battery_params_doc,
+//            false, true},
+//
+//        // Wind
+//        {"Turbine_calculate_powercurve", Turbine_calculate_powercurve,
+//            "Windpower", Turbine_calculate_powercurve_doc,
+//            false, true},
+//
+//        // CSP
+//        {"MSPT_System_Design_Equations", MSPT_System_Design_Equations,
+//            "Tcsmolten_salt", MSPT_System_Design_Equations_doc,
+//            true, false},
+//        {"Tower_SolarPilot_Solar_Field_Equations", Tower_SolarPilot_Solar_Field_Equations,
+//            "Tcsmolten_salt", Tower_SolarPilot_Solar_Field_Equations_doc,
+//            true, false},
+//        {"MSPT_Receiver_Equations", MSPT_Receiver_Equations,
+//            "Tcsmolten_salt", MSPT_Receiver_Equations_doc,
+//            true, false},
+//        {"MSPT_System_Control_Equations", MSPT_System_Control_Equations,
+//            "Tcsmolten_salt", MSPT_System_Control_Equations_doc,
+//            true, false},
+//        {"Tower_SolarPilot_Capital_Costs_MSPT_Equations", Tower_SolarPilot_Capital_Costs_MSPT_Equations,
+//            "Tcsmolten_salt", Tower_SolarPilot_Capital_Costs_MSPT_Equations_doc,
+//            true, false},
+//        //{"Tower_SolarPilot_Capital_Costs_DSPT_Equations", Tower_SolarPilot_Capital_Costs_DSPT_Equations,
+//        //    "Tcsdirect_steam", Tower_SolarPilot_Capital_Costs_DSPT_Equations_doc,
+//        //    true, false},
+//        //{"Tower_SolarPilot_Capital_Costs_ISCC_Equations", Tower_SolarPilot_Capital_Costs_ISCC_Equations,
+//        //    "Tcsiscc", Tower_SolarPilot_Capital_Costs_ISCC_Equations_doc,
+//        //    true, false},
+//
+//        // Single owner
+//        {"Financial_Construction_Financing_Equations", Financial_Construction_Financing_Equations,
+//            "Tcsmolten_salt", Financial_Construction_Financing_Equations_doc,
+//            true, false},
+//        {"Financial_Capacity_Payments_Equations", Financial_Capacity_Payments_Equations,
+//            "Singleowner", Financial_Capacity_Payments_Equations_doc,
+//            true, false},
+//
+//        // Utility Rate
+//        {"ElectricityRates_format_as_URDBv7", ElectricityRates_format_as_URDBv7,
+//            "UtilityRate5", ElectricityRates_format_as_URDBv7_doc,
+//            false, true},
+//        {nullptr, nullptr, nullptr, nullptr, false, false}
 };
 
 
