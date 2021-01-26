220fd67c45e3f0979e29e244784a4a768647d333
jcyuan <jinchaoyuan@gmail.com>
2021-01-26 10:20:59 -0600

    Added a new test units for write veri sum summary table for dual units adaption. Compiles ok. Test passed.

--- tst/EnergyPlus/unit/OutputReportTabular.unit.cc
+++ tst/EnergyPlus/unit/OutputReportTabular.unit.cc
@@ -9079,3 +9079,248 @@ TEST_F(SQLiteFixture, WriteVeriSumTableAreasTest_DualUnits)
     EXPECT_NEAR(100.00 / areaConv, std::stod(tabularData[98][10]), 0.01);
     EXPECT_NEAR(114.72 / areaConv, std::stod(tabularData[103][10]), 0.01);
 }
+
+// Dual Unit test: Borrowed from Test for #6350 and #6469
+TEST_F(SQLiteFixture, WriteVeriSumTable_TestNotPartOfTotal_DualUnits)
+{
+    EnergyPlus::sqlite->sqliteBegin();
+    EnergyPlus::sqlite->createSQLiteSimulationsRecord(1, "EnergyPlus Version", "Current Time");
+
+    state->dataOutRptTab->displayTabularVeriSum = true;
+    state->dataEnvrn->Latitude = 12.3;
+    state->dataEnvrn->Longitude = 45.6;
+
+    TotSurfaces = 4;
+    Surface.allocate(TotSurfaces);
+
+    // walls
+    Surface(1).Class = SurfaceClass::Wall;
+    Surface(1).HeatTransSurf = true;
+    Surface(1).ExtBoundCond = ExternalEnvironment;
+    Surface(1).Azimuth = 0.;
+    Surface(1).GrossArea = 200.; // 20 x 10
+    Surface(1).Tilt = 90.;
+    Surface(1).Zone = 1;
+
+    Surface(2).Class = SurfaceClass::Wall;
+    Surface(2).HeatTransSurf = true;
+    Surface(2).ExtBoundCond = ExternalEnvironment;
+    Surface(2).Azimuth = 90.;
+    Surface(2).GrossArea = 300.; // 30 x 10
+    Surface(2).Tilt = 90.;
+    Surface(2).Zone = 2;
+
+    // windows
+    Surface(3).Class = SurfaceClass::Window;
+    Surface(3).HeatTransSurf = true;
+    Surface(3).ExtBoundCond = ExternalEnvironment;
+    Surface(3).Azimuth = 0.;
+    Surface(3).GrossArea = 40.;
+    Surface(3).Height = 5;
+    Surface(3).Width = 8;
+    Surface(3).Tilt = 90.;
+    Surface(3).Zone = 1;
+
+    Surface(4).Class = SurfaceClass::Window;
+    Surface(4).HeatTransSurf = true;
+    Surface(4).ExtBoundCond = ExternalEnvironment;
+    Surface(4).Azimuth = 90.;
+    Surface(4).GrossArea = 60.;
+    Surface(4).Height = 6;
+    Surface(4).Width = 10;
+    Surface(4).Tilt = 90.;
+    Surface(4).Zone = 2;
+
+    // Loads
+    DataHeatBalance::TotLights = 3;
+    Lights.allocate(DataHeatBalance::TotLights);
+
+    DataHeatBalance::TotPeople = 3;
+    People.allocate(DataHeatBalance::TotPeople);
+
+    DataHeatBalance::TotElecEquip = 3;
+    ZoneElectric.allocate(DataHeatBalance::TotElecEquip);
+
+    Lights(1).ZonePtr = 1;
+    Lights(1).DesignLevel = 1000.0;
+    Lights(2).ZonePtr = 2;
+    Lights(2).DesignLevel = 100.0;
+    Lights(3).ZonePtr = 3;
+    Lights(3).DesignLevel = 10.0;
+
+    People(1).ZonePtr = 1;
+    People(1).NumberOfPeople = 10.0;
+    People(2).ZonePtr = 2;
+    People(2).NumberOfPeople = 5.0;
+    People(3).ZonePtr = 3;
+    People(3).NumberOfPeople = 1.0;
+
+    ZoneElectric(1).ZonePtr = 1;
+    ZoneElectric(1).DesignLevel = 500.0;
+    ZoneElectric(2).ZonePtr = 2;
+    ZoneElectric(2).DesignLevel = 50.0;
+    ZoneElectric(3).ZonePtr = 3;
+    ZoneElectric(3).DesignLevel = 5.0;
+
+    // zone
+    state->dataGlobal->NumOfZones = 3;
+    Zone.allocate(state->dataGlobal->NumOfZones);
+    Zone(1).Name = "PartofTot Conditioned Zone";
+    Zone(1).SystemZoneNodeNumber = 1; // Conditioned
+    Zone(1).isPartOfTotalArea = true;
+    Zone(1).Multiplier = 1.;
+    Zone(1).ListMultiplier = 1.;
+    // 10x10x2
+    Zone(1).FloorArea = 1000.;
+    Zone(1).Volume = 2000.;
+    Zone(1).ExtGrossWallArea = 800.;
+    Zone(1).ExteriorTotalGroundSurfArea = 0;
+    Zone(1).ExtWindowArea = Surface(3).GrossArea + Surface(4).GrossArea;
+
+    Zone(2).Name = "PartofTot Unconditioned Zone";
+    Zone(2).SystemZoneNodeNumber = 0; // Unconditioned
+    Zone(2).isPartOfTotalArea = true;
+    Zone(2).Multiplier = 1.;
+    Zone(2).ListMultiplier = 1.;
+    // 10x10x2
+    Zone(2).FloorArea = 100.;
+    Zone(2).Volume = 200.;
+    Zone(2).ExtGrossWallArea = 80.;
+    Zone(2).ExteriorTotalGroundSurfArea = 0;
+    Zone(2).ExtWindowArea = 0.0;
+
+    Zone(3).Name = "NOT PartofTot Conditioned Zone";
+    Zone(3).SystemZoneNodeNumber = 1; // Conditioned
+    Zone(3).isPartOfTotalArea = false;
+    Zone(3).Multiplier = 1.;
+    Zone(3).ListMultiplier = 1.;
+    // 10x10x2
+    Zone(3).FloorArea = 10.;
+    Zone(3).Volume = 20.;
+    Zone(3).ExtGrossWallArea = 8.;
+    Zone(3).ExteriorTotalGroundSurfArea = 0;
+    Zone(3).ExtWindowArea = 0.0;
+
+    state->dataOutRptTab->unitsStyle = OutputReportTabular::iUnitsStyle::None;
+    state->dataOutRptTab->unitsStyle_SQLite = OutputReportTabular::iUnitsStyle::InchPound;
+
+    SetupUnitConversions(*state);
+    Real64 areaConv = getSpecificUnitDivider(*state, "m2", "ft2");
+    Real64 volConv = getSpecificUnitDivider(*state, "m3", "ft3");
+    Real64 enerConv = getSpecificUnitDivider(*state, "m3", "ft3");
+
+    std::string SIunit = "[W/m2]";
+    int unitConvIndex;
+    std::string Wm2_unitName;
+    Real64 Wm2_unitConv;
+    LookupSItoIP(*state, SIunit, unitConvIndex, Wm2_unitName);
+    Wm2_unitConv = ConvertIP(*state, unitConvIndex, 1.0);
+
+    WriteVeriSumTable(*state);
+
+    /***********************************************************************************************************************************************
+     *                                                              Check Yes/No flag                                                              *
+     ***********************************************************************************************************************************************/
+
+    // RowName, ColumnName, value
+    std::vector<std::tuple<std::string, std::string, std::string>> results_strings({
+        {Zone(1).Name, "Conditioned (Y/N)", "Yes"},
+        {Zone(1).Name, "Part of Total Floor Area (Y/N)", "Yes"},
+
+        {Zone(2).Name, "Conditioned (Y/N)", "No"},
+        {Zone(2).Name, "Part of Total Floor Area (Y/N)", "Yes"},
+
+        {Zone(3).Name, "Conditioned (Y/N)", "Yes"},
+        {Zone(3).Name, "Part of Total Floor Area (Y/N)", "No"},
+    });
+
+    // Would have used bind_text in sqlite3 with a single prepared statement, but m_db is protected in SQLiteProcedures
+    std::string rowName;
+    std::string columnName;
+
+    for (auto v : results_strings) {
+
+        rowName = std::get<0>(v);
+        columnName = std::get<1>(v);
+
+        std::string query("SELECT Value From TabularDataWithStrings"
+                          "  WHERE ReportName = 'InputVerificationandResultsSummary'"
+                          "  AND TableName = 'Zone Summary'"
+                          "  AND RowName = '" +
+                          rowName + "'" + "  AND ColumnName = '" + columnName + "'");
+
+        std::string flag = queryResult(query, "TabularDataWithStrings")[0][0];
+        // Not needed, we're just querying, not inside a transaction
+        // EnergyPlus::sqlite->sqliteCommit();
+
+        // Add informative message if failed
+        EXPECT_EQ(std::get<2>(v), flag) << "Failed for RowName=" << rowName << "; ColumnName=" << columnName;
+    }
+
+    /***********************************************************************************************************************************************
+     *                                                       Check each zone and total rows                                                        *
+     ***********************************************************************************************************************************************/
+
+    // RowName, ColumnName, value
+    std::vector<std::tuple<std::string, std::string, Real64>> results({
+        {Zone(1).Name, "Area", Zone(1).FloorArea/areaConv},
+        {Zone(2).Name, "Area", Zone(2).FloorArea/areaConv},
+        {Zone(3).Name, "Area", Zone(3).FloorArea/areaConv},
+        {"Total", "Area", (Zone(1).FloorArea + Zone(2).FloorArea)/areaConv},
+        {"Conditioned Total", "Area", Zone(1).FloorArea/areaConv},
+        {"Unconditioned Total", "Area", Zone(2).FloorArea/areaConv},
+        {"Not Part of Total", "Area", Zone(3).FloorArea/areaConv},
+
+        {Zone(1).Name, "Volume", Zone(1).Volume/volConv},
+        {Zone(2).Name, "Volume", Zone(2).Volume/volConv},
+        {Zone(3).Name, "Volume", Zone(3).Volume/volConv},
+        {"Total", "Volume", (Zone(1).Volume + Zone(2).Volume)/volConv},
+        {"Conditioned Total", "Volume", Zone(1).Volume/volConv},
+        {"Unconditioned Total", "Volume", Zone(2).Volume/volConv},
+        {"Not Part of Total", "Volume", Zone(3).Volume/volConv},
+
+        {Zone(1).Name, "Lighting", Lights(1).DesignLevel * Wm2_unitConv / Zone(1).FloorArea},
+        {Zone(2).Name, "Lighting", Lights(2).DesignLevel * Wm2_unitConv / Zone(2).FloorArea},
+        {Zone(3).Name, "Lighting", Lights(3).DesignLevel * Wm2_unitConv / Zone(3).FloorArea},
+        {"Total", "Lighting", (Lights(1).DesignLevel + Lights(2).DesignLevel) * Wm2_unitConv / (Zone(1).FloorArea + Zone(2).FloorArea)},
+        {"Conditioned Total", "Lighting", Lights(1).DesignLevel * Wm2_unitConv / Zone(1).FloorArea},
+        {"Unconditioned Total", "Lighting", Lights(2).DesignLevel * Wm2_unitConv / Zone(2).FloorArea},
+        {"Not Part of Total", "Lighting", Lights(3).DesignLevel * Wm2_unitConv / Zone(3).FloorArea},
+
+        // People/m^2
+        {Zone(1).Name, "People", Zone(1).FloorArea /areaConv / People(1).NumberOfPeople},
+        {Zone(2).Name, "People", Zone(2).FloorArea / areaConv / People(2).NumberOfPeople},
+        {Zone(3).Name, "People", Zone(3).FloorArea / areaConv / People(3).NumberOfPeople},
+        {"Total", "People", (Zone(1).FloorArea + Zone(2).FloorArea) / areaConv / (People(1).NumberOfPeople + People(2).NumberOfPeople)},
+        {"Conditioned Total", "People", Zone(1).FloorArea / areaConv / People(1).NumberOfPeople},
+        {"Unconditioned Total", "People", Zone(2).FloorArea / areaConv / People(2).NumberOfPeople},
+        {"Not Part of Total", "People", Zone(3).FloorArea / areaConv / People(3).NumberOfPeople},
+
+        {Zone(1).Name, "Plug and Process", ZoneElectric(1).DesignLevel * Wm2_unitConv / Zone(1).FloorArea},
+        {Zone(2).Name, "Plug and Process", ZoneElectric(2).DesignLevel * Wm2_unitConv / Zone(2).FloorArea},
+        {Zone(3).Name, "Plug and Process", ZoneElectric(3).DesignLevel * Wm2_unitConv / Zone(3).FloorArea},
+        {"Total", "Plug and Process", (ZoneElectric(1).DesignLevel + ZoneElectric(2).DesignLevel)*Wm2_unitConv / (Zone(1).FloorArea + Zone(2).FloorArea)},
+        {"Conditioned Total", "Plug and Process", ZoneElectric(1).DesignLevel*Wm2_unitConv / Zone(1).FloorArea},
+        {"Unconditioned Total", "Plug and Process", ZoneElectric(2).DesignLevel*Wm2_unitConv / Zone(2).FloorArea},
+        {"Not Part of Total", "Plug and Process", ZoneElectric(3).DesignLevel*Wm2_unitConv / Zone(3).FloorArea},
+    });
+
+    // Would have used bind_text in sqlite3 with a single prepared statement, but m_db is protected in SQLiteProcedures
+    for (auto v : results) {
+
+        rowName = std::get<0>(v);
+        columnName = std::get<1>(v);
+
+        std::string query("SELECT Value From TabularDataWithStrings"
+                          "  WHERE ReportName = 'InputVerificationandResultsSummary'"
+                          "  AND TableName = 'Zone Summary'"
+                          "  AND RowName = '" +
+                          rowName + "'" + "  AND ColumnName = '" + columnName + "'");
+
+        Real64 return_val = execAndReturnFirstDouble(query);
+        // EnergyPlus::sqlite->sqliteCommit();
+
+        // Add informative message if failed
+        EXPECT_NEAR(std::get<2>(v), return_val, 0.01) << "Failed for RowName=" << rowName << "; ColumnName=" << columnName;
+    }
+}
