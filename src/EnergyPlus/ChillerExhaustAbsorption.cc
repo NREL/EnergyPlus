0dec54470d6c4877db4d49011cb152a547e0ad78
jcyuan <jinchaoyuan@gmail.com>
2021-03-05 16:05:29 -0600

    Enum changes and common share propagated to both cases.

--- src/EnergyPlus/ChillerExhaustAbsorption.cc
+++ src/EnergyPlus/ChillerExhaustAbsorption.cc
@@ -131,14 +131,7 @@ namespace EnergyPlus::ChillerExhaustAbsorption {
 
     void ExhaustAbsorberSpecs::simulate(EnergyPlusData &state, const PlantLocation &calledFromLocation, bool FirstHVACIteration, Real64 &CurLoad, bool RunFlag)
     {
-        enum brLoopType
-        {
-            CHILLER,
-            HEATER,
-            CONDENSER,
-            NOMATCH
-        };
-        brLoopType brIdentity(NOMATCH);
+        DataPlant::BrLoopType brIdentity(DataPlant::BrLoopType::NoMatch);
 
         int branchTotalComp = state.dataPlnt->PlantLoop(calledFromLocation.loopNum)
                                   .LoopSide(calledFromLocation.loopSideNum)
@@ -155,30 +148,30 @@ namespace EnergyPlus::ChillerExhaustAbsorption {
 
             // Match inlet node name of calling branch to determine if this call is for heating or cooling
             if (compInletNodeNum == this->ChillReturnNodeNum) { // Operate as chiller
-                brIdentity = CHILLER; // chiller
+                brIdentity = DataPlant::BrLoopType::Chiller;    // chiller
                 break;
             } else if (compInletNodeNum == this->HeatReturnNodeNum) { // Operate as heater
-                brIdentity = HEATER; // heater
+                brIdentity = DataPlant::BrLoopType::Heater;          // heater
                 break;
             } else if (compInletNodeNum == this->CondReturnNodeNum) { // called from condenser loop
-                brIdentity = CONDENSER; // condenser
+                brIdentity = DataPlant::BrLoopType::Condenser;          // condenser
                 break;
             } else {
-                brIdentity = NOMATCH;
+                brIdentity = DataPlant::BrLoopType::NoMatch;
             }
         }
 
-        if (brIdentity == CHILLER) {
+        if (brIdentity == DataPlant::BrLoopType::Chiller) {
             this->InCoolingMode = RunFlag != 0;
             this->initialize(state);
             this->calcChiller(state, CurLoad);
             this->updateCoolRecords(CurLoad, RunFlag);
-        } else if (brIdentity == HEATER) {
+        } else if (brIdentity == DataPlant::BrLoopType::Heater) {
             this->InHeatingMode = RunFlag != 0;
             this->initialize(state);
             this->calcHeater(state, CurLoad, RunFlag);
             this->updateHeatRecords(CurLoad, RunFlag);
-        } else if (brIdentity == CONDENSER) {
+        } else if (brIdentity == DataPlant::BrLoopType::Condenser) {
             if (this->CDLoopNum > 0) {
                 PlantUtilities::UpdateChillerComponentCondenserSide(state,
                                                                     this->CDLoopNum,
