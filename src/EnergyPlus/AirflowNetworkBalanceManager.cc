13b657d115cb37829457b2499eec4def1ec21e33
LipingWang <liping.wang1979@gmail.com>
2021-03-22 12:18:56 -0600

    Removed the extra semicolon after }

--- src/EnergyPlus/AirflowNetworkBalanceManager.cc
+++ src/EnergyPlus/AirflowNetworkBalanceManager.cc
@@ -335,7 +335,7 @@ namespace AirflowNetworkBalanceManager {
                 Real64 temperature(20.0);
                 if (fields.find("reference_temperature") != fields.end()) { // not required field, has default value
                     temperature = fields.at("reference_temperature");
-                };
+                }
                 Real64 pressure(101325.0);
                 if (fields.find("reference_barometric_pressure") != fields.end()) { // not required field, has default value
                     pressure = fields.at("reference_barometric_pressure");
