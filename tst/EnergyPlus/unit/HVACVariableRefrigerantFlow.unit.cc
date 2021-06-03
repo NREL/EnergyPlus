d33a301754dff2414ba550a63ef2c7b3a5ba7890
Jermy Thomas <jermythomasv@gmail.com>
2021-06-02 21:18:36 -0600

    Correct documentation

--- tst/EnergyPlus/unit/HVACVariableRefrigerantFlow.unit.cc
+++ tst/EnergyPlus/unit/HVACVariableRefrigerantFlow.unit.cc
@@ -5755,7 +5755,6 @@ TEST_F(EnergyPlusFixture, VRFTest_SysCurve_WaterCooled)
     SizingManager::GetPlantSizingInput(*state);
     PlantManager::InitOneTimePlantSizingInfo(*state, 1);
     PlantManager::SizePlantLoop(*state, 1, true);
-//    state->dataPlantMgr->InitLoopEquip = true;
     // call air-side VRF
     bool HeatingActive = false;
     bool CoolingActive = false;
