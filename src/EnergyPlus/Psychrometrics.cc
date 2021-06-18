62cc2cbd96d6939292d5f9fec08294acff2585c6
Julien Marrec <julien.marrec@gmail.com>
2021-06-18 15:10:19 +0200

    Unrelated minor change: give me a spot to put a debugger breakpoint inside if

--- src/EnergyPlus/Psychrometrics.cc
+++ src/EnergyPlus/Psychrometrics.cc
@@ -499,7 +499,9 @@ namespace Psychrometrics {
         for (iter = 1; iter <= itmax; ++iter) {
 
             // Assigning a value to WBT
-            if (WBT >= (tBoil - 0.09)) WBT = tBoil - 0.1;
+            if (WBT >= (tBoil - 0.09)) {
+                WBT = tBoil - 0.1;
+            }
 
             // Determine the saturation pressure for wet bulb temperature
             PSatstar =
