83c9805ff1104bda3a573abb800a5dfbd0a009c1
Jermy Thomas <jermythomasv@gmail.com>
2021-07-20 12:08:57 -0600

    oneTimeInit PHeatTransfer.*

--- src/EnergyPlus/PipeHeatTransfer.hh
+++ src/EnergyPlus/PipeHeatTransfer.hh
@@ -224,6 +224,8 @@ namespace PipeHeatTransfer {
 
         void PushInnerTimeStepArrays();
 
+        void oneTimeInit_new(EnergyPlusData &state) override;
+
         void oneTimeInit(EnergyPlusData &state) override;
 
         void InitPipesHeatTransfer(EnergyPlusData &state, bool FirstHVACIteration);
