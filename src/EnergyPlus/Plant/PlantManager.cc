410a9d49f76c2ea6a313224cec10bd6294dc042b
Jermy Thomas <jermythomasv@gmail.com>
2021-10-04 08:24:53 -0600

    proof of concept - case sensitive Enums

--- src/EnergyPlus/Plant/PlantManager.cc
+++ src/EnergyPlus/Plant/PlantManager.cc
@@ -875,7 +875,7 @@ void GetPlantInput(EnergyPlusData &state)
                     this_comp.TypeOf = this_comp_type;
                     this_comp.location = EnergyPlus::PlantLocation(LoopNum, LoopSideNum, BranchNum, CompNum);
 
-                    this_comp.Type = static_cast<PlantEquipmentType>(getEnumerationValue(PlantEquipTypeNamesCC, this_comp_type));
+                    this_comp.Type = static_cast<PlantEquipmentType>(getEnumerationValue(PlantEquipTypeNames, UtilityRoutines::MakeUPPERCase(this_comp_type)));
 
                     switch (this_comp.Type) {
                     case (PlantEquipmentType::Pipe): {
