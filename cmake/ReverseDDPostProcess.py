44ab8c00c0a93082f11596434d2f5363a3d8bce8
Edwin Lee <leeed2001@gmail.com>
2021-10-18 11:12:29 -0500

    Cleanup rev dd script

--- cmake/ReverseDDPostProcess.py
+++ cmake/ReverseDDPostProcess.py
@@ -1,78 +1,50 @@
 import os
 import shutil
 import sys
-from typing import List
-
-if len(sys.argv) != 3:
-    print(f"Invalid arguments passed to {sys.argv[0]}")
-    sys.exit(1)
-base_idf_path = sys.argv[1]
-reverse_dd_test_dir = sys.argv[2]
-
-# find out ow many DD's there are based on the IDF
-num_design_days = 0
-with open(base_idf_path) as f:
-    for line in f:
-        if "SizingPeriod:DesignDay," in line:
-            num_design_days += 1
-
-# now start processing the reversed output file
-base_dir = os.path.join(reverse_dd_test_dir, 'Regular')
-base_csv = os.path.join(base_dir, 'eplusout.csv')
-base_mtr = os.path.join(base_dir, 'eplusmtr.csv')
-reversed_dir = os.path.join(reverse_dd_test_dir, 'Reversed')
-reversed_csv = os.path.join(reversed_dir, 'eplusout.csv')
-reversed_mtr = os.path.join(reversed_dir, 'eplusmtr.csv')
-reversed_csv_backup = os.path.join(reversed_dir, 'eplusout_backup.csv')
-reversed_mtr_backup = os.path.join(reversed_dir, 'eplusmtr_backup.csv')
-
-# read all lines from csv
-csv_contents = []
-with open(reversed_csv) as f:
-    for line in f:
-        csv_contents.append(line)
-
-meter_exists = os.path.exists(reversed_mtr)
-mtr_contents = []
-if meter_exists:
-    with open(reversed_mtr) as f:
-        for line in f:
-            mtr_contents.append(line)
-
-# now find out how many lines of data there are in each file and each environment
-csv_data_rows = len(csv_contents) - 1
-csv_data_rows_per_environment = int(csv_data_rows / num_design_days)
-mtr_data_rows = 0
-mtr_data_rows_per_environment = 0
-if meter_exists:
-    mtr_data_rows = len(mtr_contents) - 1
-    mtr_data_rows_per_environment = int(mtr_data_rows / num_design_days)
-
-# write the files back out in the appropriate order
-shutil.copy(reversed_csv, reversed_csv_backup)
-with open(reversed_csv, "w") as f:
-    f.write("%s" % csv_contents[0])
-    for row_num in range(csv_data_rows_per_environment + 1, 2 * csv_data_rows_per_environment + 1):
-        f.write("%s" % csv_contents[row_num])
-    for row_num in range(1, csv_data_rows_per_environment + 1):
-        f.write("%s" % csv_contents[row_num])
-    if num_design_days > 2:
-        for row_num in range(2 * csv_data_rows_per_environment + 1, csv_data_rows + 1):
-            f.write("%s" % csv_contents[row_num])
-if meter_exists:
-    shutil.copy(reversed_mtr, reversed_mtr_backup)
-    with open(reversed_mtr, "w") as f:
-        f.write("%s" % mtr_contents[0])
-        for row_num in range(mtr_data_rows_per_environment + 1, 2 * mtr_data_rows_per_environment + 1):
-            f.write("%s" % mtr_contents[row_num])
-        for row_num in range(1, mtr_data_rows_per_environment + 1):
-            f.write("%s" % mtr_contents[row_num])
-        if num_design_days > 2:
-            for row_num in range(2 * mtr_data_rows_per_environment + 1, mtr_data_rows + 1):
-                f.write("%s" % mtr_contents[row_num])
+from typing import List, TextIO, Tuple
 
 
-def get_numeric_token_list(file_object) -> List[float]:
+def process_command_arguments() -> Tuple[str, str]:
+    if len(sys.argv) != 3:
+        print(f"Invalid arguments passed to {sys.argv[0]}")
+        sys.exit(1)
+    return sys.argv[1], sys.argv[2]
+
+
+def determine_number_of_design_days(idf_path: str) -> int:
+    # find out ow many DD's there are based on the IDF
+    num = 0
+    with open(idf_path) as f_file:
+        for li in f_file:
+            if "SizingPeriod:DesignDay," in li:
+                num += 1
+    return num
+
+
+def configure_root_dirs(test_dir: str) -> Tuple[str, str]:
+    return (
+        os.path.join(test_dir, 'Regular'),
+        os.path.join(test_dir, 'Reversed')
+    )
+
+
+def configure_paths(b_dir: str, r_dir: str, filename: str) -> Tuple[str, str, str]:
+    return (
+        os.path.join(b_dir, f'{filename}.csv'),
+        os.path.join(r_dir, f'{filename}.csv'),
+        os.path.join(r_dir, f'{filename}_backup.csv')
+    )
+
+
+def get_file_lines(file_path: str) -> List[str]:
+    contents = []
+    with open(file_path) as f_file:
+        for li in f_file:
+            contents.append(li)
+    return contents
+
+
+def get_numeric_token_list(file_object: TextIO) -> List[float]:
     values = []
     for li in file_object.readlines():
         for v in li.split(','):
@@ -97,6 +69,29 @@ def lists_of_values_match(base: List[float], mod: List[float], tolerance: float 
     return True
 
 
+def write_processed_data(
+        rev_file: str, rev_file_backup: str, file_lines: List[str], rows_per_environment: int,
+        num_dds: int, num_rows: int) -> None:
+    shutil.copy(rev_file, rev_file_backup)
+    with open(rev_file, "w") as f_file:
+        f_file.write("%s" % file_lines[0])
+        for row in range(rows_per_environment + 1, 2 * rows_per_environment + 1):
+            f_file.write("%s" % file_lines[row])
+        for row in range(1, rows_per_environment + 1):
+            f_file.write("%s" % file_lines[row])
+        if num_dds > 2:
+            for row in range(2 * rows_per_environment + 1, num_rows + 1):
+                f_file.write("%s" % file_lines[row])
+
+
+def do_comparison(base_file: str, rev_file: str, rev_file_backup: str, num_dds: int) -> bool:
+    file_lines = get_file_lines(rev_file)
+    data_rows = len(file_lines) - 1
+    data_rows_per_environment = int(data_rows / num_dds)
+    write_processed_data(rev_file, rev_file_backup, file_lines, data_rows_per_environment, num_dds, data_rows)
+    return files_match(base_file, rev_file)
+
+
 def files_match(base_file_path: str, mod_file_path: str) -> bool:
     with open(base_file_path) as f_base:
         with open(mod_file_path) as f_reversed:
@@ -105,10 +100,29 @@ def files_match(base_file_path: str, mod_file_path: str) -> bool:
             return lists_of_values_match(base_values, reversed_values)
 
 
-# now ideally we would just call math diff at this point, but for today let's just diff them
-csv_match = files_match(base_csv, reversed_csv)
-mtr_match = files_match(base_mtr, reversed_mtr)
-if csv_match and mtr_match:
-    sys.exit(0)
-else:
-    sys.exit(1)
+def main():
+    # validate and get command line arguments
+    base_idf_path, reverse_dd_test_dir = process_command_arguments()
+
+    # find out ow many DD's there are based on the IDF
+    num_design_days = determine_number_of_design_days(base_idf_path)
+
+    # configure up all the paths
+    base_dir, reversed_dir = configure_root_dirs(reverse_dd_test_dir)
+
+    # handle the CSV file, check the original contents, write out the reordered data, and compare them
+    base_csv, reversed_csv, reversed_csv_backup = configure_paths(base_dir, reversed_dir, 'eplusout')
+    csv_match = do_comparison(base_csv, reversed_csv, reversed_csv_backup, num_design_days)
+
+    # do the same thing if the meter file exists
+    base_mtr, rev_mtr, rev_mtr_backup = configure_paths(base_dir, reversed_dir, 'eplusmtr')
+    mtr_match = do_comparison(base_mtr, rev_mtr, rev_mtr_backup, num_design_days) if os.path.exists(rev_mtr) else True
+
+    # report the results of the comparisons
+    if csv_match and mtr_match:
+        return 0
+    return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
