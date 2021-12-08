d26b05866610e83d8b9a6c67ca6e876ac9c22e77
Yujie Xu <shenqing1225@gmail.com>
2021-12-07 16:04:02 -0800

    Unit test for roof irrigation

--- tst/EnergyPlus/unit/EcoRoofManager.unit.cc
+++ tst/EnergyPlus/unit/EcoRoofManager.unit.cc
@@ -54,8 +54,12 @@
 #include "Fixtures/EnergyPlusFixture.hh"
 #include <EnergyPlus/Data/EnergyPlusData.hh>
 #include <EnergyPlus/DataEnvironment.hh>
+#include <EnergyPlus/DataWater.hh>
 #include <EnergyPlus/EcoRoofManager.hh>
+#include <EnergyPlus/HeatBalanceManager.hh>
 #include <EnergyPlus/SolarShading.hh>
+#include <EnergyPlus/WaterManager.hh>
+#include <EnergyPlus/DataHVACGlobals.hh>
 
 using namespace EnergyPlus;
 using namespace EnergyPlus::EcoRoofManager;
@@ -105,6 +109,156 @@ TEST_F(EnergyPlusFixture, EcoRoof_CalculateEcoRoofSolarTest)
     CalculateEcoRoofSolar(*state, resultRS, resultf1, SurfNum);
     EXPECT_NEAR(resultRS, expectedRS, 0.001);
     EXPECT_NEAR(resultf1, expectedf1, 0.001);
+}
+
+TEST_F(EnergyPlusFixture, EcoRoofManager_UpdateSoilProps)
+{
+    // with site:precipitation
+    std::string const idf_objects = delimited_string({
+
+        "Construction,",
+        "ASHRAE 90.1-2004_Sec 5.5-2_Roof,  !- Name",
+        "BaseEco,                 !- Outside Layer",
+        "ASHRAE 90.1-2004_Sec 5.5-2_Roof Insulation_1,  !- Layer 2",
+        "ASHRAE 90.1-2004_Sec 5.5-2_MAT-METAL;  !- Layer 3",
+
+        "Material:RoofVegetation,",
+        "BaseEco,                 !- Name",
+        "0.5,                     !- Height of Plants {m}",
+        "5,                       !- Leaf Area Index {dimensionless}",
+        "0.2,                     !- Leaf Reflectivity {dimensionless}",
+        "0.95,                    !- Leaf Emissivity",
+        "180,                     !- Minimum Stomatal Resistance {s/m}",
+        "EcoRoofSoil,             !- Soil Layer Name",
+        "MediumSmooth,            !- Roughness",
+        "0.18,                    !- Thickness {m}",
+        "0.4,                     !- Conductivity of Dry Soil {W/m-K}",
+        "641,                     !- Density of Dry Soil {kg/m3}",
+        "1100,                    !- Specific Heat of Dry Soil {J/kg-K}",
+        "0.95,                    !- Thermal Absorptance",
+        "0.8,                     !- Solar Absorptance",
+        "0.7,                     !- Visible Absorptance",
+        "0.4,                     !- Saturation Volumetric Moisture Content of the Soil Layer",
+        "0.01,                    !- Residual Volumetric Moisture Content of the Soil Layer",
+        "0.2,                     !- Initial Volumetric Moisture Content of the Soil Layer",
+        "Advanced;                !- Moisture Diffusion Calculation Method",
+
+        "Material,",
+        "ASHRAE 90.1-2004_Sec 5.5-2_Roof Insulation_1,  !- Name",
+        "MediumRough,             !- Roughness",
+        "0.1250,                  !- Thickness {m}",
+        "0.0490,                  !- Conductivity {W/m-K}",
+        "265.0000,                !- Density {kg/m3}",
+        "836.8000,                !- Specific Heat {J/kg-K}",
+        "0.9000,                  !- Thermal Absorptance",
+        "0.7000,                  !- Solar Absorptance",
+        "0.7000;                  !- Visible Absorptance",
+
+        "Material,",
+        "ASHRAE 90.1-2004_Sec 5.5-2_MAT-METAL,  !- Name",
+        "MediumSmooth,            !- Roughness",
+        "0.0015,                  !- Thickness {m}",
+        "45.0060,                 !- Conductivity {W/m-K}",
+        "7680.0000,               !- Density {kg/m3}",
+        "418.4000,                !- Specific Heat {J/kg-K}",
+        "0.9000,                  !- Thermal Absorptance",
+        "0.7000,                  !- Solar Absorptance",
+        "0.3000;                  !- Visible Absorptance",
+
+        "Site:Precipitation,",
+        "ScheduleAndDesignLevel,  !- Precipitation Model Type",
+        "0.75,                    !- Design Level for Total Annual Precipitation {m/yr}",
+        "PrecipitationSchd,       !- Precipitation Rates Schedule Name",
+        "0.75;                    !- Average Total Annual Precipitation {m/yr}",
+
+        "Schedule:Constant,",
+        "PrecipitationSchd,",
+        ",",
+        "1;",
+    });
+    ASSERT_TRUE(process_idf(idf_objects));
+
+    bool ErrorsFound = false;
+    // Read objects
+    HeatBalanceManager::GetMaterialData(*state, ErrorsFound);
+    EXPECT_FALSE(ErrorsFound);
+    HeatBalanceManager::GetConstructData(*state, ErrorsFound);
+    EXPECT_FALSE(ErrorsFound);
+
+    Real64 Moisture = 0.2;
+    Real64 MeanRootMoisture = 0.2;
+    Real64 Alphag = 0.2;
+    Real64 MoistureMax = 0.4;
+    Real64 MoistureResidual = 0.01;
+    Real64 SoilThickness = 0.18;
+    Real64 Vfluxf = 0.0; // Water mass flux from vegetation [m/s]
+    Real64 Vfluxg = 0.0; // Water mass flux from soil surface [m/s]
+    int ConstrNum = 1;
+    int unit = 0;
+    Real64 Tg = 10;
+    Real64 Tf = 10;
+    Real64 Qsoil = 0;
+    state->dataHVACGlobal->TimeStepSys = 0.25;
+
+    // With site:precipitation schedule
+    state->dataEnvrn->LiquidPrecipitation = 0.005; // unit m
+    WaterManager::GetWaterManagerInput(*state);
+    state->dataWaterData->RainFall.CurrentAmount = 0.002;
+
+    EcoRoofManager::UpdateSoilProps(*state,
+                                    Moisture,
+                                    MeanRootMoisture,
+                                    MoistureMax,
+                                    MoistureResidual,
+                                    SoilThickness,
+                                    Vfluxf,
+                                    Vfluxg,
+                                    ConstrNum,
+                                    Alphag,
+                                    unit,
+                                    Tg,
+                                    Tf,
+                                    Qsoil);
+    ASSERT_EQ(state->dataEcoRoofMgr->CurrentPrecipitation, state->dataWaterData->RainFall.CurrentAmount);
+
+    // Without site:precipitation, use epw "LiquidPrecipitation"
+    state->dataWaterData->RainFall.ModeID = DataWater::RainfallMode::Unassigned;
+    state->dataEnvrn->LiquidPrecipitation = 0.005;
+    EcoRoofManager::UpdateSoilProps(*state,
+                                    Moisture,
+                                    MeanRootMoisture,
+                                    MoistureMax,
+                                    MoistureResidual,
+                                    SoilThickness,
+                                    Vfluxf,
+                                    Vfluxg,
+                                    ConstrNum,
+                                    Alphag,
+                                    unit,
+                                    Tg,
+                                    Tf,
+                                    Qsoil);
+    ASSERT_EQ(state->dataEcoRoofMgr->CurrentPrecipitation, 0.005 * state->dataHVACGlobal->TimeStepSys);
+
+    // without site:precipitation, "LiquidPrecipitation" is also missing, but rain flag is on
+    state->dataWaterData->RainFall.ModeID = DataWater::RainfallMode::Unassigned;
+    state->dataEnvrn->LiquidPrecipitation = 0.0;
+    state->dataEnvrn->IsRain = true;
+    EcoRoofManager::UpdateSoilProps(*state,
+                                    Moisture,
+                                    MeanRootMoisture,
+                                    MoistureMax,
+                                    MoistureResidual,
+                                    SoilThickness,
+                                    Vfluxf,
+                                    Vfluxg,
+                                    ConstrNum,
+                                    Alphag,
+                                    unit,
+                                    Tg,
+                                    Tf,
+                                    Qsoil);
+    ASSERT_EQ(state->dataEcoRoofMgr->CurrentPrecipitation, 0.0015 * state->dataHVACGlobal->TimeStepSys);
 }
 
 } // namespace EnergyPlus
