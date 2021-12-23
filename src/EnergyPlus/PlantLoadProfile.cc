9f4ca59bf13789c557f4771ff4441859724bfba8
Jermy Thomas <jermythomasv@gmail.com>
2021-12-23 09:24:32 -0700

    plantLoc PlantLoadProfile

--- src/EnergyPlus/PlantLoadProfile.cc
+++ src/EnergyPlus/PlantLoadProfile.cc
@@ -146,9 +146,9 @@ void PlantProfileData::simulate(EnergyPlusData &state,
 
     if (this->MassFlowRate > 0.0) {
         Real64 Cp = GetSpecificHeatGlycol(state,
-                                          state.dataPlnt->PlantLoop(this->WLoopNum).FluidName,
+                                          state.dataPlnt->PlantLoop(this->plantLoc.loopNum).FluidName,
                                           this->InletTemp,
-                                          state.dataPlnt->PlantLoop(this->WLoopNum).FluidIndex,
+                                          state.dataPlnt->PlantLoop(this->plantLoc.loopNum).FluidIndex,
                                           RoutineName);
         DeltaTemp = this->Power / (this->MassFlowRate * Cp);
     } else {
@@ -201,9 +201,9 @@ void PlantProfileData::InitPlantProfile(EnergyPlusData &state)
         state.dataLoopNodes->Node(OutletNode).Temp = 0.0;
 
         FluidDensityInit = GetDensityGlycol(state,
-                                            state.dataPlnt->PlantLoop(this->WLoopNum).FluidName,
+                                            state.dataPlnt->PlantLoop(this->plantLoc.loopNum).FluidName,
                                             DataGlobalConstants::InitConvTemp,
-                                            state.dataPlnt->PlantLoop(this->WLoopNum).FluidIndex,
+                                            state.dataPlnt->PlantLoop(this->plantLoc.loopNum).FluidIndex,
                                             RoutineName);
 
         Real64 MaxFlowMultiplier = GetScheduleMaxValue(state, this->FlowRateFracSchedule);
@@ -213,10 +213,10 @@ void PlantProfileData::InitPlantProfile(EnergyPlusData &state)
                            this->PeakVolFlowRate * FluidDensityInit * MaxFlowMultiplier,
                            this->InletNode,
                            this->OutletNode,
-                           this->WLoopNum,
-                           this->WLoopSideNum,
-                           this->WLoopBranchNum,
-                           this->WLoopCompNum);
+                           this->plantLoc.loopNum,
+                           this->plantLoc.loopSideNum,
+                           this->plantLoc.branchNum,
+                           this->plantLoc.compNum);
 
         this->EMSOverrideMassFlow = false;
         this->EMSMassFlowValue = 0.0;
@@ -233,9 +233,9 @@ void PlantProfileData::InitPlantProfile(EnergyPlusData &state)
     if (this->EMSOverridePower) this->Power = this->EMSPowerValue;
 
     FluidDensityInit = GetDensityGlycol(state,
-                                        state.dataPlnt->PlantLoop(this->WLoopNum).FluidName,
+                                        state.dataPlnt->PlantLoop(this->plantLoc.loopNum).FluidName,
                                         this->InletTemp,
-                                        state.dataPlnt->PlantLoop(this->WLoopNum).FluidIndex,
+                                        state.dataPlnt->PlantLoop(this->plantLoc.loopNum).FluidIndex,
                                         RoutineName);
 
     // Get the scheduled mass flow rate
@@ -247,7 +247,7 @@ void PlantProfileData::InitPlantProfile(EnergyPlusData &state)
 
     // Request the mass flow rate from the plant component flow utility routine
     SetComponentFlowRate(
-        state, this->MassFlowRate, InletNode, OutletNode, this->WLoopNum, this->WLoopSideNum, this->WLoopBranchNum, this->WLoopCompNum);
+        state, this->MassFlowRate, InletNode, OutletNode, this->plantLoc.loopNum, this->plantLoc.loopSideNum, this->plantLoc.branchNum, this->plantLoc.compNum);
 
     this->VolFlowRate = this->MassFlowRate / FluidDensityInit;
 
@@ -301,7 +301,7 @@ void PlantProfileData::oneTimeInit_new(EnergyPlusData &state)
     if (allocated(state.dataPlnt->PlantLoop)) {
         errFlag = false;
         ScanPlantLoopsForObject(
-            state, this->Name, this->Type, this->WLoopNum, this->WLoopSideNum, this->WLoopBranchNum, this->WLoopCompNum, errFlag, _, _, _, _, _);
+            state, this->Name, this->Type, this->plantLoc.loopNum, this->plantLoc.loopSideNum, this->plantLoc.branchNum, this->plantLoc.compNum, errFlag, _, _, _, _, _);
         if (errFlag) {
             ShowFatalError(state, "InitPlantProfile: Program terminated for previous conditions.");
         }
