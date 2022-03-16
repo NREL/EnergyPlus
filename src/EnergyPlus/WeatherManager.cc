77df69342c2b4235e8b28248ea6c07502b97bceb
Yujie Xu <shenqing1225@gmail.com>
2022-03-16 01:00:34 -0700

    Lower bound following dev differ by timestep=1

--- src/EnergyPlus/WeatherManager.cc
+++ src/EnergyPlus/WeatherManager.cc
@@ -2330,7 +2330,12 @@ namespace WeatherManager {
         state.dataEnvrn->OpaqueCloudCover = state.dataWeatherManager->TodayOpaqueSkyCover(state.dataGlobal->TimeStep, state.dataGlobal->HourOfDay);
 
         if (state.dataWeatherManager->UseRainValues) {
-            state.dataEnvrn->IsRain = state.dataWaterData->RainFall.CurrentAmount >= 0.8 / 1000.0 / double(state.dataGlobal->NumOfTimeStepInHour);
+            // following the dev branch lower bound condition
+            if (state.dataWeatherManager->NumIntervalsPerHour == 1 && state.dataGlobal->NumOfTimeStepInHour > 1) {
+                state.dataEnvrn->IsRain = state.dataWaterData->RainFall.CurrentAmount >= 0.8 / 1000.0 / double(state.dataGlobal->NumOfTimeStepInHour);
+            } else {
+                state.dataEnvrn->IsRain = state.dataWeatherManager->TodayIsRain(state.dataGlobal->TimeStep, state.dataGlobal->HourOfDay);
+            }
         } else {
             state.dataEnvrn->IsRain = false;
         }
