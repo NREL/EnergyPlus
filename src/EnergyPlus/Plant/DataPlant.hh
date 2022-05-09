c09db15ae49d547f64c6e4b7329a8a965f45b77f
jcyuan <jinchaoyuan@gmail.com>
2022-05-09 00:58:53 -0500

    Additional data plant enum type for new connections.

--- src/EnergyPlus/Plant/DataPlant.hh
+++ src/EnergyPlus/Plant/DataPlant.hh
@@ -182,7 +182,9 @@ namespace DataPlant {
         "AirTerminal:SingleDuct:ConstantVolume:FourPipeBeam",
         "ZoneHVAC:CoolingPanel:RadiantConvective:Water",
         "HeatPump:PlantLoop:EIR:Cooling",
-        "HeatPump:PlantLoop:EIR:Heating"};
+        "HeatPump:PlantLoop:EIR:Heating",
+        "HeatPump:AirToWater:FuelFired:Heating",
+        "HeatPump:AirToWater:FuelFired:Cooling"};
 
     static constexpr std::array<std::string_view, static_cast<size_t>(PlantEquipmentType::Num)> PlantEquipTypeNamesUC{
         "BOILER:HOTWATER",
@@ -280,7 +282,9 @@ namespace DataPlant {
         "AIRTERMINAL:SINGLEDUCT:CONSTANTVOLUME:FOURPIPEBEAM",
         "ZONEHVAC:COOLINGPANEL:RADIANTCONVECTIVE:WATER",
         "HEATPUMP:PLANTLOOP:EIR:COOLING",
-        "HEATPUMP:PLANTLOOP:EIR:HEATING"};
+        "HEATPUMP:PLANTLOOP:EIR:HEATING",
+        "HEATPUMP:AIRTOWATER:FUELFIRED:HEATING",
+        "HEATPUMP:AIRTOWATER:FUELFIRED:COOLING"};
 
     static constexpr std::array<LoopType, static_cast<size_t>(PlantEquipmentType::Num)> ValidLoopEquipTypes{
         LoopType::Plant, LoopType::Plant, LoopType::Plant, LoopType::Plant, LoopType::Plant, LoopType::Plant, LoopType::Plant, LoopType::Plant,
