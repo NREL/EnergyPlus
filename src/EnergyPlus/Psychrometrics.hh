bd0db510a248ed0c3413cfec25c833c6154d3e6b
Julien Marrec <julien.marrec@gmail.com>
2022-08-24 17:06:41 +0200

    #define DISABLE_WARNING_UNINITIALIZED on windows

--- src/EnergyPlus/Psychrometrics.hh
+++ src/EnergyPlus/Psychrometrics.hh
@@ -90,6 +90,7 @@ struct EnergyPlusData;
 
 // purposfully doing nothing here - does MSVC not have a strict-aliasing warning?
 #define DISABLE_WARNING_STRICT_ALIASING
+#define DISABLE_WARNING_UNINITIALIZED
 #elif defined(__GNUC__) || defined(__clang__)
 #define DO_PRAGMA(X) _Pragma(#X)
 #define DISABLE_WARNING_PUSH DO_PRAGMA(GCC diagnostic push)
