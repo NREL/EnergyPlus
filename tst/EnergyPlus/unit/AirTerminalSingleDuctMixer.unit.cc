a89daa923dcbbf0c185211fb947a57414e6c83fa
nigusse <bnigusse@fsec.ucf.edu>
2022-12-14 17:01:14 -0500

    Fixed failed unit tests

--- tst/EnergyPlus/unit/AirTerminalSingleDuctMixer.unit.cc
+++ tst/EnergyPlus/unit/AirTerminalSingleDuctMixer.unit.cc
@@ -5042,6 +5042,8 @@ TEST_F(EnergyPlusFixture, AirTerminalSingleDuctMixer_SimVRFfluidCntrl_ATMInletSi
         state->dataLoopNodes->Node(state->dataZoneEquip->ZoneEquipConfig(1).ZoneNode).Enthalpy;
 
     state->dataHVACVarRefFlow->VRFTU(1).ZoneNum = 1;
+    state->dataHVACVarRefFlow->VRFTU(VRFTUNum).coolingCoilActive = true;
+
     state->dataSize->SysSizingRunDone = true;
     state->dataSize->ZoneSizingRunDone = true;
     state->dataGlobal->SysSizingCalc = true;
@@ -6799,6 +6801,7 @@ TEST_F(EnergyPlusFixture, AirTerminalSingleDuctMixer_SimVRFfluidCntrl_ATMSupplyS
         state->dataLoopNodes->Node(state->dataZoneEquip->ZoneEquipConfig(1).ZoneNode).Enthalpy;
 
     state->dataHVACVarRefFlow->VRFTU(1).ZoneNum = 1;
+    state->dataHVACVarRefFlow->VRFTU(VRFTUNum).coolingCoilActive = true;
     state->dataSize->SysSizingRunDone = true;
     state->dataSize->ZoneSizingRunDone = true;
     state->dataGlobal->SysSizingCalc = true;
