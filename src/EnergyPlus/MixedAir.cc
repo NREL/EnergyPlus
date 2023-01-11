dd3ec118353b355e54b38d7bcf7965dff604ff9b
rraustad <rraustad@fsec.ucf.edu>
2023-01-11 11:27:32 -0500

    Revert check for NumSimpControllers

--- src/EnergyPlus/MixedAir.cc
+++ src/EnergyPlus/MixedAir.cc
@@ -1060,8 +1060,11 @@ void GetOutsideAirSysInputs(EnergyPlusData &state)
                 OASys.ControllerIndex.dimension(NumInList, 0);
                 for (int InListNum = 1; InListNum <= NumInList; ++InListNum) {
                     OASys.ControllerName(InListNum) = AlphArray(InListNum * 2 + 1);
-                    OASys.ControllerType(InListNum) = AlphArray(InListNum * 2); // will be either Controller:OutdoorAir or Controller:WaterCoil
-                    ++NumSimpControllers;
+                    OASys.ControllerType(InListNum) = AlphArray(InListNum * 2);
+                    // only count Controller:OutdoorAir types as valid simple controllers
+                    if (!UtilityRoutines::SameString(OASys.ControllerType(InListNum), CurrentModuleObjects(static_cast<int>(CMO::OAController)))) {
+                        ++NumSimpControllers;
+                    }
                 }
             } else {
                 ShowSevereError(state,
