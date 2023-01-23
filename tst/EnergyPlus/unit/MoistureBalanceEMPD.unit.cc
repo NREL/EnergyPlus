533dcd5e111baf66ed2d7be3026154606e6f8f0e
Yujie Xu <shenqing1225@gmail.com>
2023-01-23 11:06:00 -0800

    clang-format in unit test

--- tst/EnergyPlus/unit/MoistureBalanceEMPD.unit.cc
+++ tst/EnergyPlus/unit/MoistureBalanceEMPD.unit.cc
@@ -364,8 +364,8 @@ TEST_F(EnergyPlusFixture, CheckEMPDCalc_Slope)
     // Convert stored vapor density to RH.
     Real64 RH_deep_layer_old = PsyRhFnTdbRhov(*state, Taver, RV_Deep_Old);
     Real64 RH_surf_layer_old = PsyRhFnTdbRhov(*state, Taver, RVaver);
-    Real64 mass_flux_surf_deep_max = material->EMPDDeepDepth * material->Density * dU_dRH * (RH_surf_layer_old - RH_deep_layer_old) /
-                                     (state->dataGlobal->TimeStepZone * 3600.0);
+    Real64 mass_flux_surf_deep_max =
+        material->EMPDDeepDepth * material->Density * dU_dRH * (RH_surf_layer_old - RH_deep_layer_old) / (state->dataGlobal->TimeStepZone * 3600.0);
 
     Real64 hm_deep_layer = 6.9551289450635225e-05;
     Real64 mass_flux_surf_deep_result = hm_deep_layer * (RVaver - RV_Deep_Old);
