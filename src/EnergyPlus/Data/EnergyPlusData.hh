321cf82622b9d270544a5763d7d23a49def1397b
Edwin Lee <leeed2001@gmail.com>
2023-02-15 16:19:29 -0600

    Move instance pointer to state, no more "global static" members!

--- src/EnergyPlus/Data/EnergyPlusData.hh
+++ src/EnergyPlus/Data/EnergyPlusData.hh
@@ -307,6 +307,7 @@ struct WindowComplexManagerData;
 struct WindowEquivLayerData;
 struct WindowEquivalentLayerData;
 struct WindowManagerData;
+struct WindowManagerExteriorData;
 struct ZoneAirLoopEquipmentManagerData;
 struct ZoneContaminantPredictorCorrectorData;
 struct ZoneDehumidifierData;
@@ -565,6 +566,7 @@ struct EnergyPlusData : BaseGlobalStruct
     std::unique_ptr<WindowEquivLayerData> dataWindowEquivLayer;
     std::unique_ptr<WindowEquivalentLayerData> dataWindowEquivalentLayer;
     std::unique_ptr<WindowManagerData> dataWindowManager;
+    std::unique_ptr<WindowManagerExteriorData> dataWindowManagerExterior;
     std::unique_ptr<ZoneAirLoopEquipmentManagerData> dataZoneAirLoopEquipmentManager;
     std::unique_ptr<ZoneContaminantPredictorCorrectorData> dataZoneContaminantPredictorCorrector;
     std::unique_ptr<ZoneDehumidifierData> dataZoneDehumidifier;
