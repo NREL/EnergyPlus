1c3e69ca8b9b2cbd36f8c08c51c27edde23269b4
jcyuan <jinchaoyuan@gmail.com>
2023-08-24 16:26:42 -0500

    Unit test update--assigning different default cond and evap rating temperatures based on condenser types.

--- tst/EnergyPlus/unit/ChillerConstantCOP.unit.cc
+++ tst/EnergyPlus/unit/ChillerConstantCOP.unit.cc
@@ -167,7 +167,7 @@ TEST_F(EnergyPlusFixture, ChillerConstantCOP_WaterCooled_Autosize)
 TEST_F(EnergyPlusFixture, ChillerConstantCOP_Default_Des_Cond_Evap_Temps)
 {
     // Unit test for PR 10158 that fixes Issue 10157)
-    state->dataPlnt->TotNumLoops = 4;
+    state->dataPlnt->TotNumLoops = 12;
     state->dataEnvrn->OutBaroPress = 101325.0;
     state->dataEnvrn->StdRhoAir = 1.20;
     state->dataGlobal->NumOfTimeStepInHour = 1;
@@ -176,16 +176,48 @@ TEST_F(EnergyPlusFixture, ChillerConstantCOP_Default_Des_Cond_Evap_Temps)
 
     std::string const idf_objects = delimited_string({
         "  Chiller:ConstantCOP,",
-        "    Chiller,                 !- Name",
+        "    Chiller_1_WaterCooled,   !- Name",
         "    autosize,                !- Nominal Capacity {W}",
         "    4.0,                     !- Nominal COP {W/W}",
         "    autosize,                !- Design Chilled Water Flow Rate {m3/s}",
         "    autosize,                !- Design Condenser Water Flow Rate {m3/s}",
-        "    Chiller ChW Inlet,       !- Chilled Water Inlet Node Name",
-        "    Chiller ChW Outlet,      !- Chilled Water Outlet Node Name",
-        "    Chiller Cnd Inlet,       !- Condenser Inlet Node Name",
-        "    Chiller Cnd Outlet,      !- Condenser Outlet Node Name",
+        "    Chiller 1 ChW Inlet,     !- Chilled Water Inlet Node Name",
+        "    Chiller 1 ChW Outlet,    !- Chilled Water Outlet Node Name",
+        "    Chiller 1 Cnd Inlet,     !- Condenser Inlet Node Name",
+        "    Chiller 1 Cnd Outlet,    !- Condenser Outlet Node Name",
         "    WaterCooled,             !- Condenser Type",
+        "    ConstantFlow,            !- Chiller Flow Mode",
+        "    1,                       !- Sizing Factor",
+        "    ,                        !- Basin Heater Capacity {W/K}",
+        "    2;                       !- Basin Heater Setpoint Temperature {C}",
+
+        "  Chiller:ConstantCOP,",
+        "    Chiller_2_AirCooled,     !- Name",
+        "    autosize,                !- Nominal Capacity {W}",
+        "    4.0,                     !- Nominal COP {W/W}",
+        "    autosize,                !- Design Chilled Water Flow Rate {m3/s}",
+        "    autosize,                !- Design Condenser Water Flow Rate {m3/s}",
+        "    Chiller 2 ChW Inlet,     !- Chilled Water Inlet Node Name",
+        "    Chiller 2 ChW Outlet,    !- Chilled Water Outlet Node Name",
+        "    Chiller 2 Cnd Inlet,     !- Condenser Inlet Node Name",
+        "    Chiller 2 Cnd Outlet,    !- Condenser Outlet Node Name",
+        "    AirCooled,               !- Condenser Type",
+        "    ConstantFlow,            !- Chiller Flow Mode",
+        "    1,                       !- Sizing Factor",
+        "    ,                        !- Basin Heater Capacity {W/K}",
+        "    2;                       !- Basin Heater Setpoint Temperature {C}",
+
+        "  Chiller:ConstantCOP,",
+        "    Chiller_3_EvapCooled,    !- Name",
+        "    autosize,                !- Nominal Capacity {W}",
+        "    4.0,                     !- Nominal COP {W/W}",
+        "    autosize,                !- Design Chilled Water Flow Rate {m3/s}",
+        "    autosize,                !- Design Condenser Water Flow Rate {m3/s}",
+        "    Chiller 3 ChW Inlet,     !- Chilled Water Inlet Node Name",
+        "    Chiller 3 ChW Outlet,    !- Chilled Water Outlet Node Name",
+        "    Chiller 3 Cnd Inlet,     !- Condenser Inlet Node Name",
+        "    Chiller 3 Cnd Outlet,    !- Condenser Outlet Node Name",
+        "    EvaporativelyCooled,     !- Condenser Type",
         "    ConstantFlow,            !- Chiller Flow Mode",
         "    1,                       !- Sizing Factor",
         "    ,                        !- Basin Heater Capacity {W/K}",
@@ -195,7 +227,7 @@ TEST_F(EnergyPlusFixture, ChillerConstantCOP_Default_Des_Cond_Evap_Temps)
     EXPECT_TRUE(process_idf(idf_objects, false));
 
     state->dataPlnt->PlantLoop.allocate(state->dataPlnt->TotNumLoops);
-    state->dataPlnt->PlantLoop.allocate(state->dataPlnt->TotNumLoops);
+
     for (int l = 1; l <= state->dataPlnt->TotNumLoops; ++l) {
         auto &loopside(state->dataPlnt->PlantLoop(l).LoopSide(DataPlant::LoopSideLocation::Demand));
         loopside.TotalBranches = 1;
@@ -207,8 +239,18 @@ TEST_F(EnergyPlusFixture, ChillerConstantCOP_Default_Des_Cond_Evap_Temps)
 
     ConstCOPChillerSpecs::getInput(*state);
 
-    auto &thisChiller = state->dataPlantChillers->ConstCOPChiller(1);
+    auto &thisChiller_1 = state->dataPlantChillers->ConstCOPChiller(1);
 
-    EXPECT_NEAR(thisChiller.TempDesCondIn, 29.44, 1e-3);
-    EXPECT_NEAR(thisChiller.TempDesEvapOut, 6.67, 1e-3);
+    EXPECT_NEAR(thisChiller_1.TempDesCondIn, 29.44, 1e-3);
+    EXPECT_NEAR(thisChiller_1.TempDesEvapOut, 6.67, 1e-3);
+
+    auto &thisChiller_2 = state->dataPlantChillers->ConstCOPChiller(2);
+
+    EXPECT_NEAR(thisChiller_2.TempDesCondIn, 35.0, 1e-3);
+    EXPECT_NEAR(thisChiller_2.TempDesEvapOut, 6.67, 1e-3);
+
+    auto &thisChiller_3 = state->dataPlantChillers->ConstCOPChiller(3);
+
+    EXPECT_NEAR(thisChiller_3.TempDesCondIn, 35.0, 1e-3);
+    EXPECT_NEAR(thisChiller_3.TempDesEvapOut, 6.67, 1e-3);
 }
