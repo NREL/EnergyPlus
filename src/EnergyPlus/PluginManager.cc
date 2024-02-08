7bc209c65b7ffa63bab99c58e9f4b475ec5071e5
Edwin Lee <leeed2001@gmail.com>
2024-02-07 19:58:13 -0600

    Clang format

--- src/EnergyPlus/PluginManager.cc
+++ src/EnergyPlus/PluginManager.cc
@@ -137,7 +137,7 @@ void runAnyRegisteredCallbacks(EnergyPlusData &state, EMSManager::EMSCallFrom iC
     if (state.dataGlobal->KickOffSimulation) return;
     for (auto const &cb : state.dataPluginManager->callbacks[iCalledFrom]) {
         if (iCalledFrom == EMSManager::EMSCallFrom::UserDefinedComponentModel) {
-            continue;  // these are called -intentionally- using the runSingleUserDefinedCallback method
+            continue; // these are called -intentionally- using the runSingleUserDefinedCallback method
         }
         cb((void *)&state);
         anyRan = true;
