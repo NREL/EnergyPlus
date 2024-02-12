962541b44978857bdad0b6a75cc4e82840b7809a
Jason Glazer <jglazer@gard.com>
2024-02-12 11:51:05 -0600

    Trying to add fan motor heat zone

--- src/EnergyPlus/HVACFan.cc
+++ src/EnergyPlus/HVACFan.cc
@@ -315,6 +315,9 @@ namespace HVACFan {
             state, state.dataOutRptPredefined->pdchFanAutosized, name, m_designAirVolFlowRateWasAutosized ? "Yes" : "No");
         OutputReportPredefined::PreDefTableEntry(state, state.dataOutRptPredefined->pdchFanMotorEff, name, m_motorEff);
         OutputReportPredefined::PreDefTableEntry(state, state.dataOutRptPredefined->pdchFanMotorHeatToZoneFrac, name, m_motorInAirFrac);
+//        if (m_heatLossesDestination == ThermalLossDestination::ZoneGains) {
+//            OutputReportPredefined::PreDefTableEntry(state, state.dataOutRptPredefined->pdchFanMotorHeatZone, name, state.dataHeatBal->Zone(m_zoneNum).Name);
+//        }
         OutputReportPredefined::PreDefTableEntry(state,
                                                  state.dataOutRptPredefined->pdchFanAirLoopName,
                                                  name,
