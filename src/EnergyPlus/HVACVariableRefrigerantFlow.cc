114ccb28a53ffe33220be4eb68a5c1eb90da3a72
Yujie Xu <yujiex@5c-e9-1e-83-4f-ff.dhcp.lbnl.us>
2024-02-28 10:56:43 -0800

    error msg to ensure CompEvaporatingCAPSpdMaxCurrentTsuc >= CompEvaporatingCAPSpdMin

--- src/EnergyPlus/HVACVariableRefrigerantFlow.cc
+++ src/EnergyPlus/HVACVariableRefrigerantFlow.cc
@@ -11857,7 +11857,16 @@ void VRFCondenserEquipment::CalcVRFCondenser_FluidTCtrl(EnergyPlusData &state)
                                                      CurveValue(state, this->OUCoolingCAPFT(NumOfCompSpdInput), Tdischarge, this->EvaporatingTemp);
         Real64 CompEvaporatingPWRSpdMaxCurrentTsuc =
             this->RatedCompPower * CurveValue(state, this->OUCoolingPWRFT(NumOfCompSpdInput), Tdischarge, this->EvaporatingTemp);
+        if (CompEvaporatingCAPSpdMin > CompEvaporatingCAPSpdMaxCurrentTsuc) {
+            ShowSevereMessage(state, format("{} \"{}\":", cVRFTypes(VRF_HeatPump), this->Name));
+            ShowContinueError(state,
+                              format(" Evaporative Capacity at max speed is smaller than evaporative capacity at min speed, "
+                                     "{:.3T} < {:.3T}",
+                                     CompEvaporatingCAPSpdMaxCurrentTsuc,
+                                     CompEvaporatingCAPSpdMin));
+        }
         if ((Q_c_OU * C_cap_operation) > CompEvaporatingCAPSpdMaxCurrentTsuc + CompEvaporatingPWRSpdMaxCurrentTsuc) {
+            // this branch resolves the issue of supplemental heating coil turning on when compressor speed is not at the highest
             Q_c_OU = CompEvaporatingCAPSpdMaxCurrentTsuc;
             CompSpdActual = this->CompressorSpeed(NumOfCompSpdInput);
             Ncomp = CompEvaporatingPWRSpdMaxCurrentTsuc;
@@ -11884,6 +11893,7 @@ void VRFCondenserEquipment::CalcVRFCondenser_FluidTCtrl(EnergyPlusData &state)
             CompSpdActual = this->CompressorSpeed(1);
 
         } else {
+            // CompEvaporatingCAPSpdMin < (Q_c_OU * C_cap_operation) <= CompEvaporatingCAPSpdMaxCurrentTsuc + CompEvaporatingPWRSpdMaxCurrentTsuc
             // Required heating load is greater than or equal to the min heating capacity
 
             // Iteration_Ncomp: Perform iterations to calculate Ncomp (Label20)
