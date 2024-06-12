72f3fa07df7800449edaacee97eefe2a8b6247e2
amirroth <roth.amir@gmail.com>
2024-06-12 15:51:11 -0400

    Move init_state below OpenOutputFiles

--- src/EnergyPlus/SimulationManager.cc
+++ src/EnergyPlus/SimulationManager.cc
@@ -209,9 +209,10 @@ namespace SimulationManager {
              state.dataInputProcessing->inputProcessor->getNumObjectsFound(state, "RunPeriod:CustomRange") > 0 || state.dataSysVars->FullAnnualRun);
         state.dataErrTracking->AskForConnectionsReport = false; // set to false until sizing is finished
 
+        OpenOutputFiles(state);
+
         state.init_state(state);
         
-        OpenOutputFiles(state);
         GetProjectData(state);
         Psychrometrics::InitializePsychRoutines(state);
         CheckForMisMatchedEnvironmentSpecifications(state);
