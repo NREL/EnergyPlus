95be09761d140451be93d2d701e72d9edcd48a9f
Julien Marrec <julien.marrec@gmail.com>
2024-08-05 21:59:49 +0200

    Exercise the verify_signature.py script

--- scripts/dev/verify_signature.py
+++ scripts/dev/verify_signature.py
@@ -1,10 +1,66 @@
+# EnergyPlus, Copyright (c) 1996-2024, The Board of Trustees of the University
+# of Illinois, The Regents of the University of California, through Lawrence
+# Berkeley National Laboratory (subject to receipt of any required approvals
+# from the U.S. Dept. of Energy), Oak Ridge National Laboratory, managed by UT-
+# Battelle, Alliance for Sustainable Energy, LLC, and other contributors. All
+# rights reserved.
+#
+# NOTICE: This Software was developed under funding from the U.S. Department of
+# Energy and the U.S. Government consequently retains certain rights. As such,
+# the U.S. Government has been granted for itself and others acting on its
+# behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
+# Software to reproduce, distribute copies to the public, prepare derivative
+# works, and perform publicly and display publicly, and to permit others to do
+# so.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# (1) Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+# (3) Neither the name of the University of California, Lawrence Berkeley
+#     National Laboratory, the University of Illinois, U.S. Dept. of Energy nor
+#     the names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+#
+# (4) Use of EnergyPlus(TM) Name. If Licensee (i) distributes the software in
+#     stand-alone form without changes from the version obtained under this
+#     License, or (ii) Licensee makes a reference solely to the software
+#     portion of its product, Licensee must refer to the software as
+#     "EnergyPlus version X" software, where "X" is the version number Licensee
+#     obtained under this License and may not use a different name for the
+#     software. Except as specifically required in this Section (4), Licensee
+#     shall not use in a company name, a product name, in advertising,
+#     publicity, or other promotional activities any name, trade name,
+#     trademark, logo, or other designation of "EnergyPlus", "E+", "e+" or
+#     confusingly similar designation, without the U.S. Department of Energy's
+#     prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
 import os
+import platform
 import subprocess
 from enum import Enum
 from pathlib import Path
 from typing import List
-import argparse
-import platform
+
 
 class Generator(Enum):
     IFW = 1
@@ -61,10 +117,14 @@ def find_executable_files(root_dir: Path) -> List[Path]:
     return files
 
 
-def verify_signature(p):
+def verify_signature(p, verbose=False, root_dir=None):
     info = subprocess.run(["codesign", "-dvvv", "--strict", str(p)], stderr=subprocess.PIPE, encoding="utf-8").stderr
     if "K7JYVQJL7" not in info:
         raise ValueError(f"{p} is not codesigned properly")
+    if verbose:
+        if root_dir is not None:
+            p = p.relative_to(root_dir)
+        print(f"- {p}: OK")
 
 
 def compare_executables():
@@ -85,16 +145,21 @@ def compare_executables():
 
 if __name__ == "__main__":
 
-    if platform.system() != 'Darwin':
+    if platform.system() != "Darwin":
         raise OSError("Only supported on Darwin")
 
     parser = argparse.ArgumentParser(description="Verify codesigning on macOS")
     parser.add_argument(
-        "build_dir", type=Path,
-        help="Root of the Build directory where CMakeCache.txt can be found (or the install dir if --install is passed"
+        "build_dir",
+        type=Path,
+        help="Root of the Build directory where CMakeCache.txt can be found (or the install dir if --install is passed",
     )
-    parser.add_argument("--install", action="store_true", default=False,
-                        help="This is an install dir, not the build_dir")
+    parser.add_argument(
+        "--install", action="store_true", default=False, help="This is an install dir, not the build_dir"
+    )
+    parser.add_argument(
+        "--verbose", action="store_true", default=False, help="This is an install dir, not the build_dir"
+    )
     args = parser.parse_args()
 
     build_dir = args.build_dir.resolve()
@@ -102,27 +167,31 @@ if __name__ == "__main__":
         raise IOError(f"{build_dir} is not a valid directory")
 
     if args.install:
-        if not (build_dir / 'energyplus').is_file():
+        if not (build_dir / "energyplus").is_file():
             raise ValueError(f"{build_dir} does not contain energyplus exe")
+        print(f"Checking Install dir {build_dir}")
         executable_files = find_executable_files(root_dir=build_dir)
         excludes = ["runenergyplus", "runreadvars", "runepmacro"]
-        executable_files = [x for x in executable_files
-                            if not any([n in x.name for n in excludes])]
+        executable_files = [x for x in executable_files if not any([n in x.name for n in excludes])]
         for p in executable_files:
-            verify_signature(p)
+            verify_signature(p, verbose=args.verbose, root_dir=build_dir)
+        print("Everything is signed correctly")
     else:
-        if not (build_dir / 'CMakeCache.txt').is_file():
+        if not (build_dir / "CMakeCache.txt").is_file():
             raise ValueError(f"{build_dir} does not contain CMakeCache.txt, did you forget to build?")
 
-        if not (build_dir / '_CPack_Packages').is_dir():
+        if not (build_dir / "_CPack_Packages").is_dir():
             raise ValueError(
                 f"{build_dir} does not contain a _CPack_Packages subfolder, did you forget to build the package?"
             )
 
         for generator in Generator:
+            print(f"Checking {generator.name} Generator")
             cmake_install_prefix = get_cmake_install_prefix_for_generator(build_dir=build_dir, generator=generator)
             if cmake_install_prefix is None:
                 continue
             executable_files = find_executable_files(root_dir=cmake_install_prefix)
             for p in executable_files:
-                verify_signature(p)
+                verify_signature(p, verbose=args.verbose, root_dir=cmake_install_prefix)
+            print("Everything is signed correctly")
+            print("=" * 80)
