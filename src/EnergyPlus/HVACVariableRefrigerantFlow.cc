49fd825b3d68d4d99114afe5e047d720cb41d96c
Yujie Xu <yujiex@5c-e9-1e-83-4f-ff.dhcp.lbnl.us>
2024-08-16 12:36:46 -0700

    clang-format

--- src/EnergyPlus/HVACVariableRefrigerantFlow.cc
+++ src/EnergyPlus/HVACVariableRefrigerantFlow.cc
@@ -12850,16 +12850,7 @@ void VRFTerminalUnitEquipment::CalcVRF_FluidTCtrl(EnergyPlusData &state,
         auto *fan = state.dataFans->fans(state.dataHVACVarRefFlow->VRFTU(VRFTUNum).FanIndex);
         if (state.dataHVACVarRefFlow->VRFTU(VRFTUNum).fanType == HVAC::FanType::SystemModel) {
             if (OnOffAirFlowRatio > 0.0) {
-                fan->simulate(state,
-                              FirstHVACIteration,
-                              _,
-                              _,
-                              _,
-                              fan->inletAirMassFlowRate,
-                              OnOffFanPartLoadFraction,
-                              0,
-                              0,
-                              _);
+                fan->simulate(state, FirstHVACIteration, _, _, _, fan->inletAirMassFlowRate, OnOffFanPartLoadFraction, 0, 0, _);
             } else {
                 fan->simulate(state, FirstHVACIteration, _, _, PartLoadRatio);
             }
