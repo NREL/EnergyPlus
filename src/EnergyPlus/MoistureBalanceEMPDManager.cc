6daaf7a7bbc07ddb7e11be0aa50dcaecd3d86132
rraustad <rraustad@fsec.ucf.edu>
2024-08-23 22:18:58 -0400

    backup correct build error

--- src/EnergyPlus/MoistureBalanceEMPDManager.cc
+++ src/EnergyPlus/MoistureBalanceEMPDManager.cc
@@ -351,7 +351,6 @@ void InitMoistureBalanceEMPD(EnergyPlusData &state)
     using Psychrometrics::PsyRhovFnTdbWPb_fast;
 
     // SUBROUTINE LOCAL VARIABLE DECLARATIONS:
-    int ZoneNum;
     int SurfNum;
 
     if (state.dataMoistureBalEMPD->InitEnvrnFlag) {
@@ -366,8 +365,8 @@ void InitMoistureBalanceEMPD(EnergyPlusData &state)
         state.dataMstBalEMPD->RVwall.allocate(state.dataSurface->TotSurfaces);
     }
 
-    for (SurfNum = 1; SurfNum <= state.dataSurface->TotSurfaces; ++SurfNum) {
-        ZoneNum = state.dataSurface->Surface(SurfNum).Zone;
+    for (int SurfNum = 1; SurfNum <= state.dataSurface->TotSurfaces; ++SurfNum) {
+        int ZoneNum = state.dataSurface->Surface(SurfNum).Zone;
         if (!state.dataSurface->Surface(SurfNum).HeatTransSurf) continue;
         Real64 const rv_air_in_initval =
             min(PsyRhovFnTdbWPb_fast(state.dataZoneTempPredictorCorrector->zoneHeatBalance(ZoneNum).MAT,
@@ -387,7 +386,7 @@ void InitMoistureBalanceEMPD(EnergyPlusData &state)
 
     GetMoistureBalanceEMPDInput(state);
 
-    for (SurfNum = 1; SurfNum <= state.dataSurface->TotSurfaces; ++SurfNum) {
+    for (int SurfNum = 1; SurfNum <= state.dataSurface->TotSurfaces; ++SurfNum) {
         if (!state.dataSurface->Surface(SurfNum).HeatTransSurf) continue;
         if (state.dataSurface->Surface(SurfNum).Class == DataSurfaces::SurfaceClass::Window) continue;
         EMPDReportVarsData &rvd = state.dataMoistureBalEMPD->EMPDReportVars(SurfNum);
@@ -493,7 +492,6 @@ void CalcMoistureBalanceEMPD(EnergyPlusData &state,
     static constexpr std::string_view RoutineName("CalcMoistureEMPD");
 
     // SUBROUTINE LOCAL VARIABLE DECLARATIONS:
-    int NOFITR;           // Number of iterations
     int MatNum;           // Material number at interior layer
     int ConstrNum;        // Construction number
     Real64 hm_deep_layer; // Overall deep-layer transfer coefficient
@@ -503,7 +501,6 @@ void CalcMoistureBalanceEMPD(EnergyPlusData &state,
     Real64 RHaver; // Average zone relative humidity {0-1} between current time and previous time
     Real64 RVaver; // Average zone vapor density
     Real64 dU_dRH;
-    int Flag;             // Convergence flag (0 - converged)
     Real64 PVsurf;        // Surface vapor pressure
     Real64 PV_surf_layer; // Vapor pressure of surface layer
     Real64 PV_deep_layer;
@@ -546,8 +543,6 @@ void CalcMoistureBalanceEMPD(EnergyPlusData &state,
     auto &heat_flux_latent(state.dataMstBalEMPD->HeatFluxLatent(SurfNum)); // output
 
     heat_flux_latent = 0.0;
-    Flag = 1;
-    NOFITR = 0;
     if (!surface.HeatTransSurf) {
         return;
     }
