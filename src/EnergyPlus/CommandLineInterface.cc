f5fa4bbd4165ccf59e22b0fcf3dd0190b8811e26
Edwin Lee <leeed2001@gmail.com>
2024-09-17 14:49:00 -0500

    Clang format

--- src/EnergyPlus/CommandLineInterface.cc
+++ src/EnergyPlus/CommandLineInterface.cc
@@ -234,7 +234,7 @@ Built on Platform: {}
         // bool debugCLI = false;
         app.add_flag("--debug-cli", debugCLI, "Print the result of the CLI assignments to the console and exit")->group(""); // Empty group to hide it
 
-        #if LINK_WITH_PYTHON
+#if LINK_WITH_PYTHON
         auto *auxiliaryToolsSubcommand = app.add_subcommand("auxiliary", "Run Auxiliary Python Tools");
         auxiliaryToolsSubcommand->require_subcommand(); // should default to requiring 1 or more additional args?
 
@@ -282,7 +282,7 @@ main_gui()
             engine.exec(cmd);
             exit(0);
         });
-        #endif
+#endif
 
         app.footer("Example: energyplus -w weather.epw -r input.idf");
 
