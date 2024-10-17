0e0005b7f9e69d9722b72bc97fce55fa8344e6fa
Michael J. Witte <mjwitte@gard.com>
2024-10-17 13:49:00 -0500

    Warn if chiller chills when off

--- src/EnergyPlus/ChillerElectricEIR.cc
+++ src/EnergyPlus/ChillerElectricEIR.cc
@@ -2583,7 +2583,24 @@ void ElectricEIRChillerSpecs::update(EnergyPlusData &state, Real64 const MyLoad,
     Real64 ReportingConstant = state.dataHVACGlobal->TimeStepSysSec;
 
     if (MyLoad >= 0 || !RunFlag) { // Chiller not running so pass inlet states to outlet states
-        // Set node conditions
+        // if (MyLoad >= 0 || !RunFlag || this->QEvaporator <= 0.0) { // Chiller not running so pass inlet states to outlet states
+        // if ((MyLoad < 0 && RunFlag) && (this->CondMassFlowRate >= DataBranchAirLoopPlant::MassFlowTolerance ||
+        //                                 this->EvapMassFlowRate >= DataBranchAirLoopPlant::MassFlowTolerance)) {
+        //     if (!state.dataGlobal->WarmupFlag && !state.dataGlobal->DoingHVACSizingSimulations &&
+        //         state.dataPlnt->PlantLoop(this->CWPlantLoc.loopNum).LoopSide(this->CWPlantLoc.loopSideNum).FlowLock == DataPlant::FlowLock::Locked)
+        //         { ShowWarningError(state, "Chiller off due to this->QEvaporator <= 0.0 rule"); ShowContinueErrorTimeStamp(
+        //             state,
+        //             format("MyLoad={}, RunFlag={}, this->QEvaporator={}, this->QCondenser={}, this->EvapMassFlowRate={},
+        //             this->CondMassFlowRate={}",
+        //                    MyLoad,
+        //                    RunFlag,
+        //                    this->QEvaporator,
+        //                    this->QCondenser,
+        //                    this->EvapMassFlowRate,
+        //                    this->CondMassFlowRate));
+        //     }
+        // }
+        //  Set node conditions
         state.dataLoopNodes->Node(this->EvapOutletNodeNum).Temp = state.dataLoopNodes->Node(this->EvapInletNodeNum).Temp;
         state.dataLoopNodes->Node(this->CondOutletNodeNum).Temp = state.dataLoopNodes->Node(this->CondInletNodeNum).Temp;
         if (this->CondenserType != DataPlant::CondenserType::WaterCooled) {
@@ -2678,6 +2695,28 @@ void ElectricEIRChillerSpecs::update(EnergyPlusData &state, Real64 const MyLoad,
             state.dataLoopNodes->Node(this->HeatRecOutletNodeNum).Temp = this->HeatRecOutletTemp;
             this->HeatRecInletTemp = state.dataLoopNodes->Node(this->HeatRecInletNodeNum).Temp;
             this->HeatRecMassFlow = state.dataLoopNodes->Node(this->HeatRecInletNodeNum).MassFlowRate;
+        }
+    }
+    if (((this->EvapMassFlowRate < DataBranchAirLoopPlant::MassFlowTolerance) ||
+         (this->CondMassFlowRate < DataBranchAirLoopPlant::MassFlowTolerance)) &&
+        (this->EvapInletTemp != this->EvapOutletTemp)) {
+        if (!state.dataGlobal->WarmupFlag && !state.dataGlobal->DoingHVACSizingSimulations &&
+            state.dataPlnt->PlantLoop(this->CWPlantLoc.loopNum).LoopSide(this->CWPlantLoc.loopSideNum).FlowLock == DataPlant::FlowLock::Locked) {
+            ShowWarningError(state,
+                             format("Chiller should be off, evap flow rate={}, but EvapInletTemp={} is != EvapOutletTemp={}",
+                                    this->EvapMassFlowRate,
+                                    this->EvapInletTemp,
+                                    this->EvapOutletTemp));
+            ShowContinueErrorTimeStamp(state,
+                                       format("MyLoad={}, RunFlag={}, QEvaporator={}, QCondenser={}, EvapMassFlowRate={}, "
+                                              "CondMassFlowRate = {}, Power={} ",
+                                              MyLoad,
+                                              RunFlag,
+                                              this->QEvaporator,
+                                              this->QCondenser,
+                                              this->EvapMassFlowRate,
+                                              this->CondMassFlowRate,
+                                              this->Power));
         }
     }
 }
